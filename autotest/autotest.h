6ce2e80aa260ec83b5751cb2ee88137061f7caa4
Joseph D. Gaeddert <jgaedder@vt.edu>
2008-10-05 14:33:36 +0000

    adding basic autotest script

---
+++ autotest/autotest.h
@@ -0,0 +1,65 @@
+//
+// Lightweight autotest header
+//
+// Similar to CxxTest, but written for Liquid DSP code in C
+//
+
+#ifndef __LIQUID_AUTOTEST_H__
+#define __LIQUID_AUTOTEST_H__
+
+static unsigned int _autotest_num_passed;
+static unsigned int _autotest_num_failed;
+
+static inline void test_failed(
+    const char * _file,
+    unsigned int _line,
+    const char * _exprL,
+    const char * _blah,
+    const char * _exprR)
+{
+    printf("TEST FAILED: %s line %u : expected %s %s %s\n", _file, _line, _exprL, _blah,_exprR);
+    _autotest_num_failed++;
+}
+
+static inline void test_passed()
+{
+    _autotest_num_passed++;
+}
+
+static void autotest_print_results(void)
+{
+    printf("=============================\n");
+    if (_autotest_num_failed==0) {
+        printf(" PASSED ALL %u TESTS\n", _autotest_num_passed);
+    } else {
+        printf(" FAILED %u / %u TESTS\n", _autotest_num_failed, _autotest_num_failed+_autotest_num_passed);
+    }
+    printf("=============================\n");
+}
+
+// CONTEND_EQUALITY
+#  define TEST_EQUALITY(F,L,EX,X,EY,Y)      \
+     if ((X)!=(Y)) test_failed(F,L,EX,"==",EY); else test_passed();
+#  define _CONTEND_EQUALITY(F,L,X,Y)        TEST_EQUALITY(F,L,#X,(X),#Y,(Y))
+#  define CONTEND_EQUALITY(X,Y)             _CONTEND_EQUALITY(__FILE__,__LINE__,X,Y)
+
+// CONTEND_INEQUALITY
+#  define TEST_INEQUALITY(F,L,EX,X,EY,Y)    \
+     if ((X)==(Y)) test_failed(F,L,EX,"!=",EY); else test_passed();
+#  define _CONTEND_INEQUALITY(F,L,X,Y)      TEST_INEQUALITY(F,L,#X,(X),#Y,(Y))
+#  define CONTEND_INEQUALITY(X,Y)           _CONTEND_INEQUALITY(__FILE__,__LINE__,X,Y)
+
+// CONTEND_GREATER_THAN
+#  define TEST_GREATER_THAN(F,L,EX,X,EY,Y)    \
+     if ((X)<=(Y)) test_failed(F,L,EX,">",EY); else test_passed();
+#  define _CONTEND_GREATER_THAN(F,L,X,Y)    TEST_GREATER_THAN(F,L,#X,(X),#Y,(Y))
+#  define CONTEND_GREATER_THAN(X,Y)         _CONTEND_GREATER_THAN(__FILE__,__LINE__,X,Y)
+
+// CONTEND_LESS_THAN
+#  define TEST_LESS_THAN(F,L,EX,X,EY,Y)    \
+     if ((X)>=(Y)) test_failed(F,L,EX,">",EY); else test_passed();
+#  define _CONTEND_LESS_THAN(F,L,X,Y)       TEST_LESS_THAN(F,L,#X,(X),#Y,(Y))
+#  define CONTEND_LESS_THAN(X,Y)            _CONTEND_LESS_THAN(__FILE__,__LINE__,X,Y)
+
+#endif // __LIQUID_AUTOTEST_H__
+
---
+++ autotest/test_autotest.c
@@ -0,0 +1,31 @@
+//
+//
+//
+
+#include <stdio.h>
+#include "autotest.h"
+
+int main() {
+
+    CONTEND_EQUALITY(1,1);
+    CONTEND_EQUALITY(1,2);
+
+    CONTEND_INEQUALITY(1,1);
+    CONTEND_INEQUALITY(1,2);
+
+    CONTEND_GREATER_THAN(1,1);
+    CONTEND_GREATER_THAN(1,2);
+
+    CONTEND_LESS_THAN(1,1);
+    CONTEND_LESS_THAN(1,2);
+
+    int x = 3, y = 5;
+
+    CONTEND_INEQUALITY(x,y);
+
+    autotest_print_results();
+
+    printf("done.\n");
+    return 0;
+}
+
