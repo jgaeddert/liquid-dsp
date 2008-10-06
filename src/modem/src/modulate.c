df6b71587b9aac38127b370574fbba6e9ac87b12
Joseph D. Gaeddert <jgaedder@vt.edu>
2008-10-06 18:49:01 +0000

    cleaning up source with repeated code

--- src/modem/src/modulate.c
+++ src/modem/src/modulate.c
@@ -4,18 +4,6 @@
 
 #include "modem_internal.h"
 
-// Generate random symbol
-unsigned int modem_gen_rand_sym(modem _mod)
-{
-    return rand() % (_mod->M);
-}
-
-unsigned int modem_get_bps(modem _mod)
-{
-    return _mod->m;
-}
-
-
 void modulate(
     modem _mod,
     unsigned int symbol_in,
