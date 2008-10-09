4af9e8793b22aa8f3de42e8a87ba86b78cc94da5
Joseph D. Gaeddert <jgaedder@vt.edu>
2008-10-09 21:01:00 +0000

    addresses ticket #2 which might be somewhat invalid; agc interface was used wrong in example, but issue with log(0) still exists

--- src/agc/src/main_agc.c
+++ src/agc/src/main_agc.c
@@ -6,16 +6,15 @@
 #include "agc.h"
 
 int main() {
-    agc p = agc_create(1.0f, 0.01f);
+    agc p = agc_create(100.0f, 0.02f);
 
     unsigned int i;
-    float complex x = 0.1f, y;
+    float complex x = 1.2f + _Complex_I*3.9f, y=x;
     printf("i    \tlevel\n");
     printf("-----\t-----\n");
-    for (i=0; i<20; i++) {
-        printf("%u\t%f\n", i, crealf(x));
+    for (i=0; i<25; i++) {
+        printf("%u\t%f\n", i, cabsf(y));
         agc_apply_gain(p, x, &y);
-        x = y;
     }
 
     agc_free(p);
