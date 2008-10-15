11bed8c7b0e2dd404457018bf6b1a193bf3dd90d
Joseph D. Gaeddert <jgaedder@vt.edu>
2008-10-15 11:46:18 +0000

    creating example benchmark

--- bench/bench_example.h
+++ bench/bench_example.h
@@ -5,7 +5,9 @@
 // includes:
 //   #include <sys/resource.h>
 //   #include <getopt.h>
-#include "liquid/benchmark.h"
+//#include "liquid/benchmark.h"
+
+#include <math.h>
 
 // strings parsed by benchmarkgen.py
 const char * mybench_opts[3] = {
@@ -18,16 +20,20 @@ const char * mybench_opts[3] = {
 void benchmark_mybench(
     struct rusage *_start,
     struct rusage *_finish,
-    unsigned long int *_num_iterations,
-    unsigned int argc,
-    char *argv[])
+    unsigned long int *_num_iterations)
+//    unsigned int argc,
+//    char *argv[])
 {
     // DSP initiazation goes here
 
     unsigned int i;
+    float x, y, theta;
     getrusage(RUSAGE_SELF, _start);
     for (i=0; i<(*_num_iterations); i++) {
         // DSP execution goes here
+        x = cosf(M_PI/2.0f);
+        y = sinf(M_PI/2.0f);
+        theta = atan2(y,x);
     }
     getrusage(RUSAGE_SELF, _finish);
 
