0ecc5b18c506b227bd19d11786e79606db368c76
Joseph D. Gaeddert <jgaedder@vt.edu>
2008-10-15 12:37:29 +0000

    minor modification for agc test

--- src/agc/src/main_agc.c
+++ src/agc/src/main_agc.c
@@ -6,14 +6,15 @@
 #include "agc.h"
 
 int main() {
-    agc p = agc_create(100.0f, 0.02f);
+    agc p = agc_create(100.0f, 0.01f);
 
     unsigned int i;
     float complex x = 1.2f + _Complex_I*3.9f, y=x;
     printf("i    \tlevel\n");
     printf("-----\t-----\n");
-    for (i=0; i<25; i++) {
-        printf("%u\t%f\n", i, cabsf(y));
+    for (i=0; i<100; i++) {
+        if (((i+1)%5)==0)
+            printf("%u\t%f\n", i+1, cabsf(y));
         agc_apply_gain(p, x, &y);
     }
 
