278b025babbe534dcddbffe75486ba1b5f83190e
Joseph D. Gaeddert <jgaedder@vt.edu>
2008-10-17 17:15:53 +0000

    making header files generic to data type

--- src/buffer/src/buffer.h
+++ src/buffer/src/buffer.h
@@ -5,30 +5,36 @@
 #ifndef __MODULE_BUFFER_H__
 #define __MODULE_BUFFER_H__
 
-typedef struct buffer_s * buffer;
+#include <complex.h>
 
 typedef enum {
     CIRCULAR=0,
     STATIC
 } buffer_type;
 
-buffer buffer_create(buffer_type _type, unsigned int _n);
+#define LIQUID_CONCAT(prefix, name) prefix ## name
+#define BUFFER_MANGLE_FLOAT(name)  LIQUID_CONCAT(buffer, name)
+#define BUFFER_MANGLE_CFLOAT(name) LIQUID_CONCAT(cbuffer, name)
 
-void buffer_destroy(buffer _cb);
+// large macro
+//   X: name-mangling macro
+//   T: data type
+#define LIQUID_BUFFER_DEFINE_API(X,T)                   \
+                                                        \
+typedef struct X(_s) * X();                             \
+X() X(_create)(buffer_type _type, unsigned int _n);     \
+void X(_destroy)(X() _b);                               \
+void X(_print)(X() _b);                                 \
+void X(_debug_print)(X() _b);                           \
+void X(_clear)(X() _b);                                 \
+void X(_read)(X() _b, T ** _v, unsigned int *_n);       \
+void X(_release)(X() _b, unsigned int _n);              \
+void X(_write)(X() _b, T * _v, unsigned int _n);
+//void X(_force_write)(X() _b, T * _v, unsigned int _n);
 
-void buffer_print(buffer _cb);
-
-void buffer_debug_print(buffer _cb);
-
-void buffer_clear(buffer _cb);
-
-// read at least *_n elements, return actual number available
-void buffer_read(buffer _cb, float ** _v, unsigned int *_n);
-
-void buffer_release(buffer _cb, unsigned int _n);
-
-void buffer_write(buffer _cb, float * _v, unsigned int _n);
-//void buffer_force_write(buffer _cb, float * _v, unsigned int _n);
+// Define APIs
+LIQUID_BUFFER_DEFINE_API(BUFFER_MANGLE_FLOAT, float)
+LIQUID_BUFFER_DEFINE_API(BUFFER_MANGLE_CFLOAT, float complex)
 
 #endif // __MODULE_BUFFER_H__
 
--- src/buffer/src/buffer_internal.h
+++ src/buffer/src/buffer_internal.h
@@ -1,5 +1,5 @@
 //
-// Circular buffer
+// Buffers, internal functions
 //
 
 #ifndef __LIQUID_BUFFER_INTERNAL_H__
@@ -8,16 +8,18 @@
 #include "buffer.h"
 
 #define buffer_fast_access(c,i) (c->v[(c->read_index+i)%(c->len)])
-void buffer_linearize(buffer _b);
 
-void buffer_c_read(buffer _b, float ** _v, unsigned int * _n);
-void buffer_s_read(buffer _b, float ** _v, unsigned int * _n);
+#define LIQUID_BUFFER_DEFINE_INTERNAL_API(X,T)          \
+void X(_linearize)(X() _b);                             \
+void X(_c_read)(X() _b, T ** _v, unsigned int *_n);     \
+void X(_s_read)(X() _b, T ** _v, unsigned int *_n);     \
+void X(_c_write)(X() _b, T * _v, unsigned int _n);      \
+void X(_s_write)(X() _b, T * _v, unsigned int _n);      \
+void X(_c_release)(X() _b, unsigned int _n);            \
+void X(_s_release)(X() _b, unsigned int _n);
 
-void buffer_c_write(buffer _b, float * _v, unsigned int _n);
-void buffer_s_write(buffer _b, float * _v, unsigned int _n);
-
-void buffer_c_release(buffer _b, unsigned int _n);
-void buffer_s_release(buffer _b, unsigned int _n);
+LIQUID_BUFFER_DEFINE_INTERNAL_API(BUFFER_MANGLE_FLOAT, float);
+LIQUID_BUFFER_DEFINE_INTERNAL_API(BUFFER_MANGLE_CFLOAT, float complex);
 
 #endif // __LIQUID_BUFFER_INTERNAL_H__
 
