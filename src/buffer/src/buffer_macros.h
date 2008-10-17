a2ebf434fdbb98d56819edce932d660ded67bc71
Joseph D. Gaeddert <jgaedder@vt.edu>
2008-10-17 18:38:05 +0000

    adding comments/steps to build new buffer type

--- src/buffer/src/buffer_macros.h
+++ src/buffer/src/buffer_macros.h
@@ -1,5 +1,27 @@
 //
+// Buffer macros
+//   Defines macros to be used in buffer.c for efficient
+//   code reuse.  Macros are defined as follows:
+//     X : name-mangling macro
+//     T : data type
+//     BUFFER_PRINT_LINE : for printing formatted text
 //
+//   The following steps need to be taken to add a new
+//   buffer type "tbuffer" of type "mytype":
+//     1. Modify buffer.h and buffer_internal.h to add
+//        new APIs:
+//          #define BUFFER_MANGLE_MYTYPE(name) LIQUID_CONCAT(tbuffer, name)
+//          LIQUID_BUFFER_DEFINE_API(BUFFER_MANGLE_TYPE, mytype)
+//          LIQUID_BUFFER_DEFINE_INTERNAL_API(BUFFER_MANGLE_TYPE, mytype)
+//     2. Modify this file (buffer_macros.h) and define
+//          #define X(name) CONCAT(tbuffer, name)
+//          #define T mytype
+//          #define BUFFER_PRINT_LINE(B,I) printf(...);
+//     3. Create a new file "tbuffer.c" to include
+//          #define BUFFER_TYPE_MYTYPE
+//          #include "buffer_macros.h"
+//          #include "buffer.c"
+//     4. Modify the Makefile to include the target tbuffer.o
 //
 
 #ifndef __LIQUID_BUFFER_MACROS_H__
