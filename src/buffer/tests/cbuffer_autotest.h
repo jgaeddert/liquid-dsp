fc3961b57aabd22f844f7a1f8cb24545389db97d
Joseph D. Gaeddert <jgaedder@vt.edu>
2008-10-18 16:17:14 +0000

    using macro in autotests

--- src/buffer/tests/cbuffer_autotest.h
+++ src/buffer/tests/cbuffer_autotest.h
@@ -5,34 +5,40 @@
 #include "../../../autotest/autotest.h"
 #include "../src/buffer_internal.h"
 
+// 
+// Macro
+//
+#define CBUFFER_AUTOTEST_DEFINE_API(X,T)        \
+    T v[] = {1, 2, 3, 4, 5, 6, 7, 8};           \
+    T test1[] = {1, 2, 3, 4};                   \
+    T test2[] = {3, 4, 1, 2, 3, 4, 5, 6, 7, 8}; \
+    T *r;                                       \
+    unsigned int n;                             \
+                                                \
+    X() cb = X(_create)(CIRCULAR,10);           \
+                                                \
+    X(_write)(cb, v, 4);                        \
+    n = 4;                                      \
+    X(_read)(cb, &r, &n);                       \
+                                                \
+    CONTEND_EQUALITY(n,4);                      \
+    CONTEND_SAME_DATA(r,test1,4*sizeof(T));     \
+                                                \
+    X(_release)(cb, 2);                         \
+    X(_write)(cb, v, 8);                        \
+    n = 10;                                     \
+    X(_read)(cb, &r, &n);                       \
+    CONTEND_EQUALITY(n,10);                     \
+    CONTEND_SAME_DATA(r,test2,10*sizeof(T));    \
+                                                \
+    X(_destroy)(cb);
+
 //
 // AUTOTEST: circular float buffer
 //
 void autotest_fbuffer_circular()
 {
-    float v[] = {1, 2, 3, 4, 5, 6, 7, 8};
-    float test1[] = {1, 2, 3, 4};
-    float test2[] = {3, 4, 1, 2, 3, 4, 5, 6, 7, 8};
-    float *r; // reader
-    unsigned int n;
-
-    fbuffer cb = fbuffer_create(CIRCULAR,10);
-
-    fbuffer_write(cb, v, 4);
-    n = 4;
-    fbuffer_read(cb, &r, &n);
-
-    CONTEND_EQUALITY(n,4);
-    CONTEND_SAME_DATA(r,test1,4*sizeof(float));
-
-    fbuffer_release(cb, 2);
-    fbuffer_write(cb, v, 8);
-    n = 10;
-    fbuffer_read(cb, &r, &n);
-    CONTEND_EQUALITY(n,10);
-    CONTEND_SAME_DATA(r,test2,10*sizeof(float));
-
-    fbuffer_destroy(cb);
+    CBUFFER_AUTOTEST_DEFINE_API(BUFFER_MANGLE_FLOAT, float)
 }
 
 
@@ -41,29 +47,7 @@ void autotest_fbuffer_circular()
 //
 void autotest_cfbuffer_circular()
 {
-    float complex v[] = {1, 2, 3, 4, 5, 6, 7, 8};
-    float complex test1[] = {1, 2, 3, 4};
-    float complex test2[] = {3, 4, 1, 2, 3, 4, 5, 6, 7, 8};
-    float complex *r; // reader
-    unsigned int n;
-
-    cfbuffer cb = cfbuffer_create(CIRCULAR,10);
-
-    cfbuffer_write(cb, v, 4);
-    n = 4;
-    cfbuffer_read(cb, &r, &n);
-
-    CONTEND_EQUALITY(n,4);
-    CONTEND_SAME_DATA(r,test1,4*sizeof(float complex));
-
-    cfbuffer_release(cb, 2);
-    cfbuffer_write(cb, v, 8);
-    n = 10;
-    cfbuffer_read(cb, &r, &n);
-    CONTEND_EQUALITY(n,10);
-    CONTEND_SAME_DATA(r,test2,10*sizeof(float complex));
-
-    cfbuffer_destroy(cb);
+    CBUFFER_AUTOTEST_DEFINE_API(BUFFER_MANGLE_CFLOAT, float complex)
 }
 
 //
@@ -71,29 +55,7 @@ void autotest_cfbuffer_circular()
 //
 void autotest_uibuffer_circular()
 {
-    unsigned int v[] = {1, 2, 3, 4, 5, 6, 7, 8};
-    unsigned int test1[] = {1, 2, 3, 4};
-    unsigned int test2[] = {3, 4, 1, 2, 3, 4, 5, 6, 7, 8};
-    unsigned int *r; // reader
-    unsigned int n;
-
-    uibuffer cb = uibuffer_create(CIRCULAR,10);
-
-    uibuffer_write(cb, v, 4);
-    n = 4;
-    uibuffer_read(cb, &r, &n);
-
-    CONTEND_EQUALITY(n,4);
-    CONTEND_SAME_DATA(r,test1,4*sizeof(unsigned int));
-
-    uibuffer_release(cb, 2);
-    uibuffer_write(cb, v, 8);
-    n = 10;
-    uibuffer_read(cb, &r, &n);
-    CONTEND_EQUALITY(n,10);
-    CONTEND_SAME_DATA(r,test2,10*sizeof(unsigned int));
-
-    uibuffer_destroy(cb);
+    CBUFFER_AUTOTEST_DEFINE_API(BUFFER_MANGLE_UINT, unsigned int)
 }
 
 
