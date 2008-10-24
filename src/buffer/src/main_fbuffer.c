4035de49e3781611acc995973b473dcf9a709c3d
Joseph D. Gaeddert <jgaedder@vt.edu>
2008-10-24 17:17:35 +0000

    adding fbuffer_test

--- src/buffer/src/main_fbuffer.c
+++ src/buffer/src/main_fbuffer.c
@@ -4,30 +4,30 @@
 
 #include <stdio.h>
 
-#include "cbuffer.h"
+#include "buffer.h"
 
 int main() {
     float v[] = {1, 2, 3, 4, 5, 6, 7, 8};
     float *r; // reader
     unsigned int n=3;
 
-    cbuffer cb = cbuffer_create(10);
+    fbuffer cb = fbuffer_create(CIRCULAR, 10);
 
-    cbuffer_write(cb, v, 4);
-    cbuffer_read(cb, &r, &n);
-    printf("cbuffer read %u elements\n", n);
+    fbuffer_write(cb, v, 4);
+    fbuffer_read(cb, &r, &n);
+    printf("fbuffer read %u elements\n", n);
     unsigned int i;
     for (i=0;i<n;i++)
         printf("  %u : %f\n", i, r[i]);
 
-    cbuffer_release(cb, 2);
+    fbuffer_release(cb, 2);
 
-    cbuffer_write(cb, v, 8);
+    fbuffer_write(cb, v, 8);
 
-    cbuffer_debug_print(cb);
-    cbuffer_print(cb);
+    fbuffer_debug_print(cb);
+    fbuffer_print(cb);
 
-    cbuffer_destroy(cb);
+    fbuffer_destroy(cb);
 
     printf("done.\n");
     return 0;
