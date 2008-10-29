1f17a12743a6354b5feee3dc5995a12ae3c0a5a7
Joseph D. Gaeddert <jgaedder@vt.edu>
2008-10-29 16:59:37 +0000

    adding agc autotests

---
+++ src/agc/tests/agc_autotest.h
@@ -0,0 +1,72 @@
+#ifndef __AGC_AUTOTEST_H__
+#define __AGC_AUTOTEST_H__
+
+#include "../../../autotest/autotest.h"
+#include "../src/agc.h"
+
+// 
+// Test DC gain control
+//
+void autotest_dc_gain_control() {
+    // set paramaters
+    float energy_start = 10.0f;     // initial input energy
+    float energy_target = 0.01f;    // target signal energy
+    float bt = 0.005f;              // bandwidth-time product
+    float tol = 0.001f;             // error tolerance
+
+    float g_target = 10*log10(energy_target/energy_start);
+
+    // create AGC object and initialize
+    agc g = agc_create(energy_target, bt);
+
+    unsigned int i;
+    float complex x = energy_start, y;
+    for (i=0; i<128; i++) {
+        //printf("gain(%u) = %f;\n", i+1, agc_get_gain(g));
+        //printf("  energy: %f dB\n", agc_get_signal_level(g));
+        agc_apply_gain(g, x, &y);
+    }
+
+    // Check results
+    CONTEND_DELTA( crealf(y), energy_target,    tol );
+    CONTEND_DELTA( cimagf(y), 0.0f,             tol );
+    CONTEND_DELTA( agc_get_gain(g), g_target,   tol );
+
+    agc_free(g);
+}
+
+// 
+// Test AC gain control
+//
+void autotest_ac_gain_control() {
+    // set paramaters
+    float energy_start = 10.0f;     // initial input energy
+    float energy_target = 0.01f;    // target signal energy
+    float bt = 0.01f;               // bandwidth-time product
+    float tol = 0.001f;             // error tolerance
+    float theta = 0.0f;             // NCO angle
+    float dtheta=0.1f;              // NCO frequency
+
+    float g_target = 10*log10(energy_target/energy_start);
+
+    // create AGC object and initialize
+    agc g = agc_create(energy_target, bt);
+
+    unsigned int i;
+    float complex x, y;
+    for (i=0; i<128; i++) {
+        x = (cosf(theta) + _Complex_I*sinf(theta)) * energy_start;
+        theta += dtheta;
+        agc_apply_gain(g, x, &y);
+    }
+
+    // Check results
+    CONTEND_DELTA( agc_get_gain(g), g_target, tol );
+
+    agc_free(g);
+}
+
+
+
+#endif // __AGC_AUTOTEST_H__
+
