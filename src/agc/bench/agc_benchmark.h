c4d8920395eedc3bb9d0e5a8c7b679dee0a74002
Joseph D. Gaeddert <jgaedder@vt.edu>
2008-10-29 17:06:42 +0000

    adding agc benchmarks

---
+++ src/agc/bench/agc_benchmark.h
@@ -0,0 +1,34 @@
+#ifndef __AGC_BENCHMARK_H__
+#define __AGC_BENCHMARK_H__
+
+#include <sys/resource.h>
+#include "../src/agc.h"
+
+void benchmark_agc(
+    struct rusage *_start,
+    struct rusage *_finish,
+    unsigned long int *_num_iterations)
+{
+    unsigned int i;
+
+    // initialize AGC object
+    agc g = agc_create(1.0f, 0.05f);
+
+    float complex x=1.0f, y;
+
+    getrusage(RUSAGE_SELF, _start);
+    for (i=0; i<(*_num_iterations); i++) {
+        agc_apply_gain(g, x, &y);
+        agc_apply_gain(g, x, &y);
+        agc_apply_gain(g, x, &y);
+        agc_apply_gain(g, x, &y);
+    }
+    getrusage(RUSAGE_SELF, _finish);
+
+    *_num_iterations *= 4;
+
+    agc_free(g);
+}
+
+#endif // __AGC_BENCHMARK_H__
+
