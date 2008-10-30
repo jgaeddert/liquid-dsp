d75b591bfa7c59fd7d148f51270d96a1157c1880
Joseph D. Gaeddert <jgaedder@vt.edu>
2008-10-30 13:06:14 +0000

    adding internal sequence header

---
+++ src/sequence/src/sequence_internal.h
@@ -0,0 +1,35 @@
+//
+// binary sequence
+//
+
+#ifndef __LIQUID_SEQUENCE_INTERNAL_H__
+#define __LIQUID_SEQUENCE_INTERNAL_H__
+
+#include <inttypes.h>
+#include "sequence.h"
+#include "../../utility/src/utility.h" // for count_ones
+
+struct binary_sequence_s {
+    uint8_t * s;                // sequence array, memory pointer
+    unsigned int num_bits;      // number of bits in sequence
+    unsigned int num_bits_msb;  // number of bits in most-significant block
+    uint8_t bit_mask_msb;       // bit mask for most-significant block
+    unsigned int s_len;         // length of array, number of allocated blocks
+};
+
+
+struct msequence_s {
+    unsigned int m;     // length generator polynomial, shift register
+    unsigned int g;     // generator polynomial
+    unsigned int a;     // initial shift register state, default: 1
+
+    unsigned int n;     // length of sequence, \f$ n=2^m-1 \f$
+    unsigned int v;     // shift register
+    unsigned int b;     // return bit
+};
+
+// Default msequence generator objects
+extern struct msequence_s msequence_default[13];
+
+#endif  // __LIQUID_SEQUENCE_INTERNAL_H__
+
