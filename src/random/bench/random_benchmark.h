63b778a41081ae3c559bfd77f7da986d80dd4166
Joseph D. Gaeddert <jgaedder@vt.edu>
2008-11-06 15:29:45 +0000

    random: adding weibull benchmark

--- src/random/bench/random_benchmark.h
+++ src/random/bench/random_benchmark.h
@@ -51,6 +51,26 @@ void benchmark_random_normal(
 }
 
 // 
+// BENCHMARK: Weibull
+//
+void benchmark_random_weibull(
+    struct rusage *_start,
+    struct rusage *_finish,
+    unsigned long int *_num_iterations)
+{
+    float x, alpha=1.0f, beta=2.0f, gamma=6.0f;
+    unsigned long int i;
+
+    // start trials
+    getrusage(RUSAGE_SELF, _start);
+    for (i=0; i<(*_num_iterations); i++) {
+        x = rand_weibullf(alpha,beta,gamma);
+    }
+    getrusage(RUSAGE_SELF, _finish);
+    *_num_iterations *= 4;
+}
+
+// 
 // BENCHMARK: Rice-K
 //
 void benchmark_random_ricek(
