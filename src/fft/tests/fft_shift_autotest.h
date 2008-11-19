8a3409185a8dfe71cd118a12ef470201e50fbb4f
Joseph D. Gaeddert <jgaedder@vt.edu>
2008-11-19 14:37:20 +0000

    fft: adding another fft_shift autotest

--- src/fft/tests/fft_shift_autotest.h
+++ src/fft/tests/fft_shift_autotest.h
@@ -25,5 +25,34 @@ void autotest_fft_shift_4()
     CONTEND_SAME_DATA(x,test,4*sizeof(float complex));
 }
 
+void autotest_fft_shift_8()
+{
+    float complex x[] = {
+        0 + 0*_Complex_I,
+        1 + 1*_Complex_I,
+        2 + 2*_Complex_I,
+        3 + 3*_Complex_I,
+        4 + 4*_Complex_I,
+        5 + 5*_Complex_I,
+        6 + 6*_Complex_I,
+        7 + 7*_Complex_I
+    };
+
+    float complex test[] = {
+        4 + 4*_Complex_I,
+        5 + 5*_Complex_I,
+        6 + 6*_Complex_I,
+        7 + 7*_Complex_I,
+        0 + 0*_Complex_I,
+        1 + 1*_Complex_I,
+        2 + 2*_Complex_I,
+        3 + 3*_Complex_I
+    };
+
+    fft_shift(x,8);
+
+    CONTEND_SAME_DATA(x,test,8*sizeof(float complex));
+}
+
 #endif 
 
