392a044365bf54c0bdd5dd1246de3fcd8644cd89
Joseph D. Gaeddert <jgaedder@vt.edu>
2008-12-01 19:39:53 +0000

    filter: adding example(s)

---
+++ src/filter/examples/fir_filter_example.c
@@ -0,0 +1,18 @@
+//
+//
+//
+
+#include <stdio.h>
+
+#include "../src/filter.h"
+
+int main() {
+    float h[] = {1,2,3,4,5,6,7,8};
+    fir_filter f = fir_filter_create(h,8);
+    fir_filter_print(f);
+    fir_filter_destroy(f);
+
+    printf("done.\n");
+    return 0;
+}
+
