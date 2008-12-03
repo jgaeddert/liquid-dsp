c77aaaacbc2187e03196b071f844290af7333401
Joseph D. Gaeddert <jgaedder@vt.edu>
2008-12-03 13:55:58 +0000

    moving newbuffer header to buffer project

---
+++ src/buffer/src/newbuffer.c
@@ -0,0 +1,83 @@
+//
+//
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "newbuffer.h"
+
+// create/destroy/print methods
+// NOTE _n is suggested starting size; buffer will realloc when necessary
+buffer buffer_create(unsigned int _n)
+{
+    buffer b = (buffer) malloc(sizeof(struct buffer_s));
+    b->len = _n;
+    b->N = 4*(b->len);
+
+    b->v = (float*) malloc((b->N)*sizeof(float));
+
+    return b;
+}
+
+void buffer_destroy(buffer _b)
+{
+    free(_b->v);
+    free(_b);
+}
+
+void buffer_print(buffer _b)
+{
+
+}
+
+void buffer_debug_print(buffer _b)
+{
+
+}
+
+// accessor methods
+unsigned int buffer_get_length(buffer _b)
+{
+    return _b->len;
+}
+
+// consumer methods
+//int buffer_consumer_lockn_array(buffer _b, float **_v, unsigned int _n);
+int buffer_consumer_lock_array(buffer _b, float **_v, unsigned int *_n)
+{
+    return 0;
+}
+
+int buffer_consumer_read(buffer _b, unsigned int _n)
+{
+
+    return 0;
+}
+
+int buffer_consumer_release_array(buffer _b)
+{
+
+    return 0;
+}
+
+// producer methods
+//int buffer_producer_lockn_array(buffer _b, float **_v, unsigned int _n);
+int buffer_producer_lock_array(buffer _b, unsigned int *_n)
+{
+
+    return 0;
+}
+
+int buffer_producer_write(buffer _b, float *_v, unsigned int _n)
+{
+
+    return 0;
+}
+
+int buffer_producer_release_array(buffer _b)
+{
+
+    return 0;
+}
+
