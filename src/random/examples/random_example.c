ac023f7da99e22e0730ffc90edf982b16d3a4e45
Joseph D. Gaeddert <jgaedder@vt.edu>
2008-12-07 16:36:18 +0000

    random/example: output formatting

--- src/random/examples/random_example.c
+++ src/random/examples/random_example.c
@@ -8,11 +8,11 @@
 int main() {
     unsigned long int N=100000; // number of trials
     unsigned long int i;
-    float x, y, m1, m2;
+    float x, m1, m2;
 
     printf("running tests, %lu trials\n", N);
-    printf("dist    \tmean \texp. \tvar  \texp.\n");
-    printf("-----   \t-----\t-----\t-----\t-----\n");
+    printf("dist       mean   (expected)   var    (expected)\n");
+    printf("-----      ----------------    -----------------\n");
 
     // uniform
     m1=0.0f;
@@ -24,19 +24,19 @@ int main() {
     }
     m1 /= (float) N;
     m2 = (m2 / (float)N) - m1*m1;
-    printf("UNIFORM:\t%1.3f\t%1.3f\t%1.3f\t%1.3f\n", m1, 0.5f, m2, 1/12.0f);
+    printf("uniform: %8.4f (%8.4f) %8.4f (%8.4f)\n", m1, 0.5f, m2, 1/12.0f);
 
     // gauss
     m1=0.0f;
     m2=0.0f;
     for (i=0; i<N; i++) {
-        randnf(&x,&y);
+        x = randnf();
         m1 += x;
         m2 += x*x;
     }
     m1 /= (float) N;
     m2 = (m2 / (float)N) - m1*m1;
-    printf("GAUSS:  \t%1.3f\t%1.3f\t%1.3f\t%1.3f\n", m1, 0.0f, m2, 1.0f);
+    printf("Gauss:   %8.4f (%8.4f) %8.4f (%8.4f)\n", m1, 0.0f, m2, 1.0f);
 
     // Weibull
     m1=0.0f;
@@ -53,7 +53,7 @@ int main() {
     m1 /= (float) N;
     // m2 = (b/a)^(2/b)*(gamma(1+2/b)-(gamma(1+1/b))^2)
     m2 = (m2 / (float)N) - m1*m1;
-    printf("WEIBULL:\t%1.3f\t%1.3f\t%1.3f\t%1.3f\n", m1, 1.2533f, m2, 0.42920f);
+    printf("weibull: %8.4f (%8.4f) %8.4f (%8.4f)\n", m1, 1.2533f, m2, 0.42920f);
 
     printf("done.\n");
     return 0;
