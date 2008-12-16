76d98faf5383045806c4b497c7ab925f1295960f
Joseph D. Gaeddert <jgaedder@vt.edu>
2008-12-16 14:52:57 +0000

    fixing dotprod_run4, adding more fdotprod autotests

--- src/dotprod/src/dotprod.c
+++ src/dotprod/src/dotprod.c
@@ -20,15 +20,24 @@ T X(_run)(T *_x, T *_y, unsigned int _n)
 
 T X(_run4)(T *_x, T *_y, unsigned int _n)
 {
-    // BUG: need to ensure length of _n is a multiple of 4
     T r=0;
+
+    // t = 4*(floor(_n/4))
+    unsigned int t=(_n>>2)<<2; 
+
+    // compute dotprod in groups of 4
     unsigned int i;
-    for (i=0; i<_n; i+=4) {
+    for (i=0; i<t; i+=4) {
         r += _x[i]   * _y[i];
         r += _x[i+1] * _y[i+1];
         r += _x[i+2] * _y[i+2];
         r += _x[i+3] * _y[i+3];
     }
+
+    // clean up remaining
+    for ( ; i<_n; i++)
+        r += _x[i] * _y[i];
+
     return r;
 }
 
--- src/dotprod/tests/fdotprod_autotest.h
+++ src/dotprod/tests/fdotprod_autotest.h
@@ -41,5 +41,78 @@ void autotest_fdotprod_basic()
 
 }
 
+// 
+// AUTOTEST: uneven dot product
+//
+void autotest_fdotprod_uneven()
+{
+    float tol = 1e-6;
+
+    float h[16] = {
+        1, -1, 1, -1, 1, -1, 1, -1,
+        1, -1, 1, -1, 1, -1, 1, -1};
+
+    float x[16] = {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1};
+
+    float test1 = 1;
+    CONTEND_DELTA(fdotprod_run(h,x,1),  test1, tol);
+    CONTEND_DELTA(fdotprod_run4(h,x,1), test1, tol);
+
+    float test2 = 0;
+    CONTEND_DELTA(fdotprod_run(h,x,2),  test2, tol);
+    CONTEND_DELTA(fdotprod_run4(h,x,2), test2, tol);
+
+    float test3 = 1;
+    CONTEND_DELTA(fdotprod_run(h,x,3),  test3, tol);
+    CONTEND_DELTA(fdotprod_run4(h,x,3), test3, tol);
+
+    float test11 = 1;
+    CONTEND_DELTA(fdotprod_run(h,x,11),  test11, tol);
+    CONTEND_DELTA(fdotprod_run4(h,x,11), test11, tol);
+
+    float test13 = 1;
+    CONTEND_DELTA(fdotprod_run(h,x,13),  test13, tol);
+    CONTEND_DELTA(fdotprod_run4(h,x,13), test13, tol);
+
+    float test15 = 1;
+    CONTEND_DELTA(fdotprod_run(h,x,15),  test15, tol);
+    CONTEND_DELTA(fdotprod_run4(h,x,15), test15, tol);
+
+}
+
+// 
+// AUTOTEST: structured dot product
+//
+void autotest_fdotprod_struct()
+{
+    float tol = 1e-6;
+
+    float h[16] = {
+        1, -1, 1, -1, 1, -1, 1, -1,
+        1, -1, 1, -1, 1, -1, 1, -1};
+
+    // create object
+    fdotprod dp = fdotprod_create(h,16);
+
+    float x0[16] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+    float test0 = 0;
+    CONTEND_DELTA(fdotprod_execute(dp,x0),  test0, tol);
+
+    float x1[16] = {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1};
+    float test1 = 0;
+    CONTEND_DELTA(fdotprod_execute(dp,x1),  test1, tol);
+
+    float x2[16] = {0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1};
+    float test2 = -8;
+    CONTEND_DELTA(fdotprod_execute(dp,x2),  test2, tol);
+
+    float *x3 = h;
+    float test3 = 16;
+    CONTEND_DELTA(fdotprod_execute(dp,x3),  test3, tol);
+
+    // clean-up
+    fdotprod_destroy(dp);
+}
+
 #endif 
 
