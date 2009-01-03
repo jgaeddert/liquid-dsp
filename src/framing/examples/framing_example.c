7e40917ea9501dcb70a3beabd62511dbc5a766e1
Joseph D. Gaeddert <jgaedder@vt.edu>
2009-01-03 18:23:59 +0000

    framesync: adding basic functionality; still needs work; bug: only produced fixed-length (63) sequence

--- src/framing/examples/framing_example.c
+++ src/framing/examples/framing_example.c
@@ -3,10 +3,40 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
 
 #include "../src/framing.h"
+#include "../../random/src/random.h"    // awgn
 
 int main() {
+    // options
+    unsigned int n=63;
+    float snr_db = 10.0f;
+
+    // create objects
+    float snr = powf(10.0f, snr_db/10.0f);
+    framesync fs = framesync_create(n);
+    msequence ms = msequence_create(6);
+
+    unsigned int i;
+    float s, rxy;
+
+    for (i=0; i<n; i++) {
+        // advance
+        s = msequence_advance(ms) ? 1.0f : -1.0f;
+        s += randnf() / snr;
+
+        // correlate
+        rxy = framesync_correlate(fs,s);
+
+        printf("%4u: %12.8f\n",i,rxy);
+    }
+
+    // clean it up
+    framesync_destroy(fs);
+    msequence_destroy(ms);
+
     printf("done.\n");
     return 0;
 }
--- src/framing/src/framesync.c
+++ src/framing/src/framesync.c
@@ -21,11 +21,30 @@ struct FRAMESYNC(_s) {
 
 FRAMESYNC() FRAMESYNC(_create)(unsigned int _n)
 {
-    return NULL;
+    FRAMESYNC() fs = (FRAMESYNC()) malloc(sizeof(struct FRAMESYNC(_s)));
+    //fs->n = _n;
+    fs->n = 63;
+
+    fs->sym  = WINDOW(_create)(fs->n);
+    fs->sym2 = WINDOW(_create)(fs->n);
+
+    float h[fs->n];
+    msequence ms = msequence_create(6);
+    unsigned int i;
+    for (i=0; i<fs->n; i++)
+        h[i] = msequence_advance(ms) ? 1.0f : -1.0f;
+    msequence_destroy(ms);
+
+    fs->dp = DOTPROD(_create)(h,fs->n);
+
+    return fs;
 }
 
 void FRAMESYNC(_destroy)(FRAMESYNC() _fs)
 {
+    WINDOW(_destroy)(_fs->sym);
+    WINDOW(_destroy)(_fs->sym2);
+    DOTPROD(_destroy)(_fs->dp);
     free(_fs);
 }
 
@@ -47,9 +66,10 @@ T FRAMESYNC(_correlate)(FRAMESYNC() _fs, T _sym)
     float e=0.0f;
     for (i=0; i<_fs->n; i++)
         e += r[i];
+    e /= _fs->n;
 
     WINDOW(_read)(_fs->sym, &r);
-    _fs->rxy = DOTPROD(_execute)(_fs->dp, r) / sqrtf(e);
+    _fs->rxy = DOTPROD(_execute)(_fs->dp, r) / (sqrtf(e) * _fs->n);
 
     return _fs->rxy;
 }
