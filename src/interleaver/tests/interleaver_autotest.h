c53f9061f6fb2a83d9538502c3222e7e7f64995a
Joseph D. Gaeddert <jgaedder@vt.edu>
2009-01-06 21:36:11 +0000

    interleaver: adding more tests

--- src/interleaver/tests/interleaver_autotest.h
+++ src/interleaver/tests/interleaver_autotest.h
@@ -9,42 +9,33 @@
 // 
 // AUTOTESTS: interleave/deinterleave
 //
-void autotest_interleaver_block_8()
+void intlv_test(unsigned int _n, int _type)
 {
-    unsigned int i, n = 8;
-    unsigned char x[n];
-    unsigned char y[n];
-    unsigned char z[n];
+    unsigned int i;
+    unsigned char x[_n];
+    unsigned char y[_n];
+    unsigned char z[_n];
 
-    for (i=0; i<n; i++)
+    for (i=0; i<_n; i++)
         x[i] = rand() & 0xFF;
 
-    interleaver q = interleaver_create(n, INT_BLOCK);
+    interleaver q = interleaver_create(_n, _type);
 
     interleaver_interleave(q,x,y);
     interleaver_deinterleave(q,y,z);
 
-    CONTEND_SAME_DATA(x, z, n);
+    CONTEND_SAME_DATA(x, z, _n);
 }
 
-void autotest_interleaver_sequence_8()
-{
-    unsigned int i, n = 8;
-    unsigned char x[n];
-    unsigned char y[n];
-    unsigned char z[n];
+void autotest_interleaver_block_8()         { intlv_test(8,     INT_BLOCK); }
+void autotest_interleaver_block_16()        { intlv_test(16,    INT_BLOCK); }
+void autotest_interleaver_block_64()        { intlv_test(64,    INT_BLOCK); }
+void autotest_interleaver_block_256()       { intlv_test(256,   INT_BLOCK); }
 
-    for (i=0; i<n; i++)
-        x[i] = rand() & 0xFF;
-
-    interleaver q = interleaver_create(n, INT_SEQUENCE);
-
-    interleaver_interleave(q,x,y);
-    interleaver_deinterleave(q,y,z);
-
-    CONTEND_SAME_DATA(x, z, n);
-}
-
+void autotest_interleaver_sequence_8()      { intlv_test(8,     INT_SEQUENCE); }
+void autotest_interleaver_sequence_16()     { intlv_test(16,    INT_SEQUENCE); }
+void autotest_interleaver_sequence_64()     { intlv_test(64,    INT_SEQUENCE); }
+void autotest_interleaver_sequence_256()    { intlv_test(256,   INT_SEQUENCE); }
 
 #endif 
 
