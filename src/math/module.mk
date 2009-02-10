1aa6dd51421f772834743adbdb8716f37dd7f8be
Joseph D. Gaeddert <jgaedder@vt.edu>
2009-02-10 23:31:42 +0000

    make/math: adding module.mk

---
+++ src/math/module.mk
@@ -0,0 +1,60 @@
+# 
+# Makefile for lmath module
+#
+
+# local_dir: local library directory
+# 
+local_dir	:= src/math
+local_lib	:= liblmath.a
+
+# local_s_files
+#
+# This is a list of local source files to compile into objects,
+# referenced from the src/ subdirectory under $(local_dir)
+#
+local_s_files	:= 		\
+	cfmatrix.c		\
+	fmatrix.c		\
+	lmath.c
+
+# local_h_files
+#
+# This is a list of local header files upon which the objects
+# depend.  These files are also located under the src/
+# subdirectory within $(local_dir)
+#
+local_h_files	:=
+
+# local_t_files
+#
+# This is a list of local autotest scripts (header files) which
+# are used to generate the autotest program with the 'check'
+# target.  These files are located under the tests/ subdirectory
+# within $(local_dir)
+#
+local_t_files	:= 		\
+	math_autotest.h
+
+
+# local_b_files
+#
+# This is a list of local benchmark scripts which are used to
+# generate the benchmark program with the 'bench' target.
+# These files are located under the bench/ subdirectory within
+# $(local_dir)
+#
+local_b_files	:=
+
+
+# local_e_files
+#
+# This is a list of local example files to demonstrate
+# functionality of the module.
+#
+local_e_files	:=		\
+	cfmatrix_example.c	\
+	fmatrix_example.c	\
+	kaiser_example.c
+
+include common.mk
+
