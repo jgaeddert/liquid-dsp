4de8ac4e4a902636a6c70977191aa12bc4468168
Joseph D. Gaeddert <jgaedder@vt.edu>
2009-02-10 20:07:02 +0000

    make/utility: updating to non-recursive make

--- src/utility/module.mk
+++ src/utility/module.mk
@@ -2,21 +2,80 @@
 # Makefile for utility module
 #
 
-
+# local_dir: local library directory
+# 
 local_dir	:= src/utility
 local_lib	:= libutility.a
-blah		:= src/count_bits.c src/pack_bytes.c src/msb_index.c
-local_src	:= $(addprefix $(local_dir)/, $(blah))
-#local_src	:= src/utility/src/count_bits.c
-#	src/count_bits.c 	\
-#	src/pack_bytes.c 	\
-#	src/msb_index.c
-local_objs	:= $(subst .c,.o,$(local_src))
 
+# local_s_files
+#
+# This is a list of local source files to compile into objects,
+# referenced from the src/ subdirectory under $(local_dir)
+#
+local_s_files	:= 	\
+	count_bits.c	\
+	msb_index.c	\
+	pack_bytes.c
+
+# local_h_files
+#
+# This is a list of local header files upon which the objects
+# depend.  These files are also located under the src/
+# subdirectory within $(local_dir)
+#
+local_h_files	:=
+
+# local_t_files
+#
+# This is a list of local autotest scripts (header files) which
+# are used to generate the autotest program with the 'check'
+# target.  These files are located under the tests/ subdirectory
+# within $(local_dir)
+#
+local_t_files	:= 		\
+	count_bits_autotest.h	\
+	pack_bytes_autotest.h
+
+
+# local_b_files
+#
+# This is a list of local benchmark scripts which are used to
+# generate the benchmark program with the 'bench' target.
+# These files are located under the bench/ subdirectory within
+# $(local_dir)
+#
+local_b_files	:= 
+
+
+# local_e_files
+#
+# This is a list of local example files to demonstrate
+# functionality of the module.
+#
+local_e_files	:= 
+
+
+# 
+#
+#
+local_src	:= $(addprefix $(local_dir)/src/, $(local_s_files))
+local_objs	:= $(subst .c,.o,$(local_src))
+local_bench	:= $(addprefix $(local_dir)/bench/, $(local_b_files))
+local_tests	:= $(addprefix $(local_dir)/tests/, $(local_t_files))
+local_examples	:= $(addprefix $(local_dir)/examples/, $(local_e_files))
+
+# accumulated targets
+#
+# This is a list of target accumulated by the project-level
+# makefile.
+#
 libraries	+= $(local_lib)
 sources		+= $(local_src)
+autotests	+= $(local_tests)
+benchmarks	+= $(local_bench)
+examples	+= $(local_examples)
 
-#include ../../common.mk
-
+# Build the local library and local object files
 $(local_lib): $(local_objs)
-	ar r $@ $^
+	$(AR) $@ $^
+
