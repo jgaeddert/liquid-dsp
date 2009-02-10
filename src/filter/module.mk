f62c85b1d65970044cfe7b7e8c73fbda90e60ba0
Joseph D. Gaeddert <jgaedder@vt.edu>
2009-02-10 23:18:31 +0000

    adding common makefile for simplification

--- src/filter/module.mk
+++ src/filter/module.mk
@@ -68,28 +68,5 @@ local_b_files	:=		\
 #
 local_e_files	:= modem_example.c
 
-
-# 
-#
-#
-local_src	:= $(addprefix $(local_dir)/src/, $(local_s_files))
-local_objs	:= $(patsubst %.c,%.o,$(local_src))
-local_bench	:= $(addprefix $(local_dir)/bench/, $(local_b_files))
-local_tests	:= $(addprefix $(local_dir)/tests/, $(local_t_files))
-local_examples	:= $(addprefix $(local_dir)/examples/, $(local_e_files))
-
-# accumulated targets
-#
-# This is a list of target accumulated by the project-level
-# makefile.
-#
-libraries	+= $(local_lib)
-sources		+= $(local_src)
-autotests	+= $(local_tests)
-benchmarks	+= $(local_bench)
-examples	+= $(local_examples)
-
-# Build the local library and local object files
-$(local_lib): $(local_objs)
-	$(AR) $(ARFLAGS) $@ $^
+include common.mk
 
