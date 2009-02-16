6119cb2b766e4b3ee2b3687359976ac6ff84be6c
Joseph D. Gaeddert <jgaedder@vt.edu>
2009-02-16 15:02:05 +0000

    framesync: adding benchmarks

---
+++ src/framing/bench/framesync_rrrf_benchmark.h
@@ -0,0 +1,46 @@
+#ifndef __LIQUID_FRAMESYNC_RRRF_BENCH_H__
+#define __LIQUID_FRAMESYNC_RRRF_BENCH_H__
+
+#include <sys/resource.h>
+#include "liquid.h"
+
+#define FRAMESYNC_RRRF_BENCH_API(N) \
+(   struct rusage *_start, \
+    struct rusage *_finish, \
+    unsigned long int *_num_iterations) \
+{ framesync_rrrf_bench(_start, _finish, _num_iterations, N); }
+
+// Helper function to keep code base small
+void framesync_rrrf_bench(
+    struct rusage *_start,
+    struct rusage *_finish,
+    unsigned long int *_num_iterations,
+    unsigned int _n)
+{
+    // initialize modulator
+    float h[_n];
+    unsigned long int i;
+    for (i=0; i<_n; i++)
+        h[_n] = (i%2) ? 1.0f : -1.0f;
+    framesync_rrrf fs = framesync_rrrf_create(_n,h);
+    float rxy;
+
+    // start trials
+    getrusage(RUSAGE_SELF, _start);
+    for (i=0; i<(*_num_iterations); i++) {
+        rxy = framesync_rrrf_correlate(fs,1.0f);
+        rxy = framesync_rrrf_correlate(fs,1.0f);
+        rxy = framesync_rrrf_correlate(fs,1.0f);
+        rxy = framesync_rrrf_correlate(fs,1.0f);
+    }
+    getrusage(RUSAGE_SELF, _finish);
+    *_num_iterations *= 4;
+
+    framesync_rrrf_destroy(fs);
+}
+
+// specific modems
+void benchmark_framesync_rrrf_4     FRAMESYNC_RRRF_BENCH_API(4)
+
+#endif // __LIQUID_FRAMESYNC_RRRF_BENCH_H__
+
