dd65bfbd6c44a89fa53a754c655f889608e28831
Joseph D. Gaeddert <jgaedder@vt.edu>
2009-02-21 16:22:18 +0000

    framegen64_example: flushing end of frame

--- examples/framegen64_example.c
+++ examples/framegen64_example.c
@@ -15,16 +15,19 @@ int main() {
     framegen64 f = framegen64_create(m,beta);
 
     // data payload
+    unsigned char header[24];
     unsigned char payload[64];
 
     unsigned int i;
     // initialize data
+    for (i=0; i<24; i++)
+        header[i] = rand() & 0xff;
     for (i=0; i<64; i++)
         payload[i] = rand() & 0xff;
 
     // generate the frame
     float complex frame[2048];
-    framegen64_execute(f, payload, frame);
+    framegen64_execute(f, header, payload, frame);
 
     // write frame to output file
     FILE* fid = fopen(DEBUG_FILENAME, "w");
@@ -35,8 +38,11 @@ int main() {
     fprintf(fid,"\n\n");
     for (i=0; i<2048; i++)
         fprintf(fid, "frame(%4u) = %12.4e + j*%12.4e;\n", i+1, crealf(frame[i]), cimagf(frame[i]));
+    framegen64_flush(f, 64, frame);
+    for (i=0; i<64; i++)
+        fprintf(fid, "frame(%4u) = %12.4e + j*%12.4e;\n", i+1+2048, crealf(frame[i]), cimagf(frame[i]));
 
-    fprintf(fid,"t=0:2047;\n");
+    fprintf(fid,"t=0:(2047+64);\n");
     fprintf(fid,"plot(t,real(frame),t,imag(frame));\n");
     fclose(fid);
     printf("results written to %s\n", DEBUG_FILENAME);
