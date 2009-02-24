e3d3b9f0bce6338f8e4058187ddc1db8ef77ca6b
Joseph D. Gaeddert <jgaedder@vt.edu>
2009-02-24 16:33:16 +0000

    estimation/autotest: adding more tests

--- src/estimation/tests/estimation_autotest.h
+++ src/estimation/tests/estimation_autotest.h
@@ -4,29 +4,30 @@
 #include "autotest/autotest.h"
 #include "liquid.h"
 
-//
-// AUTOTEST: frequency offset
-//
-void autotest_freqoffset_f0_n16()
+// helper function to keep code base small
+void test_freqoffset(float _dphi, unsigned int _n)
 {
     float tol=1e-3f;
-    float dphi = 0.0f;
-    unsigned int n=16;
 
     float theta=0.0f;
     unsigned int i;
-    float complex x[n];
-    for (i=0; i<n; i++) {
+    float complex x[_n];
+    for (i=0; i<_n; i++) {
         x[i] = cexpf(_Complex_I*theta);
-        theta += dphi;
+        theta += _dphi;
     }
 
-    float nu_hat = estimate_freqoffset(x,n);
+    float nu_hat = estimate_freqoffset(x,_n);
     if (_autotest_verbose)
-        printf("nu_hat: %f (expected %f)\n", nu_hat, dphi);
+        printf("nu_hat: %f (expected %f)\n", nu_hat, _dphi);
 
-    CONTEND_DELTA( nu_hat, dphi, tol );
+    CONTEND_DELTA( nu_hat, _dphi, tol );
 }
+
+void autotest_freqoffset_f0p0_n16() { test_freqoffset(0.0f, 16  ); }
+void autotest_freqoffset_f0p1_n16() { test_freqoffset(0.1f, 16  ); }
+void autotest_freqoffset_f1p3_n16() { test_freqoffset(1.3f, 16  ); }
+void autotest_freqoffset_f2p7_n16() { test_freqoffset(2.7f, 16  ); }
 
 #endif // __LIQUID_ESTIMATION_AUTOTEST_H__
 
