84b926a3f0870a8c36980a745a3ebb26eae092c4
Joseph D. Gaeddert <jgaedder@vt.edu>
2009-03-02 03:54:37 +0000

    fbasc: adding rudimentary functionality; needs more thought

--- src/audio/src/fbasc.c
+++ src/audio/src/fbasc.c
@@ -7,9 +7,25 @@
 
 #include "liquid.internal.h"
 
+//  description         value   units
+//  -----------         -----   -----
+//  sample rate         16      kHz
+//  frame length        64      ms
+//  samples/frame       1024    samp.
+//
+//  num. channels       32      -
+//  samp./ch./frame     32      samp.
+//
+//  av. bit rate        16      kbps
+//  num. used channels  16      ch.
+//  bits/u. ch./frame   ?       bits
+//
+
 struct fbasc_s {
-    unsigned int num_channels;
-    unsigned int frame_length;
+    unsigned int num_channels;          // 32
+    unsigned int frame_len;             // 1024 real samples
+    unsigned int ht_len;                // 512  complex samples
+    unsigned int samples_per_channel;   // 32
 
     firhilb ht_interp;
     firhilb ht_decim;
@@ -19,6 +35,8 @@ struct fbasc_s {
 
     // analysis buffers
     float complex * x;
+    float complex * ht_decim_out;
+    float * energy;
 };
 
 fbasc fbasc_create()
@@ -30,13 +48,19 @@ fbasc fbasc_create()
     q->ht_decim     = firhilb_create(37);
 
     // create polyphase filterbank channelizers
-    q->num_channels = 64;
+    q->num_channels = 32;
     q->analyzer     = firpfbch_create(q->num_channels, 60.0f, FIRPFBCH_NYQUIST, FIRPFBCH_ANALYZER);
     q->synthesizer  = firpfbch_create(q->num_channels, 60.0f, FIRPFBCH_NYQUIST, FIRPFBCH_SYNTHESIZER);
 
+    // compute lengths
+    q->frame_len = 1024;  // needs to be (even) integer multiple of q->num_channels
+    q->ht_len = (q->frame_len)/2;
+    q->samples_per_channel = (q->ht_len) / (q->num_channels);
+
     // allocate memory for buffers
-    q->frame_length = 64;
-    q->x = (float complex*) malloc( (q->frame_length)*sizeof(float complex) );
+    q->x = (float complex*) malloc( (q->frame_len)*sizeof(float complex) );
+    q->ht_decim_out = (float complex*) malloc( (q->ht_len)*sizeof(float complex) );
+    q->energy = (float*) malloc( (q->num_channels)*sizeof(float) );
 
     return q;
 }
@@ -51,8 +75,10 @@ void fbasc_destroy(fbasc _q)
     firpfbch_destroy(_q->analyzer);
     firpfbch_destroy(_q->synthesizer);
 
-    // free buffer
+    // free buffers
     free(_q->x);
+    free(_q->ht_decim_out);
+    free(_q->energy);
 
     // free memory structure
     free(_q);
@@ -65,6 +91,15 @@ void fbasc_print(fbasc _q)
 
 void fbasc_encode(fbasc _q, float * _audio, unsigned char * _frame)
 {
+    unsigned int i;
+
+    // push samples through Hilbert transform (decimator)
+    for (i=0; i<_q->ht_len; i++)
+        firhilb_decim_execute(_q->ht_decim, &_audio[2*i], &(_q->ht_decim_out[i]));
+
+    // push samples into filterbank channelizer
+    for (i=0; i<_q->samples_per_channel; i+=_q->num_channels)
+        firpfbch_execute(_q->analyzer, &_q->ht_decim_out[i], &_q->x[i]);
 
 }
 
