cfae8a0e8ec4fe6bb392f196af0c4cb0d8b431f2
Joseph D. Gaeddert <jgaedder@vt.edu>
2009-03-02 03:09:54 +0000

    fbasc: adding data structures to object

--- src/audio/src/fbasc.c
+++ src/audio/src/fbasc.c
@@ -9,16 +9,52 @@
 
 struct fbasc_s {
     unsigned int num_channels;
+    unsigned int frame_length;
+
+    firhilb ht_interp;
+    firhilb ht_decim;
+
+    firpfbch analyzer;
+    firpfbch synthesizer;
+
+    // analysis buffers
+    float complex * x;
 };
 
 fbasc fbasc_create()
 {
     fbasc q = (fbasc) malloc(sizeof(struct fbasc_s));
+
+    // create Hilbert transforms
+    q->ht_interp    = firhilb_create(37);
+    q->ht_decim     = firhilb_create(37);
+
+    // create polyphase filterbank channelizers
+    q->num_channels = 64;
+    q->analyzer     = firpfbch_create(q->num_channels, 60.0f, FIRPFBCH_NYQUIST, FIRPFBCH_ANALYZER);
+    q->synthesizer  = firpfbch_create(q->num_channels, 60.0f, FIRPFBCH_NYQUIST, FIRPFBCH_SYNTHESIZER);
+
+    // allocate memory for buffers
+    q->frame_length = 64;
+    q->x = (float complex*) malloc( (q->frame_length)*sizeof(float complex) );
+
     return q;
 }
 
 void fbasc_destroy(fbasc _q)
 {
+    // destroy Hilbert transforms
+    firhilb_destroy(_q->ht_interp);
+    firhilb_destroy(_q->ht_decim);
+
+    // destroy polyphase filterbank channelizers
+    firpfbch_destroy(_q->analyzer);
+    firpfbch_destroy(_q->synthesizer);
+
+    // free buffer
+    free(_q->x);
+
+    // free memory structure
     free(_q);
 }
 
