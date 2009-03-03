d9fe9f5c327917158788c0a6a9e70a5d0231aade
Joseph D. Gaeddert <jgaedder@vt.edu>
2009-03-03 03:40:57 +0000

    fixing typo

--- examples/dotprod_cccf_example.c
+++ examples/dotprod_cccf_example.c
@@ -14,7 +14,7 @@ int main() {
     printf("fdotprod:  %8.2f + j%8.2f\n", crealf(z), cimagf(z));
 
     float complex z4;
-    dotprod_cccf_run4(x,y,5,&z);
+    dotprod_cccf_run4(x,y,5,&z4);
     printf("fdotprod:  %8.2f + j%8.2f\n", crealf(z4), cimagf(z4));
     return 0;
 }
