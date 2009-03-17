7502a38d6d87302932b283b1b7a76a7293332f5c
Joseph D. Gaeddert <jgaedder@vt.edu>
2009-03-17 13:11:00 +0000

    improving filterbanch channelizer analysis example

--- examples/firpfbch_analysis_example.c
+++ examples/firpfbch_analysis_example.c
@@ -1,5 +1,8 @@
 //
-//
+// Example of an analysis channelizer filterbank.
+// Input signal is a frequency-modulated sweep over the entire band.
+// Each filter is illuminated as the carrier passes through its
+// bandwidth.
 //
 
 #include <stdio.h>
@@ -36,6 +39,9 @@ int main() {
     nco nco_tx = nco_create();
     nco_set_frequency(nco_tx, 0.0f);
     float df = 2*M_PI/(num_channels*num_frames);
+    printf("fr/ch:");
+    for (j=0; j<num_channels; j++)  printf("%3u",j);
+    printf("\n");
     for (i=0; i<num_frames; i++) {
 
         // generate frame of data
@@ -47,6 +53,15 @@ int main() {
 
         // execute analysis filter bank
         firpfbch_execute(c, x, y);
+
+        printf("%4u : ", i);
+        for (j=0; j<num_channels; j++) {
+            if (cabsf(y[j]) > num_channels / 4)
+                printf(" x ");
+            else
+                printf(" . ");
+        }
+        printf("\n");
 
         // write output to file
         for (j=0; j<num_channels; j++) {
@@ -61,19 +76,24 @@ int main() {
 
     // plot results
     fprintf(fid,"\n\n");
+    fprintf(fid,"figure;\n");
+    fprintf(fid,"subplot(2,1,1);\n");
+    fprintf(fid,"  plot(1:length(x),real(x),1:length(x),imag(x));\n");
+    fprintf(fid,"  xlabel('time');\n");
+    fprintf(fid,"  ylabel('signal');\n");
+    fprintf(fid,"subplot(2,1,2);\n");
+    fprintf(fid,"  plot(20*log10(abs(y.')/num_channels));\n");
+    fprintf(fid,"  xlabel('time (decimated)');\n");
+    fprintf(fid,"  ylabel('channelized energy [dB]');\n");
+
     fprintf(fid,"n=min(num_channels,8);\n");
     fprintf(fid,"figure;\n");
-    /*
     fprintf(fid,"for i=1:n\n");
     fprintf(fid,"  subplot(n,1,i);\n");
-    fprintf(fid,"  plot(abs(y(i,:)));\n");
+    fprintf(fid,"  plot(1:num_frames,real(y(i,:)),1:num_frames,imag(y(i,:)));\n");
     fprintf(fid,"  axis off;\n");
     fprintf(fid,"  ylabel(num2str(i));\n");
     fprintf(fid,"end;\n");
-    */
-    fprintf(fid,"plot(abs(y.'));\n");
-    fprintf(fid,"xlabel('time');\n");
-    fprintf(fid,"ylabel('channelized energy');\n");
 
 
     fclose(fid);
