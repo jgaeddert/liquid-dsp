2e5c37165e919a128765d7f9eb19b78145b2a389
Joseph D. Gaeddert <jgaedder@vt.edu>
2009-03-25 14:36:15 +0000

    filter/symsync: adding benchmark

--- src/filter/module.mk
+++ src/filter/module.mk
@@ -57,7 +57,8 @@ local_b_files	:=			\
 	firhilb_benchmark.h		\
 	iir_filter_benchmark.h		\
 	interp_benchmark.h		\
-	resamp2_benchmark.h
+	resamp2_benchmark.h		\
+	symsync_benchmark.h
 
 
 include common.mk
