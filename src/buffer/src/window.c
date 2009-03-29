ac2f0653eb3844a628d14727256b27d0f89a2009
Joseph D. Gaeddert <jgaedder@vt.edu>
2009-03-29 21:18:47 +0000

    buffer/window: cleaning up code, comments

--- src/buffer/src/window.c
+++ src/buffer/src/window.c
@@ -9,15 +9,14 @@
 #include <stdlib.h>
 
 struct WINDOW(_s) {
-    T * v;                      // allocated array
+    T * v;                      // allocated array pointer
     unsigned int len;           // length of window
-    unsigned int m;
-    unsigned int n;
-    unsigned int mask;
+    unsigned int m;             // floor(log2(len)) + 1
+    unsigned int n;             // 2^m
+    unsigned int mask;          // n-1
     unsigned int N;             // number of elements allocated
                                 // in memory
     unsigned int read_index;
-    //T * v_excess;               // excess memory pointer
 };
 
 WINDOW() WINDOW(_create)(unsigned int _n)
@@ -25,23 +24,18 @@ WINDOW() WINDOW(_create)(unsigned int _n)
     WINDOW() w = (WINDOW()) malloc(sizeof(struct WINDOW(_s)));
     w->len = _n;
 
-    w->m = msb_index(_n)-1;
-    if ( w->len != (1<<(w->m)) )
-        w->m++;
+    w->m = msb_index(_n);   // effectively floor(log2(len))+1
+    w->n = 1<<(w->m);       // 
+    w->mask = w->n - 1;     // bit mask
 
-    w->n = 1<<(w->m);
-    w->mask = w->n - 1;
-
-    printf("len=%u\n",w->len);
-    printf("m=%u\n",w->m);
-    printf("n=%u\n",w->n);
-    printf("mask=%u\n",w->mask);
-
+    // number of elements to allocate to memory
     w->N = w->n + w->len - 1;
 
+    // allocte memory
     w->v = (T*) malloc((w->N)*sizeof(T));
     w->read_index = 0;
 
+    // clear window
     WINDOW(_clear)(w);
 
     return w;
@@ -94,24 +88,23 @@ void WINDOW(_clear)(WINDOW() _w)
 
 void WINDOW(_read)(WINDOW() _w, T ** _v)
 {
-    /*
-    if (_w->read_index > 0) {
-        //
-        WINDOW(_linearize)(_w);
-    }
-    */
+    // return pointer to buffer
     *_v = _w->v + _w->read_index;
 }
 
 void WINDOW(_push)(WINDOW() _w, T _v)
 {
-    //_w->v[_w->read_index] = _v;
-    //_w->read_index = (_w->read_index+1) % _w->len;
+    // increment index
+    _w->read_index++;
 
-    _w->read_index = (_w->read_index + 1) & _w->mask;
-    if (_w->read_index == 0) {
+    // wrap around pointer
+    _w->read_index &= _w->mask;
+
+    // if pointer wraps around, copy excess memory
+    if (_w->read_index == 0)
         memmove(_w->v, _w->v + _w->n, (_w->len)*sizeof(T));
-    }
+
+    // append value to end of buffer
     _w->v[_w->read_index + _w->len - 1] = _v;
 }
 
@@ -121,13 +114,5 @@ void WINDOW(_write)(WINDOW() _w, T * _v, unsigned int _n)
     unsigned int i;
     for (i=0; i<_n; i++)
         WINDOW(_push)(_w, _v[i]);
-}
-
-void WINDOW(_linearize)(WINDOW() _w)
-{
-    // TODO check to see if anything needs to be done
-
-    // perform memory copy
-    //memcpy(_w->v + _w->len, _w->v, (_w->read_index)*sizeof(T));
 }
 
