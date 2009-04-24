7336184e8a366d42d6a3d19edba3c7eab8b96d55
Joseph D. Gaeddert <jgaedder@vt.edu>
2009-04-24 15:53:39 +0000

    fir_filter: adding macro to disable use of dotprod() interface; is actually slower

--- src/filter/src/fir_filter.c
+++ src/filter/src/fir_filter.c
@@ -8,6 +8,8 @@
 
 #include "filter_internal.h"
 
+#define FIR_FILTER_USE_DOTPROD 1
+
 // defined:
 //  FIR_FILTER()    name-mangling macro
 //  T               coefficients type
@@ -19,7 +21,12 @@ struct FIR_FILTER(_s) {
     TC * h;
     unsigned int h_len;
 
+#if FIR_FILTER_USE_DOTPROD
     WINDOW() w;
+#else
+    TI * v;
+    unsigned int i;
+#endif
 
     fir_prototype p;
 };
@@ -35,8 +42,15 @@ FIR_FILTER() FIR_FILTER(_create)(TC * _h, unsigned int _n)
     for (i=_n; i>0; i--)
         f->h[i-1] = _h[_n-i];
 
+#if FIR_FILTER_USE_DOTPROD
     f->w = WINDOW(_create)(f->h_len);
     WINDOW(_clear)(f->w);
+#else
+    f->v = malloc((f->h_len)*sizeof(TI));
+    for (i=0; i<f->h_len; i++)
+        f->v[i] = 0;
+    f->i = 0;
+#endif
 
     return f;
 }
@@ -55,7 +69,11 @@ FIR_FILTER() FIR_FILTER(_create_prototype)(unsigned int _n)
 
 void FIR_FILTER(_destroy)(FIR_FILTER() _f)
 {
+#if FIR_FILTER_USE_DOTPROD
     WINDOW(_destroy)(_f->w);
+#else
+    free(_f->v);
+#endif
     free(_f->h);
     free(_f);
 }
@@ -73,14 +91,28 @@ void FIR_FILTER(_print)(FIR_FILTER() _f)
 
 void FIR_FILTER(_push)(FIR_FILTER() _f, TI _x)
 {
+#if FIR_FILTER_USE_DOTPROD
     WINDOW(_push)(_f->w, _x);
+#else
+    _f->v[ _f->i ] = _x;
+    (_f->i)++;
+    _f->i = (_f->i) % (_f->h_len);
+#endif
 }
 
 void FIR_FILTER(_execute)(FIR_FILTER() _f, TO *_y)
 {
+#if FIR_FILTER_USE_DOTPROD
     TI *r;
     WINDOW(_read)(_f->w, &r);
     DOTPROD(_run)(_f->h, r, _f->h_len, _y);
+#else
+    TO y = 0;
+    unsigned int i;
+    for (i=0; i<_f->h_len; i++)
+        y += _f->v[ (i+_f->i)%(_f->h_len) ] * _f->h[i];
+    *_y = y;
+#endif
 }
 
 unsigned int FIR_FILTER(_get_length)(FIR_FILTER() _f)
