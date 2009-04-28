4edc7627191ba2aab996da14fb39f3b85ca93486
Joseph D. Gaeddert <jgaedder@vt.edu>
2009-04-28 17:40:55 +0000

    fft/example: cleaning up code

--- examples/fft_example.c
+++ examples/fft_example.c
@@ -7,13 +7,12 @@
 
 int main() {
     unsigned int n=16;
-    float complex x[n];
-    float complex y[n];
-    float complex z[n];
+    float complex x[n]; // time-domain 'signal'
+    float complex y[n]; // fft(x)
+    float complex z[n]; // ifft(y)
 
     unsigned int i;
     for (i=0; i<n; i++)
-        //x[i] = (float)i;
         x[i] = (float)i - _Complex_I*(float)i;
 
     fftplan pf = fft_create_plan(n, x, y, FFT_FORWARD);
@@ -28,11 +27,15 @@ int main() {
         z[i] /= (float) n;
 
     // print results
+    printf("original signal, x[n]:\n");
     for (i=0; i<n; i++)
-        printf("y[%u] =\t%8.2f +\tj%8.2f\n", i, crealf(y[i]), cimagf(y[i]));
-    printf("==========\n");
+        printf("  x[%3u] = %8.4f + j%8.4f\n", i, crealf(x[i]), cimagf(x[i]));
+    printf("y[n] = fft( x[n] ):\n");
     for (i=0; i<n; i++)
-        printf("z[%u] =\t%8.2f +\tj%8.2f\n", i, crealf(z[i]), cimagf(z[i]));
+        printf("  y[%3u] = %8.4f + j%8.4f\n", i, crealf(y[i]), cimagf(y[i]));
+    printf("z[n] = ifft( y[n] ):\n");
+    for (i=0; i<n; i++)
+        printf("  z[%3u] = %8.4f + j%8.4f\n", i, crealf(z[i]), cimagf(z[i]));
 
     fft_destroy_plan(pf);
     fft_destroy_plan(pr);
