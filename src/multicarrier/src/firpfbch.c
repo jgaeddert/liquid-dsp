0410b32838467fb696aaab0d2b8277d975ccd0b7
Joseph D. Gaeddert <jgaedder@vt.edu>
2009-04-29 16:40:08 +0000

    build: checking for fftw3 library; using it if available

--- src/multicarrier/src/firpfbch.c
+++ src/multicarrier/src/firpfbch.c
@@ -8,6 +8,10 @@
 
 #include "liquid.internal.h"
 
+#if HAVE_FFTW3_H
+#   include <fftw3.h>
+#endif
+
 //#define DEBUG
 
 #define FIR_FILTER(name)    LIQUID_CONCAT(fir_filter_crcf,name)
@@ -18,7 +22,11 @@ struct firpfbch_s {
     float complex * X;  // freq-domain buffer
     
     FIR_FILTER() * bank;
+#if HAVE_FFTW3_H
+    fftwf_plan fft;
+#else
     fftplan fft;
+#endif
     unsigned int type;  // synthesis/analysis
 };
 
@@ -65,9 +73,17 @@ firpfbch firpfbch_create(unsigned int _num_channels, float _slsl, int _nyquist, 
     // create fft plan
     c->type = _type;
     if (c->type == FIRPFBCH_ANALYZER) {
+#if HAVE_FFTW3_H
+        c->fft = fftwf_plan_dft_1d(c->num_channels, c->X, c->x, FFTW_FORWARD, FFTW_ESTIMATE);
+#else
         c->fft = fft_create_plan(c->num_channels, c->X, c->x, FFT_FORWARD);
+#endif
     } else if (c->type == FIRPFBCH_SYNTHESIZER) {
+#if HAVE_FFTW3_H
+        c->fft = fftwf_plan_dft_1d(c->num_channels, c->x, c->X, FFTW_BACKWARD, FFTW_ESTIMATE);
+#else
         c->fft = fft_create_plan(c->num_channels, c->x, c->X, FFT_REVERSE);
+#endif
     } else {
         printf("error: firpfbch_create(), unknown channelizer type\n");
         exit(0);
@@ -83,7 +99,11 @@ void firpfbch_destroy(firpfbch _c)
         fir_filter_crcf_destroy(_c->bank[i]);
     free(_c->bank);
 
+#if HAVE_FFTW3_H
+    fftwf_destroy_plan(_c->fft);
+#else
     fft_destroy_plan(_c->fft);
+#endif
     free(_c->x);
     free(_c->X);
     free(_c);
@@ -103,7 +123,11 @@ void firpfbch_synthesizer_execute(firpfbch _c, float complex * _x, float complex
     memmove(_c->x, _x, (_c->num_channels)*sizeof(float complex));
 
     // execute fft, store in freq-domain buffer (_c->X)
+#if HAVE_FFTW3_H
+    fftwf_execute(_c->fft);
+#else
     fft_execute(_c->fft);
+#endif
 
     // push samples into filter bank
     // execute filterbank, putting samples into output buffer
@@ -127,7 +151,11 @@ void firpfbch_analyzer_execute(firpfbch _c, float complex * _X, float complex * 
     }
     
     // execute inverse fft, store in time-domain buffer (_c->x)
+#if HAVE_FFTW3_H
+    fftwf_execute(_c->fft);
+#else
     fft_execute(_c->fft);
+#endif
 
     // copy results to output buffer
     memmove(_x, _c->x, (_c->num_channels)*sizeof(float complex));
