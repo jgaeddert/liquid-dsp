8891970193010d60a06d0bb93a108c2c056bf76d
Joseph D. Gaeddert <jgaedder@vt.edu>
2009-05-06 15:40:03 +0000

    firpfbch/autotest: adding test with pure noise input; found bug in analyzer

--- src/multicarrier/tests/firpfbch_analyzer_autotest.h
+++ src/multicarrier/tests/firpfbch_analyzer_autotest.h
@@ -98,6 +98,102 @@ void autotest_firpfbch_analysis() {
     firpfbch_destroy(c);
 }
 
+
+//
+// AUTOTEST: validate analysis correctness
+//
+void autotest_firpfbch_analysis_noise() {
+    unsigned int num_channels = 4;  // number of channels
+    unsigned int m=2;               // filter delay
+    float beta=-40.0f;              // excess bandwidth factor
+    unsigned int num_symbols = 8;   // number of symbols per channel
+    float tol=0.05f;                // error tolerance
+
+    unsigned int i;
+
+    float complex x[32] = {
+        -5.937e-02+ 8.925e-02*_Complex_I, -1.288e-01+ 2.123e-02*_Complex_I, 
+        -1.655e-02+ 1.321e-02*_Complex_I, -1.451e-01+ 3.243e-02*_Complex_I, 
+         1.801e-01+-1.064e-02*_Complex_I,  3.832e-02+ 1.462e-01*_Complex_I, 
+         3.041e-02+-1.010e-01*_Complex_I, -1.765e-01+ 6.912e-02*_Complex_I, 
+         2.375e-01+ 4.779e-02*_Complex_I, -3.149e-02+-5.068e-02*_Complex_I, 
+         1.492e-01+-5.717e-02*_Complex_I,  1.615e-03+-3.996e-02*_Complex_I, 
+         4.525e-02+ 2.823e-01*_Complex_I, -3.987e-02+ 3.753e-03*_Complex_I, 
+         7.667e-02+-1.671e-01*_Complex_I,  2.917e-02+-4.288e-02*_Complex_I, 
+         1.128e-02+ 1.703e-01*_Complex_I, -5.305e-02+ 1.134e-01*_Complex_I, 
+        -2.110e-01+ 1.215e-01*_Complex_I,  8.129e-02+ 5.713e-03*_Complex_I, 
+        -4.876e-02+ 8.990e-04*_Complex_I, -5.468e-02+-9.310e-02*_Complex_I, 
+        -1.016e-02+-1.011e-01*_Complex_I, -7.739e-02+ 2.040e-01*_Complex_I, 
+         2.625e-02+-1.820e-01*_Complex_I,  1.761e-02+-5.792e-03*_Complex_I, 
+         5.142e-02+-2.764e-02*_Complex_I,  8.508e-02+-1.741e-01*_Complex_I, 
+        -5.252e-02+ 2.439e-02*_Complex_I,  1.067e-01+ 2.441e-01*_Complex_I, 
+        -4.582e-03+ 1.308e-01*_Complex_I,  5.419e-03+-6.557e-02*_Complex_I
+        };
+
+    float complex y_test[32] = {
+         0.000e+00+ 0.000e+00*_Complex_I,  0.000e+00+ 0.000e+00*_Complex_I, 
+        -0.000e+00+ 0.000e+00*_Complex_I,  0.000e+00+-0.000e+00*_Complex_I, 
+         2.124e-02+-4.859e-03*_Complex_I, -2.506e-03+-6.411e-03*_Complex_I, 
+        -1.805e-02+ 2.319e-03*_Complex_I, -6.752e-04+ 8.951e-03*_Complex_I, 
+        -2.188e-01+ 1.152e-01*_Complex_I, -4.877e-02+ 1.605e-01*_Complex_I, 
+         7.475e-02+ 9.823e-02*_Complex_I, -4.468e-02+-1.690e-02*_Complex_I, 
+         6.507e-03+ 1.306e-01*_Complex_I,  2.785e-01+-1.418e-01*_Complex_I, 
+         3.413e-01+-2.441e-01*_Complex_I,  9.413e-02+ 2.127e-01*_Complex_I, 
+         1.875e-01+ 9.516e-03*_Complex_I,  7.891e-02+-6.548e-03*_Complex_I, 
+         4.872e-01+-7.034e-02*_Complex_I,  1.964e-01+ 2.585e-01*_Complex_I, 
+         1.805e-01+ 6.749e-02*_Complex_I, -7.878e-02+ 4.793e-01*_Complex_I, 
+         2.104e-01+ 2.295e-01*_Complex_I, -1.311e-01+ 3.530e-01*_Complex_I, 
+        -8.165e-02+ 2.312e-01*_Complex_I,  2.573e-01+ 2.808e-01*_Complex_I, 
+        -8.047e-02+ 8.628e-02*_Complex_I, -5.003e-02+ 8.308e-02*_Complex_I, 
+        -2.071e-01+ 4.612e-02*_Complex_I, -2.758e-02+ 7.776e-02*_Complex_I, 
+        -1.751e-01+ 1.707e-02*_Complex_I,  2.147e-01+-1.373e-01*_Complex_I
+        };
+
+
+    // create channelizer
+    firpfbch c = firpfbch_create(num_channels,
+                                 m,
+                                 beta,
+                                 FIRPFBCH_NYQUIST,
+                                 FIRPFBCH_ANALYZER);
+
+    float complex y[32];
+
+    unsigned int n=0;
+    for (i=0; i<num_symbols; i++) {
+        firpfbch_execute(c, &x[n], &y[n]);
+        n += num_channels;
+    }
+
+//#if 0
+    // print formatted results (octave)
+    printf("y=zeros(%u,%u);\n", num_channels, num_symbols);
+    printf("y_test=zeros(%u,%u);\n", num_channels, num_symbols);
+    unsigned int j;
+    n=0;
+    for (j=0; j<num_symbols; j++) {
+        for (i=0; i<num_channels; i++) {
+            printf("     y(%2u,%2u) = %10.3e + j*%10.3e;\n", i+1, j+1, crealf(y[n]),      cimagf(y[n]));
+            printf("y_test(%2u,%2u) = %10.3e + j*%10.3e;\n", i+1, j+1, crealf(y_test[n]), cimagf(y_test[n]));
+            n++;
+        }
+    }
+    // plot results
+    printf("for i=1:4,\n");
+    printf("    figure;\n");
+    printf("    subplot(2,1,1); plot(1:8,real(y(i,:)),1:8,real(y_test(i,:)));\n");
+    printf("    subplot(2,1,2); plot(1:8,imag(y(i,:)),1:8,imag(y_test(i,:)));\n");
+    printf("end;\n");
+//#endif
+
+    for (i=0; i<32; i++) {
+        CONTEND_DELTA(crealf(y[i]), crealf(y_test[i]), tol);
+        CONTEND_DELTA(cimagf(y[i]), cimagf(y_test[i]), tol);
+    }
+
+    firpfbch_destroy(c);
+}
+
 // 
 // AUTOTEST: test sub-band energy
 //
--- src/multicarrier/tests/firpfbch_synthesizer_autotest.h
+++ src/multicarrier/tests/firpfbch_synthesizer_autotest.h
@@ -83,5 +83,78 @@ void autotest_firpfbch_synthesis() {
     firpfbch_destroy(c);
 }
 
+
+//
+// AUTOTEST: validate synthesis correctness
+//
+void autotest_firpfbch_synthesis_noise() {
+    unsigned int num_channels = 4;  // number of channels
+    unsigned int num_symbols = 8;   // number of symbols per channel
+    unsigned int m=2;               // filter delay
+    float beta=-40.0f;              // excess bandwidth factor
+    float tol=0.05f;                // error tolerance
+
+    unsigned int i;
+
+    float complex x[32] = {
+         2.101e-01+-3.436e-02*_Complex_I, -2.225e-02+ 6.707e-02*_Complex_I, 
+        -7.929e-02+-1.404e-01*_Complex_I, -1.573e-01+ 4.003e-02*_Complex_I, 
+         4.426e-02+-1.927e-02*_Complex_I, -7.451e-02+-6.044e-02*_Complex_I, 
+         3.396e-02+ 1.280e-01*_Complex_I,  1.146e-01+-2.094e-01*_Complex_I, 
+        -5.009e-02+ 8.634e-02*_Complex_I,  3.517e-02+ 1.141e-01*_Complex_I, 
+         1.256e-01+-1.558e-02*_Complex_I,  2.088e-02+-1.004e-01*_Complex_I, 
+        -7.309e-02+-5.451e-02*_Complex_I,  6.383e-02+-7.361e-02*_Complex_I, 
+         2.481e-02+-1.636e-01*_Complex_I, -6.789e-02+ 1.272e-02*_Complex_I, 
+         2.313e-01+ 7.870e-02*_Complex_I,  1.394e-01+-3.172e-02*_Complex_I, 
+         7.554e-02+ 5.138e-02*_Complex_I,  1.511e-02+-7.650e-02*_Complex_I, 
+         1.788e-01+-6.750e-02*_Complex_I, -9.681e-02+ 4.787e-02*_Complex_I, 
+         1.063e-03+-1.350e-01*_Complex_I, -1.337e-01+-6.143e-02*_Complex_I, 
+        -3.548e-02+-4.487e-02*_Complex_I, -5.897e-03+-1.817e-01*_Complex_I, 
+        -1.524e-02+-8.241e-02*_Complex_I, -1.661e-02+-1.732e-01*_Complex_I, 
+        -4.875e-02+-4.123e-02*_Complex_I, -1.245e-01+ 1.724e-01*_Complex_I, 
+        -1.316e-01+ 8.264e-02*_Complex_I, -1.496e-01+-6.837e-02*_Complex_I
+        };
+
+    float complex y_test[32] = {
+         9.551e-20+ 1.325e-19*_Complex_I, -2.704e-03+-2.485e-03*_Complex_I, 
+        -7.459e-03+ 6.773e-03*_Complex_I, -8.394e-03+ 7.708e-04*_Complex_I, 
+        -5.739e-19+-1.591e-19*_Complex_I,  1.785e-02+ 1.856e-02*_Complex_I, 
+         4.472e-02+-5.053e-02*_Complex_I,  6.561e-02+-7.523e-03*_Complex_I, 
+        -1.220e-02+-1.691e-02*_Complex_I,  5.323e-02+ 3.096e-02*_Complex_I, 
+         5.077e-02+ 1.285e-02*_Complex_I,  5.394e-02+ 5.096e-03*_Complex_I, 
+         2.958e-02+-4.029e-02*_Complex_I, -6.188e-02+-7.584e-02*_Complex_I, 
+         2.075e-03+ 7.459e-02*_Complex_I,  2.016e-02+ 2.286e-02*_Complex_I, 
+         3.290e-02+ 2.112e-02*_Complex_I, -8.431e-02+ 4.808e-02*_Complex_I, 
+        -8.209e-03+-2.955e-02*_Complex_I, -4.519e-02+ 2.624e-03*_Complex_I, 
+        -1.309e-02+-6.974e-02*_Complex_I,  1.404e-02+ 5.847e-02*_Complex_I, 
+         5.564e-03+ 1.510e-02*_Complex_I,  2.472e-02+-2.455e-02*_Complex_I, 
+         1.154e-01+ 5.465e-03*_Complex_I,  2.921e-02+ 3.312e-02*_Complex_I, 
+         8.446e-02+ 5.556e-03*_Complex_I,  7.856e-02+ 2.083e-04*_Complex_I, 
+        -1.267e-02+-5.402e-02*_Complex_I,  1.304e-02+ 2.306e-02*_Complex_I, 
+         5.027e-02+ 1.462e-03*_Complex_I,  9.807e-04+ 1.279e-02*_Complex_I
+        };
+
+    firpfbch c = firpfbch_create(num_channels,
+                                 m,
+                                 beta,
+                                 FIRPFBCH_NYQUIST,
+                                 FIRPFBCH_SYNTHESIZER);
+
+    float complex y[32];
+
+    unsigned int n=0;
+    for (i=0; i<num_symbols; i++) {
+        firpfbch_execute(c, &x[n], &y[n]);
+        n += num_channels;
+    }
+
+    for (i=0; i<32; i++) {
+        CONTEND_DELTA(crealf(y[i]), crealf(y_test[i]), tol);
+        CONTEND_DELTA(cimagf(y[i]), cimagf(y_test[i]), tol);
+    }
+
+    firpfbch_destroy(c);
+}
+
 #endif // __LIQUID_FIRPFBCH_SYNTHESIZER_AUTOTEST_H__
 
