0b719e3f5563f83792520fe06ce0cbebf0582301
Joseph D. Gaeddert <jgaedder@vt.edu>
2009-05-13 12:03:27 +0000

    firpfbch/example: adding more code to output script

--- examples/firpfbch_example.c
+++ examples/firpfbch_example.c
@@ -12,15 +12,16 @@
 int main() {
     // options
     unsigned int num_channels=8;    // number of channels
-    unsigned int m=2;               // filter delay
+    unsigned int m=5;               // filter delay
     float slsl=-60;                 // sidelobe suppression level
-    unsigned int num_frames=25;     // num frames
+    unsigned int num_symbols=16;    // num frames
+
+    unsigned int num_frames = num_symbols+4*m;
+    unsigned int num_samples = num_frames*num_channels;
 
     // create objects
     firpfbch c0 = firpfbch_create(num_channels, m, slsl, FIRPFBCH_NYQUIST, FIRPFBCH_ANALYZER);
     firpfbch c1 = firpfbch_create(num_channels, m, slsl, FIRPFBCH_NYQUIST, FIRPFBCH_SYNTHESIZER);
-
-    //firpfbch_print(c0);
 
     FILE*fid = fopen(DEBUG_FILENAME,"w");
     fprintf(fid,"%% %s: auto-generated file\n\n", DEBUG_FILENAME);
@@ -28,48 +29,69 @@ int main() {
     fprintf(fid,"num_channels=%u;\n", num_channels);
     fprintf(fid,"num_frames=%u;\n", num_frames);
 
-    fprintf(fid,"x0 = zeros(1,%u);\n",  num_channels * num_frames);
+    fprintf(fid,"x0 = zeros(1,%u);\n",  num_samples);
     fprintf(fid,"X  = zeros(%u,%u);\n", num_channels, num_frames);
-    fprintf(fid,"x1 = zeros(1,%u);\n",  num_channels * num_frames);
+    fprintf(fid,"x1 = zeros(1,%u);\n",  num_samples);
+
+    // generate data buffers
+    float complex x0[num_samples];
+    float complex  X[num_samples];
+    float complex x1[num_samples];
 
     unsigned int i, j, n=0;
-    float complex x0[num_channels], X[num_channels], x1[num_channels];
 
-    // create nco: sweeps entire range of frequencies over the evaluation interval
-    nco nco_tx = nco_create();
-    nco_set_frequency(nco_tx, -M_PI);
-    float df = 2*M_PI/(num_channels*num_frames);
+    // generate Kaiser window
+    float beta=10.0f;
+    for (i=0; i<num_samples; i++) {
+        x0[i] = randnf() + randnf()*_Complex_I;
+
+        if (i<num_symbols*num_channels) {
+            x0[i] *= kaiser(i,num_symbols*num_channels,beta);
+        } else {
+            x0[i] *= 0.0f;
+        }
+    }
+
+    // channelize time series
     for (i=0; i<num_frames; i++) {
 
-        // generate frame of data
-        for (j=0; j<num_channels; j++) {
-            nco_cexpf(nco_tx, &x0[j]);
-            nco_adjust_frequency(nco_tx, df);
-            nco_step(nco_tx);
-        }
-
         // execute analysis filter bank
-        firpfbch_execute(c0, x0, X);
+        firpfbch_execute(c0, &x0[n], &X[n]);
 
         // execute synthesis filter bank
-        firpfbch_execute(c1, X, x1);
+        firpfbch_execute(c1, &X[n], &x1[n]);
 
-        // write output to file
+        n += num_channels;
+    }
+
+    // write output to file
+    for (i=0; i<num_samples; i++) {
+        fprintf(fid,"x0(%4u) = %12.4e + j*%12.4e;\n", i+1, crealf(x0[i]), cimag(x0[i]));
+        fprintf(fid,"x1(%4u) = %12.4e + j*%12.4e;\n", i+1, crealf(x1[i]), cimag(x1[i]));
+    }
+
+    n=0;
+    for (i=0; i<num_frames; i++) {
         for (j=0; j<num_channels; j++) {
             // frequency data
-            fprintf(fid,"X(%4u,%4u) = %12.4e + j*%12.4e;\n", j+1, i+1, crealf(X[j]), cimagf(X[j]));
-
-            // time data
-            fprintf(fid,"x0(%4u) = %12.4e + j*%12.4e;\n", n+1, crealf(x0[j]), cimag(x0[j]));
-            fprintf(fid,"x1(%4u) = %12.4e + j*%12.4e;\n", n+1, crealf(x1[j]), cimag(x1[j]));
+            fprintf(fid,"X(%4u,%4u) = %12.4e + j*%12.4e;\n", j+1, i+1, crealf(X[n]), cimagf(X[n]));
             n++;
         }
-    }   
+    }
+
+    fprintf(fid,"\n\n");
+    fprintf(fid,"t  = %u:%u;\n",    1,  num_symbols*num_channels);
+    fprintf(fid,"t0 = t + %u;\n",   0);
+    fprintf(fid,"t1 = t + %u;\n",   2*num_channels*m);
+    fprintf(fid,"figure;\n");
+    fprintf(fid,"subplot(2,1,1);\n");
+    fprintf(fid,"    plot(t,real(x0(t0)), t, real(x1(t1)));\n");
+    fprintf(fid,"subplot(2,1,2);\n");
+    fprintf(fid,"    plot(t,imag(x0(t0)), t, imag(x1(t1)));\n");
 
     fclose(fid);
     printf("results written to %s\n", DEBUG_FILENAME);
 
-    nco_destroy(nco_tx);
     firpfbch_destroy(c0);
     firpfbch_destroy(c1);
 
