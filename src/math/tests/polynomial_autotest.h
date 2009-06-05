5f5abb12931fb1e50dd0c655c7c93112d144d3f7
Joseph D. Gaeddert <jgaedder@vt.edu>
2009-06-05 13:08:53 +0000

    polyfit/autotest: fixing interface usage

--- src/math/tests/polynomial_autotest.h
+++ src/math/tests/polynomial_autotest.h
@@ -25,33 +25,34 @@
 #include "liquid.h"
 
 // 
-// AUTOTEST: polyfit
+// AUTOTEST: polyfit 3rd order polynomial, critically sampled
 //
-void autotest_polyfit_q3n4()
+void autotest_polyfit_q3n3()
 {
-    unsigned int Q=3;   // polynomial order
-    unsigned int n=4;   // input vector size
+    unsigned int Q=2;   // polynomial order
+    unsigned int n=3;   // input vector size
 
-    float x[4] = {0.2, 0.5, 0.7, 0.9};
-    float y[4] = {0.2, 0.6, 0.9, 1.5};
-    float p[Q+1];
-    float p_test[4] = {
-        -0.37500000,
-         3.97976190,
-        -6.50000000,
-         4.88095238f};
+    float x[3] = {-1.0f, 0.0f, 1.0f};
+    float y[3] = { 1.0f, 0.0f, 1.0f};
+    float p[3];
+    float p_test[3] = {0.0f, 0.0f, 1.0f};
     float tol = 1e-3f;
 
-    polyfit(x,y,n, p,Q);
+    unsigned int k=Q+1;
+    polyfit(x,y,n, p,k);
 
-    unsigned int i;
-    for (i=0; i<Q+1; i++)
-        printf("p[%3u] = %12.8f\n", i, p[i]);
+    if (_autotest_verbose) {
+        unsigned int i;
+        for (i=0; i<3; i++)
+            printf("%3u : %12.8f > %12.8f\n", i, x[i], y[i]);
+        for (i=0; i<3; i++)
+            printf("p[%3u] = %12.8f\n", i, p[i]);
+    }
     
     CONTEND_DELTA(p[0], p_test[0], tol);
     CONTEND_DELTA(p[1], p_test[1], tol);
     CONTEND_DELTA(p[2], p_test[2], tol);
-    CONTEND_DELTA(p[3], p_test[3], tol);
+    //CONTEND_DELTA(p[3], p_test[3], tol);
 }
 
 #endif 
