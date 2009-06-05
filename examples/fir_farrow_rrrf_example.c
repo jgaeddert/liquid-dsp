617f72cb22447814cf2b18a2f66e43b059530458
Joseph D. Gaeddert <jgaedder@vt.edu>
2009-06-05 14:19:51 +0000

    farrow: improving example; bug: delay is not symmetric

--- examples/fir_farrow_rrrf_example.c
+++ examples/fir_farrow_rrrf_example.c
@@ -11,14 +11,14 @@
 int main() {
     // options
     unsigned int h_len=17;  // filter length
-    unsigned int p=3;
+    unsigned int p=5;       // polynomial order
     float fc=0.9f;          // filter cutoff
     float slsl=60.0f;       // sidelobe suppression level
-    unsigned int nfft=64;      // 
-    unsigned int m=5;       // number of delays to evaluate
+    unsigned int m=7;       // number of delays to evaluate
 
     // coefficients array
     float h[h_len];
+    float tao = ((float)h_len-1)/2.0f;  // nominal filter delay
 
     fir_farrow_rrrf f = fir_farrow_rrrf_create(h_len, p, slsl);
 
@@ -29,6 +29,7 @@ int main() {
     fprintf(fid,"h_len = %u;\n", h_len);
     fprintf(fid,"h = zeros(%u,%u);\n", m, h_len);
     fprintf(fid,"mu = zeros(1,%u);\n", m);
+    fprintf(fid,"tao = %12.8f;\n", tao);
 
     unsigned int i, j;
     float mu_vect[m];
@@ -58,6 +59,10 @@ int main() {
     fprintf(fid,"f = [0:(nfft-1)]/(2*nfft);\n");
     fprintf(fid,"figure;\n");
     fprintf(fid,"plot(f,D,'-k','LineWidth',2);\n");
+    fprintf(fid,"axis([0 0.5 (tao-1) (tao+1)]);\n");
+    fprintf(fid,"for i=1:m,\n");
+    fprintf(fid,"    text(0.3,tao+mu(i)+0.025,['\mu =' num2str(mu(i))]);\n");
+    fprintf(fid,"end;\n");
 
     fclose(fid);
     printf("results written to %s\n", OUTPUT_FILENAME);
