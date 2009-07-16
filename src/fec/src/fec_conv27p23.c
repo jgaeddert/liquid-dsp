18de23250d5e3769099ac54172df9d2cd43ebe2a
Joseph D. Gaeddert <jgaedder@vt.edu>
2009-07-16 14:12:58 +0000

    fec/conv(punctured): getting matrix dimensions right

--- src/fec/src/fec_conv27p23.c
+++ src/fec/src/fec_conv27p23.c
@@ -47,7 +47,7 @@ const int FEC_CONV(_poly)[FEC_CONV(_R)] = {V27POLYA,
                                            V27POLYB};
 
 // puncturing matrix : 
-const int FEC_CONV(_puncturing_matrix)[2][FEC_CONV(_P)] = {
+const int FEC_CONV(_puncturing_matrix)[FEC_CONV(_P)][2] = {
     {1, 1},
     {1, 0}
 };
