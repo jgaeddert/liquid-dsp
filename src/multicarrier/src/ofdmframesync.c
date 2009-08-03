a60778fc97f195b57032be13493229a6329ae056
Joseph D. Gaeddert <jgaedder@vt.edu>
2009-08-03 14:29:25 +0000

    ofdmframesync: adding debugging information ; fixing delay correlator

--- src/multicarrier/src/ofdmframesync.c
+++ src/multicarrier/src/ofdmframesync.c
@@ -32,7 +32,9 @@
 #   include <fftw3.h>
 #endif
 
-#define DEBUG_OFDMFRAMESYNC 1
+#define DEBUG_OFDMFRAMESYNC             1
+#define DEBUG_OFDMFRAMESYNC_FILENAME    "ofdmframesync_internal_debug.m"
+#define DEBUG_OFDMFRAMESYNC_BUFFER_LEN  (1024)
 
 struct ofdmframesync_s {
     unsigned int num_subcarriers;
@@ -48,6 +50,10 @@ struct ofdmframesync_s {
     fftwf_plan fft;
 #else
     fftplan fft;
+#endif
+
+#if DEBUG_OFDMFRAMESYNC
+    cfwindow debug_rxy;
 #endif
 };
 
@@ -75,11 +81,38 @@ ofdmframesync ofdmframesync_create(unsigned int _num_subcarriers,
     q->wdelay = cfwindow_create(q->cp_len + q->num_subcarriers);
     cfwindow_clear(q->wcp);
     cfwindow_clear(q->wdelay);
+#if DEBUG_OFDMFRAMESYNC
+    q->debug_rxy = cfwindow_create(DEBUG_OFDMFRAMESYNC_BUFFER_LEN);
+#endif
     return q;
 }
 
 void ofdmframesync_destroy(ofdmframesync _q)
 {
+#if DEBUG_OFDMFRAMESYNC
+    FILE * fid = fopen(DEBUG_OFDMFRAMESYNC_FILENAME,"w");
+    fprintf(fid,"%% %s : auto-generated file\n", DEBUG_OFDMFRAMESYNC_FILENAME);
+    fprintf(fid,"close all;\n");
+    fprintf(fid,"clear all;\n");
+    fprintf(fid,"n = %u;\n", DEBUG_OFDMFRAMESYNC_BUFFER_LEN);
+    unsigned int i;
+
+    fprintf(fid,"rxy = zeros(1,n);\n");
+    float complex * rc;
+    cfwindow_read(_q->debug_rxy, &rc);
+    for (i=0; i<DEBUG_OFDMFRAMESYNC_BUFFER_LEN; i++)
+        fprintf(fid,"rxy(%4u) = %12.4e + j*%12.4e;\n", i+1, crealf(rc[i]), cimagf(rc[i]));
+    fprintf(fid,"figure;\n");
+    fprintf(fid,"plot(0:(n-1),abs(rxy));\n");
+    fprintf(fid,"xlabel('sample index');\n");
+    fprintf(fid,"ylabel('|r_{xy}|');\n");
+
+    fclose(fid);
+    printf("ofdmframesync/debug: results written to %s\n", DEBUG_OFDMFRAMESYNC_FILENAME);
+
+    cfwindow_destroy(_q->debug_rxy);
+#endif
+
     free(_q->x);
     free(_q->X);
 #if HAVE_FFTW3_H
@@ -123,14 +156,14 @@ void ofdmframesync_cpcorrelate(ofdmframesync _q,
     cfwindow_read(_q->wcp,    &rcp);
     cfwindow_read(_q->wdelay, &rdelay);
 
-    // increment delay pointer
-    rdelay += _q->num_subcarriers;
-
     float complex rxy;
     dotprod_cccf_run(rcp,rdelay,_q->cp_len,&rxy);
-    rxy /= (float)(_q->cp_len);
+    //rxy /= (float)(_q->cp_len);
 
     // TODO : push rxy into buffer?
-    printf("|rxy| = %12.8f\n", cabsf(rxy));
+    //printf("|rxy| = %12.8f\n", cabsf(rxy));
+#ifdef DEBUG_OFDMFRAMESYNC
+    cfwindow_push(_q->debug_rxy, rxy);
+#endif
 }
 
