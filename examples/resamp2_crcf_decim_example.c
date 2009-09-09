5f4f2f67ab4b0e7e3bf813e3469c217f41f2fca7
Joseph D. Gaeddert <jgaedder@vt.edu>
2009-09-09 15:26:39 +0000

    resamp2/examples: cleaning up code; adding more description in comments

--- examples/resamp2_crcf_decim_example.c
+++ examples/resamp2_crcf_decim_example.c
@@ -11,31 +11,36 @@
 #define OUTPUT_FILENAME "resamp2_crcf_decim_example.m"
 
 int main() {
-    unsigned int m=5;
+    unsigned int m=5;           // filter semi-length
     unsigned int h_len = 4*m+1; // filter length
-    float fc=0.17f;
-    unsigned int N=128;
+    float fc=0.17f;             // input tone frequency
+    unsigned int N=128;         // number of output samples
 
+    // create/print the half-band resampler
     resamp2_cccf f = resamp2_cccf_create(h_len);
-
     resamp2_cccf_print(f);
 
+    // open output file
     FILE*fid = fopen(OUTPUT_FILENAME,"w");
     fprintf(fid,"%% %s : auto-generated file\n", OUTPUT_FILENAME);
     fprintf(fid,"clear all;\nclose all;\n\n");
-    fprintf(fid,"h_len=%u;\nN=%u;\n", h_len, N);
+    fprintf(fid,"h_len=%u;\n", h_len);
+    fprintf(fid,"N=%u;\n", N);
 
     unsigned int i;
     float theta=0.0f, dtheta=2*M_PI*fc;
     float complex x[2], y;
     for (i=0; i<N; i++) {
+        // generate input : complex sinusoid
         x[0] = cexpf(_Complex_I*theta);
         theta += dtheta;
         x[1] = cexpf(_Complex_I*theta);
         theta += dtheta;
 
+        // run the decimator
         resamp2_cccf_decim_execute(f, x, &y);
 
+        // save results to output file
         fprintf(fid,"x(%3u) = %8.4f + j*%8.4f;\n", 2*i+1, crealf(x[0]), cimagf(x[0]));
         fprintf(fid,"x(%3u) = %8.4f + j*%8.4f;\n", 2*i+2, crealf(x[1]), cimagf(x[1]));
         fprintf(fid,"y(%3u) = %8.4f + j*%8.4f;\n", i+1,   crealf(y),    cimagf(y));
@@ -44,16 +49,22 @@ int main() {
     }
 
     fprintf(fid,"nfft=512;\n");
-    fprintf(fid,"X=20*log10(abs(fftshift(fft(x.*hamming(length(x))',nfft))));\n");
-    fprintf(fid,"Y=20*log10(abs(fftshift(fft(y.*hamming(length(y))',nfft))));\n");
+    fprintf(fid,"wx = hamming(length(x))'/N*1.8678/2;   %% normalized window (input)\n");
+    fprintf(fid,"wy = hamming(length(y))'/N*1.8678/2;   %% normalized window (output)\n");
+    fprintf(fid,"X=20*log10(abs(fftshift(fft(x.*wx,nfft))));\n");
+    fprintf(fid,"Y=20*log10(abs(fftshift(fft(y.*wy,nfft))));\n");
     fprintf(fid,"f=[0:(nfft-1)]/nfft-0.5;\n");
     fprintf(fid,"figure; plot(f,X,'Color',[0.5 0.5 0.5],f/2,Y,'LineWidth',2);\n");
-    fprintf(fid,"grid on;\nxlabel('normalized frequency');\nylabel('PSD [dB]');\n");
+    fprintf(fid,"grid on;\n");
+    fprintf(fid,"xlabel('normalized frequency');\n");
+    fprintf(fid,"ylabel('PSD [dB]');\n");
     fprintf(fid,"legend('original','decimated',1);");
+    fprintf(fid,"axis([-0.5 0.5 -100 10]);\n");
 
     fclose(fid);
     printf("results written to %s\n", OUTPUT_FILENAME);
 
+    // clean up allocated objects
     resamp2_cccf_destroy(f);
     printf("done.\n");
     return 0;
--- examples/resamp2_crcf_interp_example.c
+++ examples/resamp2_crcf_interp_example.c
@@ -11,29 +11,34 @@
 #define OUTPUT_FILENAME "resamp2_crcf_interp_example.m"
 
 int main() {
-    unsigned int m=5;
+    unsigned int m=5;           // filter semi-length
     unsigned int h_len = 4*m+1; // filter length
-    float fc=0.17f;
-    unsigned int N=128;
+    float fc=0.17f;             // input tone frequency
+    unsigned int N=128;         // number of input samples
 
+    // create/print the half-band resampler
     resamp2_cccf f = resamp2_cccf_create(h_len);
-
     resamp2_cccf_print(f);
 
+    // open output file
     FILE*fid = fopen(OUTPUT_FILENAME,"w");
     fprintf(fid,"%% %s: auto-generated file\n",OUTPUT_FILENAME);
     fprintf(fid,"clear all;\nclose all;\n\n");
-    fprintf(fid,"h_len=%u;\nN=%u;\n", h_len, N);
+    fprintf(fid,"h_len=%u;\n", h_len);
+    fprintf(fid,"N=%u;\n", N);
 
     unsigned int i;
     float theta=0.0f, dtheta=2*M_PI*fc;
     float complex x, y[2];
     for (i=0; i<N; i++) {
+        // generate input : complex sinusoid
         x = cexpf(_Complex_I*theta);
         theta += dtheta;
 
+        // run the interpolator
         resamp2_cccf_interp_execute(f, x, y);
 
+        // save results to output file
         fprintf(fid,"x(%3u) = %8.4f + j*%8.4f;\n", i+1,   crealf(x),    cimagf(x));
         fprintf(fid,"y(%3u) = %8.4f + j*%8.4f;\n", 2*i+1, crealf(y[0]), cimagf(y[0]));
         fprintf(fid,"y(%3u) = %8.4f + j*%8.4f;\n", 2*i+2, crealf(y[1]), cimagf(y[1]));
@@ -43,16 +48,22 @@ int main() {
     }
 
     fprintf(fid,"nfft=512;\n");
-    fprintf(fid,"X=20*log10(abs(fftshift(fft(x.*hamming(length(x))',nfft))));\n");
-    fprintf(fid,"Y=20*log10(abs(fftshift(fft(y.*hamming(length(y))',nfft))));\n");
+    fprintf(fid,"wx = hamming(length(x))'/N*1.8678;   %% normalized window (input)\n");
+    fprintf(fid,"wy = hamming(length(y))'/N*1.8678/2; %% normalized window (output)\n");
+    fprintf(fid,"X=20*log10(abs(fftshift(fft(x.*wx,nfft))));\n");
+    fprintf(fid,"Y=20*log10(abs(fftshift(fft(y.*wy,nfft))));\n");
     fprintf(fid,"f=[0:(nfft-1)]/nfft-0.5;\n");
     fprintf(fid,"figure; plot(f/2,X,'Color',[0.5 0.5 0.5],f,Y,'LineWidth',2);\n");
-    fprintf(fid,"grid on;\nxlabel('normalized frequency');\nylabel('PSD [dB]');\n");
+    fprintf(fid,"grid on;\n");
+    fprintf(fid,"xlabel('normalized frequency');\n");
+    fprintf(fid,"ylabel('PSD [dB]');\n");
     fprintf(fid,"legend('original','interpolated',1);");
+    fprintf(fid,"axis([-0.5 0.5 -100 10]);\n");
 
     fclose(fid);
     printf("results written to %s\n",OUTPUT_FILENAME);
 
+    // clean up allocated objects
     resamp2_cccf_destroy(f);
     printf("done.\n");
     return 0;
