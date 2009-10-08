0a9a1dce8b091bff315d2f1e2be82c45ac592a39
Joseph D. Gaeddert <jgaedder@vt.edu>
2009-10-08 13:57:34 +0000

    firpfbch/example: simplifying arrays so as to only store data as it becomes available (except in output file)

--- examples/firpfbch_example.c
+++ examples/firpfbch_example.c
@@ -16,8 +16,6 @@ int main() {
     float slsl=-60;                 // sidelobe suppression level
     unsigned int num_symbols=16;    // number of baseband symbols
 
-    unsigned int num_frames = num_symbols+4*m;
-    unsigned int num_samples = num_frames*num_channels;
     unsigned int i, j, k, n;
 
     // create objects
@@ -27,17 +25,17 @@ int main() {
     fprintf(fid,"%% %s: auto-generated file\n\n", OUTPUT_FILENAME);
     fprintf(fid,"clear all;\nclose all;\n\n");
     fprintf(fid,"num_channels=%u;\n", num_channels);
-    fprintf(fid,"num_frames=%u;\n", num_frames);
-    fprintf(fid,"num_samples = num_channels*num_frames;\n");
+    fprintf(fid,"num_symbols=%u;\n", num_symbols);
+    fprintf(fid,"num_samples = num_channels*num_symbols;\n");
 
-    fprintf(fid,"x  = zeros(num_channels,num_frames);\n");
+    fprintf(fid,"x  = zeros(num_channels,num_symbols);\n");
     fprintf(fid,"y0 = zeros(1,num_samples);\n");
     fprintf(fid,"y1 = zeros(1,num_samples);\n");
 
     // generate data buffers
-    float complex  x[num_samples];  // input matrix: num_channels x num_frames
-    float complex y0[num_samples];  // conventional output
-    float complex y1[num_samples];  // filterbank channelizer output
+    float complex  x[num_symbols];  // input
+    float complex y0[num_symbols];  // conventional output
+    float complex y1[num_symbols];  // filterbank channelizer output
 
     // objects to run conventional channelizer
     interp_crcf interp[num_channels];
@@ -56,69 +54,64 @@ int main() {
         nco_set_frequency(ncox[i], f);
     }
 
-    // generate random data
-    for (i=0; i<num_samples; i++) {
-        x[i] = randnf() + randnf()*_Complex_I;
-        // select a specific channel
-        //if ((i%num_channels)!=1)
-        //    x[i] = 0.0f;
-    }
-
     // synthesize time series
     n=0;
     float complex y0a[num_channels];
     float complex y0b[num_channels];
-    for (i=0; i<num_frames; i++) {
+    float rmse_synthesis=0.0f;
+    float complex err;
+    for (i=0; i<num_symbols; i++) {
+        // generate random samples
+        for (j=0; j<num_channels; j++) {
+            x[j] = randnf() + randnf()*_Complex_I;
+            // select a specific channel
+            //if ((i%num_channels)!=1)
+            //    x[i] = 0.0f;
+        }
+
+        // 
         // execute conventional synthesizer
+        //
         for (j=0; j<num_channels; j++)
-            y0[n+j] = 0.0f;
+            y0[j] = 0.0f;
 
         for (j=0; j<num_channels; j++) {
             // run interpolator
-            interp_crcf_execute(interp[j],x[n+j],y0a);
+            interp_crcf_execute(interp[j],x[j],y0a);
 
             // up-convert
             nco_mix_block_up(ncox[j],y0a,y0b,num_channels);
 
             // append to output buffer
             for (k=0; k<num_channels; k++) {
-                y0[n+k] += y0b[k] / (float)(num_channels);
+                y0[k] += y0b[k] / (float)(num_channels);
             }
         }
 
+        // 
         // execute synthesis filter bank
-        firpfbch_execute(cs, &x[n], &y1[n]);
+        //
+        firpfbch_execute(cs, x, y1);
 
+        // compute error
+        for (j=0; j<num_channels; j++) {
+            err = y0[j] - y1[j];
+            rmse_synthesis += crealf(err*conjf(err)) / (float)(num_channels);
+        }
+
+        // save output
+        for (j=0; j<num_channels; j++) {
+            fprintf(fid,"x(%4u,%4u) = %12.4e + j*%12.4e;\n", i+1, j+1, crealf(x[j]), cimag(x[j]));
+            fprintf(fid,"y0(%4u) = %12.4e + j*%12.4e;\n", n+j+1, crealf(y0[j]), cimag(y0[j]));
+            fprintf(fid,"y1(%4u) = %12.4e + j*%12.4e;\n", n+j+1, crealf(y1[j]), cimag(y1[j]));
+        }
         n += num_channels;
     }
-
-    // compute error
-    float rmse=0.0f;
-    float complex err;
-    for (i=0; i<num_samples; i++) {
-        err = y0[i] - y1[i];
-        rmse += crealf(err*conjf(err));
-    }
-    rmse = sqrtf(rmse / (float)num_samples);
-    printf("rmse : %12.4e\n", rmse);
-
-    // write output to file
-    for (i=0; i<num_samples; i++) {
-        fprintf(fid,"y0(%4u) = %12.4e + j*%12.4e;\n", i+1, crealf(y0[i]), cimag(y0[i]));
-        fprintf(fid,"y1(%4u) = %12.4e + j*%12.4e;\n", i+1, crealf(y1[i]), cimag(y1[i]));
-    }
-
-    n=0;
-    // channelized data
-    for (i=0; i<num_frames; i++) {
-        for (j=0; j<num_channels; j++) {
-            fprintf(fid,"x(%4u,%4u) = %12.4e + j*%12.4e;\n", j+1, i+1, crealf(x[n]), cimagf(x[n]));
-            n++;
-        }
-    }
+    rmse_synthesis = sqrtf(rmse_synthesis);
+    printf("synthesis rmse : %12.4e\n", rmse_synthesis);
 
     fprintf(fid,"\n\n");
-    fprintf(fid,"t  = 1:num_samples;\n");
+    fprintf(fid,"t  = 0:(num_samples-1);\n");
     fprintf(fid,"figure;\n");
     fprintf(fid,"subplot(2,1,1);\n");
     fprintf(fid,"    plot(t,real(y0), t,real(y1));\n");
