25dc030e6b37614c7006bc90365d770b5eb3b5aa
Joseph D. Gaeddert <jgaedder@vt.edu>
2009-11-16 16:36:05 +0000

    packetizer/example: adding command-line options

--- examples/packetizer_example.c
+++ examples/packetizer_example.c
@@ -5,14 +5,68 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <getopt.h>
 
 #include "liquid.h"
 
-int main() {
+// print usage/help message
+void usage()
+{
+    printf("fec_example [options]\n");
+    printf("  u/h   : print usage\n");
+    printf("  n     : input data size (number of uncoded bytes): 16 default\n");
+    printf("  c     : coding scheme (inner): h74 default\n");
+    printf("  k     : coding scheme (outer): r3 default\n");
+    // print all available FEC schemes
+    unsigned int i;
+    for (i=0; i<LIQUID_NUM_FEC_SCHEMES; i++)
+        printf("          %s\n", fec_scheme_str[i]);
+}
+
+
+int main(int argc, char*argv[]) {
     // 
     unsigned int n=16;
     fec_scheme fec0 = FEC_HAMMING74;
     fec_scheme fec1 = FEC_REP3;
+
+    int dopt;
+    while((dopt = getopt(argc,argv,"uhn:c:k:")) != EOF){
+        switch (dopt) {
+        case 'h':
+        case 'u': usage(); return 0;
+        case 'n':
+            n = atoi(optarg);
+            if (n < 1) {
+                printf("error: packet length must be positive\n");
+                usage();
+                exit(-1);
+            }
+            break;
+        case 'c':
+            // inner FEC scheme
+            fec0 = liquid_getopt_str2fec(optarg);
+            if (fec0 == FEC_UNKNOWN) {
+                printf("error: unknown/unsupported modulation scheme \"%s\"\n\n",optarg);
+                usage();
+                exit(-1);
+            }
+            break;
+        case 'k':
+            // outer FEC scheme
+            fec1 = liquid_getopt_str2fec(optarg);
+            if (fec1 == FEC_UNKNOWN) {
+                printf("error: unknown/unsupported modulation scheme \"%s\"\n\n",optarg);
+                usage();
+                exit(-1);
+            }
+            break;
+        default:
+            printf("error: unknown/invalid option\n");
+            usage();
+            exit(-1);
+        }
+    }
 
     unsigned int packet_len = packetizer_get_packet_length(n,fec0,fec1);
     packetizer p = packetizer_create(n,fec0,fec1);
