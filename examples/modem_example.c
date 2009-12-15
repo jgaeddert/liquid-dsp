14265a13ec5db63828a953342a5bec2571323fce
Joseph D. Gaeddert <jgaedder@vt.edu>
2009-12-15 17:17:21 +0000

    modem|example: adding output file to plot constellation

--- examples/modem_example.c
+++ examples/modem_example.c
@@ -10,6 +10,8 @@
 #include <stdio.h>
 #include "liquid.h"
 
+#define OUTPUT_FILENAME "modem_example.m"
+
 int main() {
     // create mod/demod objects
     unsigned int bps=5;
@@ -17,6 +19,16 @@ int main() {
     modem demod = modem_create(MOD_APSK32, bps);
 
     modem_print(mod);
+
+    // open output file
+    FILE*fid = fopen(OUTPUT_FILENAME,"w");
+    fprintf(fid,"%% %s : auto-generated file\n", OUTPUT_FILENAME);
+    fprintf(fid,"clear all;\n");
+    fprintf(fid,"close all;\n\n");
+    fprintf(fid,"m = %u;\n", bps);
+    fprintf(fid,"M = %u;\n", 1<<bps);
+    fprintf(fid,"c = zeros(1,M);\n");
+    fprintf(fid,"i_str = cell(1,M);\n");
 
     unsigned int i; // modulated symbol
     unsigned int s; // demodulated symbol
@@ -28,8 +40,28 @@ int main() {
         modem_modulate(mod, i, &x);
         modem_demodulate(demod, x, &s);
         num_errors += count_bit_errors(i,s);
+
+        // write symbol to output file
+        fprintf(fid,"c(%3u) = %12.4e + j*%12.4e;\n", i+1, crealf(x), cimagf(x));
+        fprintf(fid,"i_str{%3u} = [num2str(%3u)];\n", i+1, i);
     }
     printf("num errors: %4u / %4u\n", num_errors, num_symbols*bps);
+
+    // plot results
+    fprintf(fid,"\n\n");
+    fprintf(fid,"figure;\n");
+    fprintf(fid,"plot(c,'o');\n");
+    fprintf(fid,"hold on;\n");
+    fprintf(fid,"text(real(c)+0.02, imag(c)+0.02, i_str);\n");
+    fprintf(fid,"hold off;\n");
+    fprintf(fid,"axis([-1 1 -1 1]*1.6);\n");
+    fprintf(fid,"axis square;\n");
+    fprintf(fid,"grid on;\n");
+    fprintf(fid,"xlabel('in phase');\n");
+    fprintf(fid,"ylabel('quadrature phase');\n");
+
+    fclose(fid);
+    printf("results written to %s.\n", OUTPUT_FILENAME);
 
     modem_destroy(mod);
     modem_destroy(demod);
