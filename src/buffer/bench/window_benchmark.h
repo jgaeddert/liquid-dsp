e8351b062f87260a08bb8fb91e2edf48fdbd906f
Joseph D. Gaeddert <jgaedder@vt.edu>
2009-12-17 14:11:53 +0000

    window|benchmark: adding more window lengths to benchmark

--- src/buffer/bench/window_benchmark.h
+++ src/buffer/bench/window_benchmark.h
@@ -18,30 +18,32 @@ void window_bench(
     unsigned int _n)
 {
     // initialize port
-    fwindow w = fwindow_create(_n);
+    cfwindow w = cfwindow_create(_n);
 
     unsigned long int i;
 
     // start trials:
     //   write to port, read from port
+    *_num_iterations *= 32;
     getrusage(RUSAGE_SELF, _start);
     for (i=0; i<(*_num_iterations); i++) {
-        fwindow_push(w, 1.0f);
-        fwindow_push(w, 1.0f);
-        fwindow_push(w, 1.0f);
-        fwindow_push(w, 1.0f);
+        cfwindow_push(w, 1.0f);
+        cfwindow_push(w, 1.0f);
+        cfwindow_push(w, 1.0f);
+        cfwindow_push(w, 1.0f);
     }
     getrusage(RUSAGE_SELF, _finish);
     *_num_iterations *= 4;
 
-    fwindow_destroy(w);
+    cfwindow_destroy(w);
 }
 
 // 
-void xbenchmark_window_n1     WINDOW_BENCH_API(1)
-void xbenchmark_window_n4     WINDOW_BENCH_API(4)
-void benchmark_window_n16    WINDOW_BENCH_API(16)
-void xbenchmark_window_n64    WINDOW_BENCH_API(64)
+void benchmark_cfwindow_n16     WINDOW_BENCH_API(16)
+void benchmark_cfwindow_n32     WINDOW_BENCH_API(32)
+void benchmark_cfwindow_n64     WINDOW_BENCH_API(64)
+void benchmark_cfwindow_n128    WINDOW_BENCH_API(128)
+void benchmark_cfwindow_n256    WINDOW_BENCH_API(256)
 
 #endif // __LIQUID_WINDOW_BENCHMARK_H__
 
