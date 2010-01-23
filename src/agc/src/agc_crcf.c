166a7493817b1d9cba5a24b6e3d93a98b00a15bb
Joseph D. Gaeddert <jgaedder@vt.edu>
2010-01-23 21:11:50 +0000

    agc: fixing bug; forgot to add crucial files

---
+++ src/agc/src/agc_crcf.c
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) 2007, 2008, 2009, 2010 Joseph Gaeddert
+ * Copyright (c) 2007, 2008, 2009, 2010 Virginia Polytechnic
+ *                                      Institute & State University
+ *
+ * This file is part of liquid.
+ *
+ * liquid is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * liquid is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with liquid.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+//
+// AGC API: floating-point [complex]
+//
+
+#include "liquid.internal.h"
+
+// macros
+#define AGC(name)           LIQUID_CONCAT(agc_crcf,name)
+
+#define T                   float           // general
+#define TC                  float complex   // input/output
+
+#define TC_COMPLEX          1
+
+// source files
+#include "agc.c"
