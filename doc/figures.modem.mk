012f24cb87120535f4c8a4e7a2f78e55a4982281
Joseph D. Gaeddert <jgaedder@vt.edu>
2010-01-28 19:00:28 +0000

    doc|build: cleaning up figures generation scirpting in makefile

--- doc/figures.modem.mk
+++ doc/figures.modem.mk
@@ -5,8 +5,17 @@
 GNUPLOT_VERSION = 4.2
 GNUPLOT = gnuplot
 
-figures_generated_modem :=		\
-	figures.gen/modem_16qam.eps
+local_epsfiles :=			\
+	figures.gen/modem_bpsk.eps	\
+	figures.gen/modem_qpsk.eps	\
+	figures.gen/modem_8psk.eps	\
+	figures.gen/modem_16psk.eps	\
+	figures.gen/modem_16qam.eps	\
+	figures.gen/modem_64qam.eps	\
+	figures.gen/modem_256qam.eps
+
+local_gnufiles := $(patsubst %.eps,%.gnu,$(local_epsfiles))
+local_datfiles := $(patsubst %.eps,%.dat,$(local_epsfiles))
 
 # gnuplot script generator function
 src/modem.genplot : src/modem.genplot.c
@@ -14,15 +23,32 @@ src/modem.genplot : src/modem.genplot.c
 # modem data generator function
 src/modem.gendata : src/modem.gendata.c
 
-figures.gen/modem_16qam.dat : src/modem.gendata
-	./$< -f $@ -g $(GNUPLOT_VERSION) -m qam -p 4
-figures.gen/modem_16qam.gnu : src/modem.genplot
-	./$< -f $@ -g $(GNUPLOT_VERSION) -t eps -d figures.gen/modem_16qam.dat
-figures.gen/modem_16qam.eps : figures.gen/modem_16qam.gnu figures.gen/modem_16qam.dat
+# eps target files
+$(local_epsfiles) : %.eps : %.gnu %.dat
 	$(GNUPLOT) $< > $@
 
-extra_clean += src/modem.genplot src/modem/gendata
+# gnu target files (gnuplot)
+$(local_gnufiles) : %.gnu : %.dat src/modem.genplot
+	./src/modem.genplot -f $@ -g $(GNUPLOT_VERSION) -t eps -d $<
+
+# psk
+figures.gen/modem_bpsk.dat   : src/modem.gendata ; ./$< -f $@ -m qpsk -p 1
+figures.gen/modem_qpsk.dat   : src/modem.gendata ; ./$< -f $@ -m qpsk -p 2
+figures.gen/modem_8psk.dat   : src/modem.gendata ; ./$< -f $@ -m  psk -p 3
+figures.gen/modem_16psk.dat  : src/modem.gendata ; ./$< -f $@ -m  psk -p 4
+
+# qam
+figures.gen/modem_16qam.dat  : src/modem.gendata ; ./$< -f $@ -m  qam -p 4
+figures.gen/modem_64qam.dat  : src/modem.gendata ; ./$< -f $@ -m  qam -p 6
+figures.gen/modem_256qam.dat : src/modem.gendata ; ./$< -f $@ -m  qam -p 8
 
 # accumulate target
-figures_generated += $(figures_generated_modem)
+figures_generated += $(local_epsfiles)
+
+extra_clean +=				\
+	src/modem.genplot		\
+	src/modem.gendata		\
+	$(local_epsfiles)		\
+	$(local_gnufiles)		\
+	$(local_datfiles)
 
