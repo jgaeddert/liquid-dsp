d40fa97b41cff40695fbca36370f6b59be62b6e9
Joseph D. Gaeddert <jgaedder@vt.edu>
2010-01-29 14:49:13 +0000

    doc|filter: cleaning up PSD plot

--- doc/src/filter_kaiser.c
+++ doc/src/filter_kaiser.c
@@ -10,6 +10,11 @@
 
 #define OUTPUT_FILENAME "figures.gen/filter_kaiser.gnu"
 
+void compute_filter_psd(float * _h,
+                        unsigned int _h_len,
+                        float complex * _X,
+                        unsigned int _nfft);
+
 int main() {
     // options
     float fc=0.4f;          // filter cutoff frequency
@@ -17,27 +22,15 @@ int main() {
     float slsl=60.0f;       // sidelobe suppression level
     float mu=0.0f;          // fractional timing offset
 
+    // estimate required filter length and generate filter
     unsigned int h_len=(unsigned int)num_fir_filter_taps(slsl,ft);
-
-    unsigned int i;
     float h[h_len];
     fir_kaiser_window(h_len,fc,slsl,mu,h);
-#if 0
-    for (i=0; i<h_len; i++)
-        printf("h(%3u) = %12.8f;\n", i+1, h[i]);
-#endif
 
+    // compute filter power spectral density
     unsigned int nfft=512;
-    float complex x[nfft];
     float complex X[nfft];
-    fftplan fft = fft_create_plan(nfft,x,X,FFT_FORWARD,0);
-    for (i=0; i<nfft; i++)
-        x[i] = i < h_len ? h[i] : 0.0f;
-    fft_execute(fft);
-    fft_destroy_plan(fft);
-
-    //for (i=0; i<nfft; i++)
-    //    printf("x(%3u) = %12.4e + j*%12.4e;\n", i+1, crealf(x[i]), cimagf([i]));
+    compute_filter_psd(h,h_len,X,nfft);
 
     // open output file and print header
     FILE*fid = fopen(OUTPUT_FILENAME,"w");
@@ -45,28 +38,47 @@ int main() {
     fprintf(fid,"reset\n");
     // TODO : switch terminal types here
     fprintf(fid,"set terminal postscript eps enhanced color solid rounded\n");
-    fprintf(fid,"set xrange [-0.5:0.5]\n");
-    fprintf(fid,"set yrange [-100:40]\n");
-    //fprintf(fid,"set size square\n");
+    fprintf(fid,"set xrange [-0.5:0.5];\n");
+    fprintf(fid,"set yrange [-100:20]\n");
+    fprintf(fid,"set size ratio 0.6\n");
     //fprintf(fid,"set title 'filter design'\n");
     fprintf(fid,"set xlabel 'Normalized Frequency'\n");
     fprintf(fid,"set ylabel 'Power Spectral Density [dB]'\n");
     fprintf(fid,"set nokey # disable legned\n");
     fprintf(fid,"set grid xtics ytics\n");
-    fprintf(fid,"set grid lc rgb '#999999' lw 1\n");
-    fprintf(fid,"plot '-' using 1:2 with lines lw 4 linecolor rgb '#004080'\n");
+    fprintf(fid,"set grid linetype 1 linecolor rgb '#999999' lw 1\n");
+    fprintf(fid,"plot '-' using 1:2 with lines linetype 1 linewidth 4 linecolor rgb '#004080'\n");
 
     // save array to output
     float f;
-    unsigned int k;
+    unsigned int i, k;
     fprintf(fid,"# frequency psd\n");
     for (i=0; i<nfft; i++) {
         f = (float)(i) / (float)nfft - 0.5f;
         k = (i + nfft/2) % nfft;
-        fprintf(fid,"%12.8f %12.8f\n", f, 20*log10(cabsf(X[k])));
-    }   
+        fprintf(fid,"%12.8f %12.4e\n", f, 20.0f*log10f(cabsf(X[k])));
+    }
 
     fclose(fid);
     return 0;
+}
+
+void compute_filter_psd(float * _h,
+                        unsigned int _h_len,
+                        float complex * _X,
+                        unsigned int _nfft)
+{
+    unsigned int i;
+    // compute and remove any DC bias
+    float sum=0.0f;
+    for (i=0; i<_h_len; i++)
+        sum += _h[i];
+
+    float complex x[_nfft];
+    fftplan fft = fft_create_plan(_nfft,x,_X,FFT_FORWARD,0);
+    for (i=0; i<_nfft; i++)
+        x[i] = i < _h_len ? _h[i] / sum : 0.0f;
+    fft_execute(fft);
+    fft_destroy_plan(fft);
 }
 
