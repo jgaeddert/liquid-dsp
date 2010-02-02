83299ffbb7bd7007b79a1d11f7b522704c09f783
Joseph D. Gaeddert <jgaedder@vt.edu>
2010-02-02 15:24:14 +0000

    doc|interp: adding code snippet

---
+++ doc/listings/interp.example.c
@@ -0,0 +1,22 @@
+#include <liquid/liquid.h>
+// ...
+{
+    unsigned int M=4;       // interpolation factor
+    unsigned int h_len;     // interpolation filter length
+
+    // design filter and create interpolator
+    float h[h_len];
+    // ... design appropriate filter ...
+    interp_crcf q = interp_crcf_create(M,h,h_len);
+
+    // generate input signal and interpolate
+    float complex x;        // input sample
+    float complex y[M];     // output array
+
+    // ...
+
+    interp_crcf_execute(q, x, y);   // repeat as necessary
+
+    // destroy the interpolator object
+    interp_crcf_destroy(q);
+}
