1b91f5ee46047580a5db7cf101aa32e2e946ebad
Joseph D. Gaeddert <jgaedder@vt.edu>
2010-02-03 15:16:44 +0000

    doc|dotprod: adding general discussion on dot product; adding example

---
+++ doc/listings/dotprod_rrrf.example.c
@@ -0,0 +1,18 @@
+#include <liquid/liquid.h>
+// ...
+{
+    // create input arrays
+    float x[] = {1, 2, 3, 4, 5};
+    float v[] = {1, 1, 1, 1, 1};
+    float y;
+
+    // run the basic vector dot product, store in 'y'
+    dotprod_rrrf_run(x,v,5,&y);
+
+    // create dotprod object and execute, store in 'y'
+    dotprod_rrrf q = dotprod_rrrf_create(v,5);
+    dotprod_rrrf_execute(q,x,&y);
+    dotprod_rrrf_destroy(q);
+}
+
+
