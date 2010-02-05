87db7ac32912be2ed98514bdfad091bd96c20aa3
Joseph D. Gaeddert <jgaedder@vt.edu>
2010-02-05 20:06:06 +0000

    forgot to add example file

---
+++ examples/iirdes_cheby1_example.c
@@ -0,0 +1,51 @@
+//
+//
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+#include "liquid.h"
+
+#define OUTPUT_FILENAME "iirdes_cheby1_example.m"
+
+int main() {
+    // options
+    unsigned int n=4;   // filter order
+    float fc = 0.25f;   // cutoff
+    float ripple = 1.0f;// passband ripple [dB]
+
+    // epsilon
+    float epsilon = sqrtf( powf(10.0f, ripple / 10.0f) - 1.0f );
+
+    float b[n+1];       // numerator
+    float a[n+1];       // denominator
+
+    cheby1f(n,epsilon,b,a);
+
+    // print coefficients
+    unsigned int i;
+    for (i=0; i<=n; i++) printf("a(%3u) = %12.8f;\n", i+1, a[i]);
+    for (i=0; i<=n; i++) printf("b(%3u) = %12.8f;\n", i+1, b[i]);
+
+    // open output file
+    FILE*fid = fopen(OUTPUT_FILENAME,"w");
+    fprintf(fid,"%% %s : auto-generated file\n", OUTPUT_FILENAME);
+    fprintf(fid,"\nclear all;\nclose all;\n\n");
+    fprintf(fid,"n=%u;\n", n);
+    fprintf(fid,"a = zeros(1,n+1);\n");
+    fprintf(fid,"b = zeros(1,n+1);\n");
+    for (i=0; i<=n; i++) {
+        fprintf(fid,"a(%3u) = %12.4e;\n", i+1, a[i]);
+        fprintf(fid,"b(%3u) = %12.4e;\n", i+1, b[i]);
+    }
+    fprintf(fid,"\n");
+    fprintf(fid,"freqz(b,a);\n");
+    fclose(fid);
+    printf("results written to %s.\n", OUTPUT_FILENAME);
+
+    printf("done.\n");
+    return 0;
+}
+
