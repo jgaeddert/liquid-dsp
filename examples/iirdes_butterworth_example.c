f150b400ab50c21c670be07540b90e055c2d4438
Joseph D. Gaeddert <jgaedder@vt.edu>
2010-02-05 19:06:36 +0000

    filter|butter: cleaning up butterworth filter design

--- examples/iirdes_butterworth_example.c
+++ examples/iirdes_butterworth_example.c
@@ -7,12 +7,12 @@
 
 #include "liquid.h"
 
-//#define OUTPUT_FILENAME "iirdes_butterworth_example.c"
+#define OUTPUT_FILENAME "iirdes_butterworth_example.m"
 
 int main() {
     // options
     unsigned int n=5;   // filter order
-    float fc = 0.1f;    // cutoff
+    float fc = 0.25f;   // cutoff
 
     float b[n+1];       // numerator
     float a[n+1];       // denominator
@@ -23,6 +23,22 @@ int main() {
     unsigned int i;
     for (i=0; i<=n; i++) printf("a(%3u) = %12.8f;\n", i+1, a[i]);
     for (i=0; i<=n; i++) printf("b(%3u) = %12.8f;\n", i+1, b[i]);
+
+    // open output file
+    FILE*fid = fopen(OUTPUT_FILENAME,"w");
+    fprintf(fid,"%% %s : auto-generated file\n", OUTPUT_FILENAME);
+    fprintf(fid,"\nclear all;\nclose all;\n\n");
+    fprintf(fid,"n=%u;\n", n);
+    fprintf(fid,"a = zeros(1,n+1);\n");
+    fprintf(fid,"b = zeros(1,n+1);\n");
+    for (i=0; i<=n; i++) {
+        fprintf(fid,"a(%3u) = %12.4e;\n", i+1, a[i]);
+        fprintf(fid,"b(%3u) = %12.4e;\n", i+1, b[i]);
+    }
+    fprintf(fid,"\n");
+    fprintf(fid,"freqz(b,a);\n");
+    fclose(fid);
+    printf("results written to %s.\n", OUTPUT_FILENAME);
 
     printf("done.\n");
     return 0;
--- src/filter/src/butter.c
+++ src/filter/src/butter.c
@@ -28,10 +28,42 @@
 #include <math.h>
 #include "liquid.internal.h"
 
-// butterworth polynomial
-void butterpolyf(unsigned int _n, float *_p)
+#define LIQUID_DEBUG_BUTTER_PRINT   0
+
+// compute complex roots of the butterworth polynomial (poles of the
+// filter); length of _r is _n+1
+void butter_rootsf(unsigned int _n,
+                   float complex *_r)
 {
+    unsigned int i;
+    float theta;
+    for (i=0; i<_n; i++) {
+        theta = (float)(2*(i+1) + _n - 1)*M_PI/(float)(2*_n);
+        _r[i] = -cexpf(_Complex_I*theta);
+    }
+
+#if LIQUID_DEBUG_BUTTER_PRINT
+    printf("poles:\n");
+    for (i=0; i<_n; i++)
+        printf("  r[%3u] = %12.8f + j*%12.8f\n", i+1, crealf(-_r[i]), cimagf(-_r[i]));
+#endif
+}
+
+// butterworth polynomial of order _n (length of _p is _n+1)
+void butter_polyf(unsigned int _n, float *_p)
+{
+    // compute poles
+    float complex s[_n];
+    butter_rootsf(_n,s);
     
+    // expand roots
+    float complex pcf[_n];
+    cfpoly_expandroots(s,_n,pcf);
+
+    // return only real components
+    unsigned int i;
+    for (i=0; i<=_n; i++)
+        _p[i] = crealf(pcf[i]);
 }
 
 void butterf(unsigned int _n,
@@ -48,28 +80,16 @@ void butterf(unsigned int _n,
         exit(1);
     }
 
-    // poles
-    float complex s[_n];
+    unsigned int i, j;
+    // butterworth polynomial
+    float p[_n+1];
+    butter_polyf(_n,p);
 
-    unsigned int i;
-    float theta;
-    for (i=0; i<_n; i++) {
-        theta = (float)(2*(i+1) + _n - 1)*M_PI/(float)(2*_n);
-        s[i] = -cexpf(_Complex_I*theta);
-    }
-    
-    printf("poles:\n");
-    for (i=0; i<_n; i++)
-        printf("  s[%3u] = %12.8f + j*%12.8f\n", i+1, crealf(-s[i]), cimagf(-s[i]));
-
-    // expand roots
-    float complex p[_n+1];
-    cfpoly_expandroots(s,_n,p);
-
-    // print results
+#if LIQUID_DEBUG_BUTTER_PRINT
     printf("expanded polynomial:\n");
     for (i=0; i<=_n; i++)
-        printf("  p[%3u] = %12.8f + j*%12.8f\n", i+1, crealf(p[i]), cimagf(p[i]));
+        printf("  p[%3u] = %12.8f\n", i+1, p[i]);
+#endif
 
     // ...
     unsigned int nb = _n+1;
@@ -78,11 +98,9 @@ void butterf(unsigned int _n,
     // normalized cutoff frequency
     float m = 1.0f / tanf(M_PI * _fc);
     float mk = 1.0f;    // placeholder for m^k
+#if LIQUID_DEBUG_BUTTER_PRINT
     printf("m = %12.8f\n", m);
-
-    float as[na];
-    for (i=0; i<=_n; i++)
-        as[i] = crealf(p[i]);
+#endif
 
     for (i=0; i<nb; i++) _b[i] = 0;
     for (i=0; i<na; i++) _a[i] = 0;
@@ -92,17 +110,19 @@ void butterf(unsigned int _n,
 
     // compute denominator
     for (i=0; i<na; i++) {
+        // expand the polynomial (1 + x)^i * (1 - x)^(_n-i)
         poly_binomial_expand_pm(_n,_n-i,poly_1pz);
-        unsigned int j;
 
+#if LIQUID_DEBUG_BUTTER_PRINT
         printf("  poly[n=%-3u k=%-3u] : ", _n, i);
         for (j=0; j<na; j++)
             printf("%6d", poly_1pz[j]);
         printf("\n");
+#endif
 
         // accumulate polynomial coefficients
         for (j=0; j<na; j++)
-            _a[j] += as[i]*mk*poly_1pz[j];
+            _a[j] += p[i]*mk*poly_1pz[j];
 
         // update frequency-scaling multiplier
         mk *= m;
@@ -113,14 +133,14 @@ void butterf(unsigned int _n,
     for (i=0; i<na; i++)
         _b[i] = poly_1pz[i];
 
-    // normalize to a[0]
+    // normalize by a[0]
     float a0_inv = 1.0f / _a[0];
     for (i=0; i<=_n; i++) {
         _b[i] *= a0_inv;
         _a[i] *= a0_inv;
     }
      
-#if 0
+#if LIQUID_DEBUG_BUTTER_PRINT
     printf("numerator:\n");
     for (i=0; i<nb; i++)
         printf("  b[%3u] = %12.8f\n", i, _b[i]);
