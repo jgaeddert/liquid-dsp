e70d45c10f7ab6e926f50d2f187dcec27c166d9d
Joseph D. Gaeddert <jgaedder@vt.edu>
2010-02-12 17:44:24 +0000

    modem|am|example: adding command-line options

--- examples/ampmodem_example.c
+++ examples/ampmodem_example.c
@@ -4,23 +4,67 @@
 // carrier phase, and carrier frequency offsets
 //
 
+#include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 #include <math.h>
+#include <getopt.h>
 #include <complex.h>
 
 #include "liquid.h"
 
 #define OUTPUT_FILENAME "ampmodem_example.m"
 
-int main() {
+// print usage/help message
+void usage()
+{
+    printf("ampmodem_example [options]\n");
+    printf("  u/h   : print usage\n");
+    printf("  f     : frequency offset [default: 0.02]\n");
+    printf("  p     : phase offset [default: pi/4]\n");
+    printf("  n     : number of samples [default: 256]\n");
+    printf("  s     : SNR (dB) [default: 20]\n");
+    printf("  t     : AM type (ssb/dsb) [default: ssb]\n");
+}
+
+int main(int argc, char*argv[]) {
     // options
     float mod_index = 0.1f;         // modulation index (bandwidth)
     //float fc = 0.1371f*2.0f*M_PI;   // AM carrier
     float cfo = 0.02f;              // carrier frequency offset
-    float cpo = M_PI / 3.0f;        // carrier phase offset
+    float cpo = M_PI / 4.0f;        // carrier phase offset
     unsigned int num_samples = 256; // number of samples
     float SNRdB = 20.0f;            // signal-to-noise ratio [dB]
-    liquid_modem_amtype type = LIQUID_MODEM_AM_DSB;
+    liquid_modem_amtype type = LIQUID_MODEM_AM_SSB;
+
+    int dopt;
+    while ((dopt = getopt(argc,argv,"uhf:p:n:s:t:")) != EOF) {
+        switch (dopt) {
+        case 'u':
+        case 'h':
+            usage();
+            return 0;
+        case 'f':   cfo = atof(optarg); break;
+        case 'p':   cpo = atof(optarg); break;
+        case 'n':   num_samples = atoi(optarg); break;
+        case 's':   SNRdB = atof(optarg);       break;
+        case 't':
+            if (strcmp(optarg,"ssb")==0) {
+                type = LIQUID_MODEM_AM_SSB;
+            } else if (strcmp(optarg,"dsb")==0) {
+                type = LIQUID_MODEM_AM_DSB;
+            } else {
+                fprintf(stderr,"error: ampmodem_example, invalid AM type: %s\n", optarg);
+                usage();
+                return 1;
+            }
+            break;
+        default:
+            fprintf(stderr,"error: ampmodem_example, unknown option\n");
+            usage();
+            return 1;
+        }
+    }
 
     // create mod/demod objects
     ampmodem mod   = ampmodem_create(mod_index,type);
@@ -69,7 +113,11 @@ int main() {
     fprintf(fid,"t=0:(n-1);\n");
     fprintf(fid,"figure;\n");
     fprintf(fid,"plot(t,x,t,z);\n");
-    fprintf(fid,"axis([0 n -1.2 1.2]);\n");
+    fprintf(fid,"axis([0 n -0.4 1.2]);\n");
+    fprintf(fid,"xlabel('time');\n");
+    fprintf(fid,"ylabel('signal');\n");
+    fprintf(fid,"legend('original','demodulated',1);\n");
+    fprintf(fid,"grid on;\n");
     // spectrum
     fprintf(fid,"nfft=1024;\n");
     fprintf(fid,"f=[0:(nfft-1)]/nfft - 0.5;\n");
