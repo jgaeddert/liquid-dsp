28c60a5245969ba47ecb0b30d451dabd7b53bb2c
Joseph D. Gaeddert <jgaedder@vt.edu>
2010-02-13 19:11:40 +0000

    poly|example: adding approximation to bessel polynomial roots

--- examples/poly_findroots_example.c
+++ examples/poly_findroots_example.c
@@ -9,7 +9,27 @@
 
 #include "liquid.h"
 
+#define OUTPUT_FILENAME "poly_findroots_example.m"
+
 void besselpoly(unsigned int _n, int * _p);
+
+//  n   m0(n)                   m2(n)                   tr(n)
+//  4   -2.32218504000000e+00   1.57017774731388e-01    1.75438094000000
+//  5   -2.99066420749534e+00   1.25586431233268e-01    1.76789754200000
+//  6   -3.66047349303832e+00   1.04583627408534e-01    1.77689253100000
+//  7   -4.33196373609137e+00   8.96656396226647e-02    1.78345867057143e+00
+//  8   -4.99584039483034470    0.07824392346702261     1.78322436142857e+00
+//
+// m[0] ~ -0.668861023825672*n + 0.352940768662957
+// m[2] ~ 1 / (1.6013579390149844*n -0.0429146801453954)
+//
+// Roots r[i] of the (n-1)^th-order Bessel polynomial lie on
+// quadratic polynomial (approximately)
+//      Im{r[i]} ~ t * 1.778
+// where t = i - L - w + 0.5, w = (n-1) % 2, L = (n-w-1)/2
+//      Re{r[i]} ~ m0(n) + m1(n)*Im{r[i]}^2
+//
+//
 
 int main() {
     unsigned int n=5;
@@ -46,6 +66,33 @@ int main() {
     printf("poly (expanded roots):\n");
     for (i=0; i<n; i++)
         printf("  p[%3u] = %12.8f + j*%12.8f\n", i, crealf(p_hat[i]), cimagf(p_hat[i]));
+
+    FILE * fid = fopen(OUTPUT_FILENAME,"w");
+    fprintf(fid,"%% %s : auto-generated file\n", OUTPUT_FILENAME);
+    fprintf(fid,"clear all;\n");
+    fprintf(fid,"close all;\n\n");
+    fprintf(fid,"n = %u;\n", n);
+    for (i=0; i<n; i++)
+        fprintf(fid,"p(%3u) = %12.8f + j*%12.8f;\n", i+1, crealf(p[i]), cimagf(p[i]));
+    for (i=0; i<n-1; i++)
+        fprintf(fid,"r(%3u) = %12.8f + j*%12.8f;\n", i+1, crealf(roots[i]), cimagf(roots[i]));
+    fprintf(fid,"figure;\n");
+    fprintf(fid,"t = -0.5:0.01:0.1;\n");
+    fprintf(fid,"plot(t,polyval(p,t));\n");
+    fprintf(fid,"grid on;\n");
+
+    // fit roots to 2nd-order polynomial
+    fprintf(fid,"m = polyfit(imag(r),real(r),2);\n");
+    fprintf(fid,"y = [-1:0.01:1]*1.1*max(imag(r));\n");
+    fprintf(fid,"figure;\n");
+    fprintf(fid,"plot(polyval(m,y),y,'-',r,'x');\n");
+    fprintf(fid,"grid on;\n");
+
+    fprintf(fid,"n\n");
+    fprintf(fid,"m'\n");
+
+    fclose(fid);
+    printf("results written to %s\n", OUTPUT_FILENAME);
     
     return 0;
 }
