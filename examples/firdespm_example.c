429d93b99ab307ce2a8f835b45379388871e6499
Joseph D. Gaeddert <jgaedder@vt.edu>
2010-03-26 14:18:45 +0000

    firdespm|example: writing results to output file

--- examples/firdespm_example.c
+++ examples/firdespm_example.c
@@ -11,6 +11,8 @@
 
 #include "liquid.h"
 
+#define OUTPUT_FILENAME "firdespm_example.m"
+
 int main() {
     unsigned int n=25;
     float fp = 0.1f;
@@ -22,15 +24,37 @@ int main() {
     float weights[2] = {1.0f, 1.0f};
     liquid_firdespm_btype btype = LIQUID_FIRDESPM_BANDPASS;
 
+    unsigned int i;
     float h[n];
     firdespm q = firdespm_create(n,bands,des,weights,num_bands,btype);
     firdespm_print(q);
     firdespm_execute(q,h);
     firdespm_destroy(q);
 
-    unsigned int i;
     for (i=0; i<n; i++)
         printf("%20.12f\n", h[i]);
+
+    // open output file
+    FILE*fid = fopen(OUTPUT_FILENAME,"w");
+    fprintf(fid,"%% %s : auto-generated file\n", OUTPUT_FILENAME);
+    fprintf(fid,"clear all;\n");
+    fprintf(fid,"close all;\n\n");
+    fprintf(fid,"h_len=%u;\n", n);
+
+    for (i=0; i<n; i++)
+        fprintf(fid,"h(%4u) = %20.8e;\n", i+1, h[i]);
+
+    fprintf(fid,"nfft = 1024;\n");
+    fprintf(fid,"f = [0:(nfft-1)]/nfft - 0.5;\n");
+    fprintf(fid,"H = 20*log10(abs(fftshift(fft(h,nfft))));\n");
+    fprintf(fid,"figure;\n");
+    fprintf(fid,"plot(f,H);\n");
+    fprintf(fid,"xlabel('Normalized Frequency');\n");
+    fprintf(fid,"ylabel('PSD [dB]');\n");
+    fprintf(fid,"grid on;\n");
+
+    fclose(fid);
+    printf("results written to %s.\n", OUTPUT_FILENAME);
 
     printf("done.\n");
     return 0;
