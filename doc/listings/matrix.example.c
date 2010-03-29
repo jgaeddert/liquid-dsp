baca0e696b7fd18c327b9a78596adee29df132bb
Joseph D. Gaeddert <jgaedder@vt.edu>
2010-03-29 20:50:10 +0000

    doc|poly,matrix: updating interfaces

--- doc/listings/matrix.example.c
+++ doc/listings/matrix.example.c
@@ -7,10 +7,10 @@
        3.99475,   0.88066,   4.69373,   0.44563,
        7.28072,  -2.06608,   0.67074,   9.80657,
        6.07741,  -3.93099,   1.22826,  -0.42142};
-    fmatrix_print(X,4,4);
+    matrixf_print(X,4,4);
 
     // L/U decomp (Doolittle's method)
     float L[16], U[16], P[16];
-    fmatrix_ludecomp_doolittle(X,4,4,L,U,P);
+    matrixf_ludecomp_doolittle(X,4,4,L,U,P);
 }
 
