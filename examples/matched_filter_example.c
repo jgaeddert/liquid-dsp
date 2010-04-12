7be9bbf467f13ca64d77fec6c2596be071f91b6a
Joseph D. Gaeddert <jgaedder@vt.edu>
2010-04-12 16:07:42 +0000

    firdes|example: improving plots in matched filter example

--- examples/matched_filter_example.c
+++ examples/matched_filter_example.c
@@ -13,8 +13,8 @@
 int main() {
     // options
     unsigned int k=2;   // samples/symbol
-    unsigned int m=5;   // symbol delay
-    float beta=0.3f;    // excess bandwidth factor
+    unsigned int m=3;   // symbol delay
+    float beta=0.5f;    // excess bandwidth factor
     unsigned int num_symbols=16;
 
     // initialize objects
@@ -25,6 +25,13 @@ int main() {
     //design_rrc_filter(k,m,beta,0,h);
     interp_rrrf q  = interp_rrrf_create(k,h,h_len);
     decim_rrrf d   = decim_rrrf_create(k,h,h_len);
+
+    // compute filter inter-symbol interference
+    float isi_mse=0;
+    float isi_max=0;
+    liquid_filter_isi(h,k,m,&isi_mse, &isi_max);
+    printf("  isi (max) : %12.8f dB\n", 20*log10f(isi_max));
+    printf("  isi (mse) : %12.8f dB\n", 20*log10f(isi_mse));
 
     // generate signal
     float sym_in, buff[k], sym_out;
@@ -76,10 +83,23 @@ int main() {
     fprintf(fid,"f = [0:(nfft-1)]/nfft - 0.5;\n");
     fprintf(fid,"H = 20*log10(abs(fftshift(fft(h/k,nfft))));\n");
     fprintf(fid,"figure;\n");
-    fprintf(fid,"plot(f,H);\n");
+    fprintf(fid,"plot(f,H,'-','LineWidth',2,...\n");
+    fprintf(fid,"     [0.5/k],[-3],'or',...\n");
+    fprintf(fid,"     [0.5/k*(1-beta) 0.5/k*(1-beta)],[-100 10],'-r',...\n");
+    fprintf(fid,"     [0.5/k*(1+beta) 0.5/k*(1+beta)],[-100 10],'-r');\n");
     fprintf(fid,"xlabel('normalized frequency');\n");
     fprintf(fid,"ylabel('PSD');\n");
     fprintf(fid,"axis([-0.5 0.5 -100 10]);\n");
+    fprintf(fid,"grid on;\n");
+
+    fprintf(fid,"figure;\n");
+    fprintf(fid,"g = conv(h,h)/k;\n");
+    fprintf(fid,"t = [(-2*k*m):(2*k*m)]/k;\n");
+    fprintf(fid,"i0 = [0:k:4*k*m]+1;\n");
+    fprintf(fid,"plot(t,g,'-s',...\n");
+    fprintf(fid,"     t(i0),g(i0),'or');\n");
+    fprintf(fid,"xlabel('symbol index');\n");
+    fprintf(fid,"ylabel('matched filter response');\n");
     fprintf(fid,"grid on;\n");
 
     fclose(fid);
