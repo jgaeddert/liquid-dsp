b30f24329841569e10e36232fd93b57ee1011686
Joseph D. Gaeddert <jgaedder@vt.edu>
2010-04-20 15:32:33 +0000

    quantizer|example: extending example to use compander; caluclating error and plotting output

--- examples/quantize_example.c
+++ examples/quantize_example.c
@@ -1,32 +1,126 @@
 //
+// quantize_example.c
 //
+// Demonstrates the quantizer/compander combo.
 //
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <getopt.h>
+#include <math.h>
 
 #include "liquid.h"
 
-int main() {
-    unsigned int num_steps=30;
-    unsigned int num_bits=3;
+#define OUTPUT_FILENAME "quantize_example.m"
 
-    unsigned int i, q;
-    float x=-1.5f, y, dx=3.0f/((float)num_steps);
+// print usage/help message
+void usage()
+{
+    printf("quantize_example [options]\n");
+    printf("  u/h   : print usage\n");
+    printf("  b     : number of bits, 0 < b <= 16 [default: 4]\n");
+    printf("  m     : mu, compression factor, mu > 0 [default: 255.0]\n");
+}
 
-    printf("  analog   ADC     DAC\n");
-    printf("  ------   ---     ---\n");
-    // x = -1.0, -0.9, ... 1.0
-    for (i=0; i<num_steps+1; i++) {
-        // analog to digital converter
-        q = quantize_adc(x,num_bits);
 
-        // digital to analog converter
-        y = quantize_dac(q,num_bits);
+int main(int argc, char*argv[]) {
+    unsigned int num_bits=4;
+    float mu = 255.0f;
+    unsigned int num_samples = 64;
 
-        printf("%8.4f,  0x%8.8x, %8.4f\n", x, q, y);
-        x += dx;
+    int dopt;
+    while ((dopt = getopt(argc,argv,"uhb:m:")) != EOF) {
+        switch (dopt) {
+        case 'u':
+        case 'h': usage();                  return 0;
+        case 'b': num_bits = atoi(optarg);  break;
+        case 'm': mu = atof(optarg);        break;
+        default:
+            fprintf(stderr,"error: modem_example, unknown option\n");
+            usage();
+            return 1;
+        }
     }
+
+    // validate input
+    if (num_bits > 16 || num_bits < 1) {
+        fprintf(stderr,"error: %s, quantizer bits must be in [1,16]\n", argv[0]);
+        exit(1);
+    } else if (mu < 0.0f) {
+        fprintf(stderr,"error: %s, mu must be greater than 0\n", argv[0]);
+        exit(1);
+    }
+
+    unsigned int i;
+    float x[num_samples];
+    float y[num_samples];
+
+    float v;        // compressed sample
+    unsigned int q; // quantized sample
+    float u;        // uncompressed sample
+    float e;        // error
+    float rmse = 0.0f;
+    printf("         input        ADC        output\n");
+    printf("         -----        ---        ------\n");
+    float phi = 0.0f;
+    float dphi = 0.02f * 256.0f / (float) num_samples;
+    for (i=0; i<num_samples; i++) {
+        // generate windowed pulse
+        x[i] = 0.5f*cosf(2.0f*M_PI*phi) + 0.5f*cosf(2.0f*M_PI*phi*0.57f);
+        x[i] *= 0.5f * kaiser(i, num_samples, 10.0f, 0.0f);
+        phi += dphi;
+
+        // compress sample
+        v = compress_mulaw(x[i],mu);
+
+        // quantize: analog to digital converter
+        q = quantize_adc(v,num_bits);
+
+        // quantize: digital to analog converter
+        u = quantize_dac(q,num_bits);
+
+        // expand sample
+        y[i] = expand_mulaw(u,mu);
+
+        // compute error
+        e = y[i] - x[i];
+        rmse += e*e;
+
+        printf("%4u : %12.8f > 0x%4.4x > %12.8f\n", i, x[i], q, y[i]);
+    }
+    rmse = sqrtf(rmse / (float)num_samples);
+    printf("-----------------------\n");
+    printf("rmse : %12.4e\n", rmse);
+
+    // open debug file
+    FILE * fid = fopen(OUTPUT_FILENAME,"w");
+    fprintf(fid,"%% %s: auto-generated file\n\n", OUTPUT_FILENAME);
+    fprintf(fid,"clear all\n");
+    fprintf(fid,"close all\n");
+    fprintf(fid,"num_samples = %u;\n", num_samples);
+
+    for (i=0; i<num_samples; i++) {
+        fprintf(fid,"x(%4u) = %16.8e; y(%4u) = %16.8e;\n", i+1, x[i], i+1, y[i]);
+    }
+    fprintf(fid,"figure;\n");
+    fprintf(fid,"t = 0:(num_samples-1);\n");
+#if 0
+    fprintf(fid,"plot(t,x,t,y);\n");
+#else
+    fprintf(fid,"hold on;\n");
+    fprintf(fid,"plot(t,x,'Color',[1 0 0]);\n");
+    fprintf(fid,"stairs(t,y);\n");
+    fprintf(fid,"hold off;\n");
+#endif
+    fprintf(fid,"xlabel('sample index');\n");
+    fprintf(fid,"ylabel('signal');\n");
+    fprintf(fid,"legend('original','reconstructed',1);\n");
+    fprintf(fid,"grid on;\n");
+ 
+    // close debug file
+    fclose(fid);
+    printf("results wrtten to %s\n", OUTPUT_FILENAME);
+
     printf("done.\n");
     return 0;
 }
