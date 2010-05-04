d42ae827a76105ff7ea15743229f4baffd8fd77b
Joseph D. Gaeddert <jgaedder@vt.edu>
2010-05-04 01:41:14 +0000

    agc: improving example to compare ag types

--- examples/agc_example.c
+++ examples/agc_example.c
@@ -1,56 +1,128 @@
 //
-// Automatic gain control test
+// agc_example.c
+//
+// Automatic gain control example demonstrating the transient
+// responses between the three AGC types (LIQUID_AGC_DEFAULT,
+// LIQUID_AGC_LOG, LIQUID_AGC_EXP).
 //
 
 #include <stdio.h>
+#include <math.h>
+#include <complex.h>
 #include "liquid.h"
 
 #define OUTPUT_FILENAME "agc_example.m"
 
 int main() {
     // options
-    float etarget=500.0f;   // target energy
-    float bt=0.01f;         // loop bandwidth
-    unsigned int n=100;     // number of iterations
-    unsigned int d=5;       // print every d iterations
+    float etarget=1.0f;                 // target level
+    float gamma=1.0f;                   // channel gain
+    float noise_floor = -25.0f;         // noise floor [dB]
+    float bt=0.001f;                    // agc loop bandwidth
+    unsigned int num_samples = 2048;    // number of samples
+    unsigned int d=num_samples/32;      // print every d iterations
 
     // create objects
     agc_crcf p = agc_crcf_create();
-    agc_crcf_set_type(p,LIQUID_AGC_EXP);
-    agc_crcf_set_target(p,etarget);
-    agc_crcf_set_bandwidth(p,bt);
+    agc_crcf_set_target(p, etarget);
+    agc_crcf_set_bandwidth(p, bt);
 
-    // print info
-    printf("automatic gain control // target: %5.1f, loop bandwidth: %4.2e\n",etarget,bt);
-
-    FILE* fid = fopen(OUTPUT_FILENAME,"w");
-    fprintf(fid,"%% %s : auto-generated file\n\n",OUTPUT_FILENAME);
-    fprintf(fid,"clear all;\nclose all;\n\n");
+    // squelch
+    agc_crcf_squelch_deactivate(p);
 
     unsigned int i;
-    float complex x = 1.2f + _Complex_I*3.9f, y=x;
-    for (i=0; i<n; i++) {
-        fprintf(fid,"x(%3u) = %12.4e;\n", i+1, cabsf(y));
+    float complex x[num_samples];
+    float rssi_default[num_samples];
+    float rssi_log[num_samples];
+    float rssi_exp[num_samples];
 
-        if ( ((i+1)%d)==0 || i+1==n )
-            printf("%3u: %8.3f\n", i+1, cabsf(y));
+    // print info
+    printf("automatic gain control // target: %8.4f, loop bandwidth: %4.2e\n",etarget,bt);
 
-        agc_crcf_execute(p, x, &y);
+    // generate signal
+    for (i=0; i<num_samples; i++)
+        x[i] = gamma * cexpf(_Complex_I*2*M_PI*0.093f*i);
+
+    unsigned int n=0;
+    unsigned int n0   = num_samples / 6;
+    unsigned int ramp = num_samples / 256;
+    unsigned int n1   = num_samples / 2;
+    for (i=0; i<n0; i++)    x[n++] *= 0.0f;
+    for (i=0; i<ramp; i++)  x[n++] *= 0.5f - 0.5f*cosf(M_PI*i/(float)ramp);
+    for (i=0; i<n1; i++)    x[n++] *= 1.0f;
+    for (i=0; i<ramp; i++)  x[n++] *= 0.5f + 0.5f*cosf(M_PI*i/(float)ramp);
+    while (n < num_samples) x[n++] *= 0.0f;
+
+    // add noise
+    float noise_std = powf(10.0f, noise_floor / 10.0f) / sqrtf(2.0f);
+    for (i=0; i<num_samples; i++)
+        x[i] += noise_std*(randnf() + _Complex_I*randnf());
+
+    // run agc
+    float complex y;
+
+    // default
+    agc_crcf_reset(p);
+    agc_crcf_set_type(p,LIQUID_AGC_DEFAULT);
+    for (i=0; i<num_samples; i++) {
+        agc_crcf_execute(p, x[i], &y);
+        rssi_default[i] = agc_crcf_get_signal_level(p);
     }
 
-    fprintf(fid,"\n\n%% print results\n");
-    fprintf(fid,"n=%u;\nt=0:(n-1);\n",n);
-    fprintf(fid,"e=%12.4e;\n",etarget);
-    fprintf(fid,"figure;\nsemilogy([0 n],[e e],'-r',t,x,'-b','LineWidth',2);\n");
-    fprintf(fid,"xlabel('time');\nylabel('output amplitude');\n");
-    fprintf(fid,"title(['Automatic gain control, target=%5.1f, bt=%4.2e']);\n", etarget, bt);
-    fprintf(fid,"grid on;\n");
-    fprintf(fid,"\n\n");
+    // 
+    agc_crcf_reset(p);
+    agc_crcf_set_type(p,LIQUID_AGC_LOG);
+    for (i=0; i<num_samples; i++) {
+        agc_crcf_execute(p, x[i], &y);
+        rssi_log[i] = agc_crcf_get_signal_level(p);
+    }
 
-    fclose(fid);
-    printf("results written to %s\n", OUTPUT_FILENAME);
+    // default
+    agc_crcf_reset(p);
+    agc_crcf_set_type(p,LIQUID_AGC_EXP);
+    for (i=0; i<num_samples; i++) {
+        agc_crcf_execute(p, x[i], &y);
+        rssi_exp[i] = agc_crcf_get_signal_level(p);
+    }
 
     agc_crcf_destroy(p);
+
+    // print results to screen
+    printf("received signal strength indication (rssi):\n");
+    printf("   i    default      log      exp\n");
+    printf("----   -------- -------- --------\n");
+    for (i=0; i<num_samples; i+=d) {
+        printf("%4u : %8.2f %8.2f %8.2f\n",
+            i,
+            10*log10f(rssi_default[i]),
+            10*log10f(rssi_log[i]),
+            10*log10f(rssi_exp[i]));
+    }
+
+    // open output file
+    FILE* fid = fopen(OUTPUT_FILENAME,"w");
+    fprintf(fid,"%% %s: auto-generated file\n\n",OUTPUT_FILENAME);
+    fprintf(fid,"clear all;\nclose all;\n\n");
+    fprintf(fid,"n = %u;\n", num_samples);
+
+    for (i=0; i<num_samples; i++) {
+        //fprintf(fid,"x(%4u) = %12.4e + j*%12.4e;\n", i+1, crealf(x[i]), cimagf(x[i]));
+        fprintf(fid,"rssi_default(%4u)  = %12.4e;\n", i+1, rssi_default[i]);
+        fprintf(fid,"rssi_log(%4u)      = %12.4e;\n", i+1, rssi_log[i]);
+        fprintf(fid,"rssi_exp(%4u)      = %12.4e;\n", i+1, rssi_exp[i]);
+    }
+
+    fprintf(fid,"\n\n");
+    fprintf(fid,"t = 0:(n-1);\n");
+    fprintf(fid,"plot(t,10*log10(rssi_default),'-','LineWidth',1,...\n");
+    fprintf(fid,"     t,10*log10(rssi_log),    '-','LineWidth',1,...\n");
+    fprintf(fid,"     t,10*log10(rssi_exp),    '-','LineWidth',1);");
+    fprintf(fid,"grid on;\n");
+    fprintf(fid,"xlabel('sample index');\n");
+    fprintf(fid,"ylabel('rssi [dB]');\n");
+    fprintf(fid,"legend('AGC\\_DEFAULT','AGC\\_LOG','AGC\\_EXP',1);\n");
+    fclose(fid);
+    printf("results written to %s\n", OUTPUT_FILENAME);
 
     printf("done.\n");
     return 0;
