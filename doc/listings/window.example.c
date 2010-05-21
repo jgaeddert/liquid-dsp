e07333916050610601bb67301fadcdda7e5825cc
Joseph D. Gaeddert <jgaedder@vt.edu>
2010-05-21 17:21:26 -0400

    fleshing out window section

--- doc/listings/window.example.c
+++ doc/listings/window.example.c
@@ -1,34 +1,36 @@
 #include <liquid/liquid.h>
 // ...
 {
+    // initialize array for writing
     float v[] = {9, 8, 7, 6, 5, 4, 3, 2, 1, 0};
 
     // create window with 10 elements
-    // 0 0 0 0 0 0 0 0 0 0
     fwindow w = fwindow_create(10);
+    // window[10] : {0 0 0 0 0 0 0 0 0 0}
 
     // push 4 elements into the window
-    // 0 0 0 0 0 0 1 3 6 2
     fwindow_push(w, 1);
     fwindow_push(w, 3);
     fwindow_push(w, 6);
     fwindow_push(w, 2);
+    // window[10] : {0 0 0 0 0 0 1 3 6 2}
 
     // push 4 elements at a time
-    // 0 0 1 3 6 2 9 8 7 6
     fwindow_write(w, v, 4);
+    // window[10] : {0 0 1 3 6 2 9 8 7 6}
 
     // recreate window (truncate to last 6 elements)
-    // 6 2 9 8 7 6
     w = fwindow_recreate(w,6);
+    // window[6] : {6 2 9 8 7 6}
 
     // recreate window (extend to 12 elements)
-    // 0 0 0 0 0 0 6 2 9 8 7 6
     w = fwindow_recreate(w,12);
+    // window[12] : {0 0 0 0 0 0 6 2 9 8 7 6}
 
     // read buffer (return pointer to aligned memory)
     float * r;
     fwindow_read(w, &r);
+    // r[12] : {0 0 0 0 0 0 6 2 9 8 7 6}
 
     // clean up allocated object
     fwindow_destroy(w);
