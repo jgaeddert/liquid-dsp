b92c1533926bff6e1d303145e2417b14fba6fe87
Joseph D. Gaeddert <jgaedder@vt.edu>
2010-06-02 11:31:40 -0400

    improving fec rep3 code

--- src/fec/src/fec_rep.c
+++ src/fec/src/fec_rep.c
@@ -82,33 +82,28 @@ void fec_rep3_decode(fec _q,
                      unsigned char *_msg_enc,
                      unsigned char *_msg_dec)
 {
-    unsigned char s0, s1, s2, a, b, c, x, y;
+    unsigned char s0, s1, s2;
     unsigned int i, num_errors=0;
     for (i=0; i<_dec_msg_len; i++) {
         s0 = _msg_enc[i];
         s1 = _msg_enc[i +   _dec_msg_len];
         s2 = _msg_enc[i + 2*_dec_msg_len];
 
-        a = s0 ^ s1;
-        b = s0 ^ s2;
-        c = s1 ^ s2;
+        //  s0  s1  s2  |   y   e
+        //  ------------+---------
+        //  0   0   0   |   0   0
+        //  0   0   1   |   0   1
+        //  0   1   0   |   0   1
+        //  0   1   1   |   1   1
+        //  1   0   0   |   0   1
+        //  1   0   1   |   1   1
+        //  1   1   0   |   1   1
+        //  1   1   1   |   1   0
 
-        x = a | b | c;
-        y = s0 ^ s1 ^ s2;
-        num_errors += x ? 1 : 0;
-
-        _msg_dec[i] = x ^ y;
-
-        // TODO : see if there a simpler way
-        //  s0  s1  s2  y
-        //  0   0   0   0
-        //  0   0   1   0
-        //  0   1   0   0
-        //  0   1   1   1
-        //  1   0   0   0
-        //  1   0   1   1
-        //  1   1   0   1
-        //  1   1   1   1
+        _msg_dec[i] = (s0 & s1) | (s0 & s2) | (s1 & s2);
+    
+        //num_errors += (s0 ^ s1) | (s0 ^ s2) | (s1 ^ s2) ? 1 : 0;
+        num_errors += 0;
     }
     //return num_errors;
 }
