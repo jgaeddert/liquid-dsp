059a9531710ce5893e541b7053e23d6c7563896f
Joseph D. Gaeddert <jgaedder@vt.edu>
2010-06-07 10:44:03 -0400

    normalizing number of window benchmark iterations

--- src/buffer/bench/window_benchmark.h
+++ src/buffer/bench/window_benchmark.h
@@ -1,22 +1,45 @@
+/*
+ * Copyright (c) 2007, 2008, 2009, 2010 Joseph Gaeddert
+ * Copyright (c) 2007, 2008, 2009, 2010 Virginia Polytechnic
+ *                                      Institute & State University
+ *
+ * This file is part of liquid.
+ *
+ * liquid is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * liquid is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with liquid.  If not, see <http://www.gnu.org/licenses/>.
+ */
 #ifndef __LIQUID_WINDOW_BENCHMARK_H__
 #define __LIQUID_WINDOW_BENCHMARK_H__
 
 #include <sys/resource.h>
 #include "liquid.h"
 
-#define WINDOW_BENCH_API(N)              \
+#define WINDOW_BENCH_API(N)             \
 (   struct rusage *_start,              \
     struct rusage *_finish,             \
     unsigned long int *_num_iterations) \
 { window_bench(_start, _finish, _num_iterations, N); }
 
 // Helper function to keep code base small
-void window_bench(
-    struct rusage *_start,
-    struct rusage *_finish,
-    unsigned long int *_num_iterations,
-    unsigned int _n)
+void window_bench(struct rusage *_start,
+                  struct rusage *_finish,
+                  unsigned long int *_num_iterations,
+                  unsigned int _n)
 {
+    // normalize number of iterations
+    *_num_iterations /= 4;
+    if (*_num_iterations < 1) *_num_iterations = 1;
+
     // initialize port
     cfwindow w = cfwindow_create(_n);
 
