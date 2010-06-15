60a7039bef043242e97c80a8f710efbfe95bfa2a
Joseph D. Gaeddert <jgaedder@vt.edu>
2010-06-14 20:59:50 -0400

    fixing interface to comply with new APIs

--- doc/listings/agc.example.c
+++ doc/listings/agc.example.c
@@ -1,16 +1,16 @@
 #include <liquid/liquid.h>
 // ...
 {
-    agc p = agc_create();       // create object
-    agc_set_target(p,1.0f);     // set target energy level
-    agc_set_bandwidth(p,1e-3f); // set loop filter bandwidth
+    agc_rrrf q = agc_rrrf_create();     // create object
+    agc_rrrf_set_target(q,1.0f);        // set target energy level
+    agc_rrrf_set_bandwidth(q,1e-3f);    // set loop filter bandwidth
 
-    float x;                    // input
-    float y;                    // output
+    float x;                            // input sample
+    float y;                            // output sample
 
     // ...
 
-    agc_execute(p, x, &y);      // repeat as necessary
+    agc_rrrf_execute(q, x, &y);         // repeat as necessary
 
-    agc_destroy(p);             // clean it up
+    agc_rrrf_destroy(q);                // clean it up
 }
