efe65048a7145a6c67558ab39a66349a9e25ba69
Joseph D. Gaeddert <jgaedder@vt.edu>
2010-06-21 15:56:44 -0400

    decim object now uses internal structured dot product

--- src/filter/src/decim.c
+++ src/filter/src/decim.c
@@ -1,6 +1,7 @@
 /*
- * Copyright (c) 2007, 2009 Joseph Gaeddert
- * Copyright (c) 2007, 2009 Virginia Polytechnic Institute & State University
+ * Copyright (c) 2007, 2008, 2009, 2010 Joseph Gaeddert
+ * Copyright (c) 2007, 2008, 2009, 2010 Virginia Polytechnic
+ *                                      Institute & State University
  *
  * This file is part of liquid.
  *
@@ -19,7 +20,9 @@
  */
 
 //
-// Decimator
+// decim.c
+//
+// finite impulse response decimator object definitions
 //
 
 #include <stdio.h>
@@ -41,39 +44,62 @@ struct DECIM(_s) {
     unsigned int D;
 
     WINDOW() w;
-    //DOTPROD() dp;
+    DOTPROD() dp;
 
 #if 0
     fir_prototype p;    // prototype object
 #endif
 };
 
-DECIM() DECIM(_create)(unsigned int _D, TC *_h, unsigned int _h_len)
+// create decimator object
+//  _D      :   decimation factor
+//  _h      :   filter coefficients array pointer
+//  _h_len  :   length of filter
+DECIM() DECIM(_create)(unsigned int _D,
+                       TC *_h,
+                       unsigned int _h_len)
 {
+    // validate input
+    if (_h_len == 0) {
+        fprintf(stderr,"error: decim_xxxt_create(), filter length must be greater than zero\n");
+        exit(1);
+    } else if (_D == 0) {
+        fprintf(stderr,"error: decim_xxxt_create(), decimation factor must be greater than zero\n");
+        exit(1);
+    }
+
     DECIM() q = (DECIM()) malloc(sizeof(struct DECIM(_s)));
     q->h_len = _h_len;
+    q->D = _D;
     q->h = (TC*) malloc((q->h_len)*sizeof(TC));
+
+    // create window (internal buffer)
+    q->w = WINDOW(_create)(q->h_len);
 
     // load filter in reverse order
     unsigned int i;
     for (i=0; i<q->h_len; i++)
         q->h[i] = _h[_h_len-i-1];
 
-    q->D = _D;
+    // create dot product object
+    q->dp = DOTPROD(_create)(q->h, q->h_len);
 
-    q->w = WINDOW(_create)(q->h_len);
-    WINDOW(_clear)(q->w);
+    // reset filter state (clear buffer)
+    DECIM(_clear)(q);
 
     return q;
 }
 
+// destroy decimator object
 void DECIM(_destroy)(DECIM() _q)
 {
     WINDOW(_destroy)(_q->w);
+    DOTPROD(_destroy)(_q->dp);
     free(_q->h);
     free(_q);
 }
 
+// print decimator object internals
 void DECIM(_print)(DECIM() _q)
 {
     printf("DECIM() [%u] :\n", _q->D);
@@ -81,20 +107,39 @@ void DECIM(_print)(DECIM() _q)
     WINDOW(_print)(_q->w);
 }
 
+// clear decimator object
 void DECIM(_clear)(DECIM() _q)
 {
     WINDOW(_clear)(_q->w);
 }
 
-void DECIM(_execute)(DECIM() _q, TI *_x, TO *_y, unsigned int _index)
+// execute decimator
+//  _q      :   decimator object
+//  _x      :   input sample array
+//  _y      :   output sample pointer
+//  _index  :   output sample phase
+void DECIM(_execute)(DECIM() _q,
+                     TI *_x,
+                     TO *_y,
+                     unsigned int _index)
 {
+    // validate input
+    if (_index >= _q->D) {
+        fprintf(stderr,"error: decim_xxxt_execute(), output sample phase exceeds decimation factor\n");
+        exit(1);
+    }
+
     TI * r; // read pointer
     unsigned int i;
     for (i=0; i<_q->D; i++) {
         WINDOW(_push)(_q->w, _x[i]);
+
         if (i==_index) {
+            // read buffer (retrieve pointer to aligned memory array)
             WINDOW(_read)(_q->w, &r);
-            DOTPROD(_run4)(_q->h, r, _q->h_len, _y);
+
+            // execute dot product
+            DOTPROD(_execute)(_q->dp, r, _y);
         }
     }
 }
