91a7470d6e43b39dad32cf00a832d67e6814b589
Joseph D. Gaeddert <jgaedder@vt.edu>
2010-06-30 09:23:44 -0400

    cleaning up group delay calculation

--- src/filter/src/group_delay.c
+++ src/filter/src/group_delay.c
@@ -1,6 +1,7 @@
 /*
- * Copyright (c) 2007, 2009 Joseph Gaeddert
- * Copyright (c) 2007, 2009 Virginia Polytechnic Institute & State University
+ * Copyright (c) 2007, 2008, 2009, 2010 Joseph Gaeddert
+ * Copyright (c) 2007, 2008, 2009, 2010 Virginia Polytechnic
+ *                                      Institute & State University
  *
  * This file is part of liquid.
  *
@@ -25,13 +26,22 @@
 #include "liquid.internal.h"
 
 
-// Compute group delay for an FIR filter
-//  _h      : filter coefficients array
+// Compute group delay for a FIR filter
+//  _h      : filter coefficients array [size: _n x 1]
 //  _n      : filter length
 //  _fc     : frequency at which delay is evaluated (-0.5 < _fc < 0.5)
-float fir_group_delay(float * _h, unsigned int _n, float _fc)
+float fir_group_delay(float * _h,
+                      unsigned int _n,
+                      float _fc)
 {
-    // TODO: validate input
+    // validate input
+    if (_n == 0) {
+        fprintf(stderr,"error: fir_group_delay(), length must be greater than zero\n");
+        exit(1);
+    } else if (_fc < -0.5 || _fc > 0.5) {
+        fprintf(stderr,"error: fir_group_delay(), _fc must be in [-0.5,0.5]\n");
+        exit(1);
+    }
 
     unsigned int i;
     float complex t0=0.0f;
@@ -44,13 +54,29 @@ float fir_group_delay(float * _h, unsigned int _n, float _fc)
     return crealf(t0/t1);
 }
 
+// Compute group delay for an IIR filter
+//  _b      : filter coefficients array (numerator), [size: _nb x 1]
+//  _nb     : filter length (numerator)
+//  _a      : filter coefficients array (denominator), [size: _na x 1]
+//  _na     : filter length (denominator)
+//  _fc     : frequency at which delay is evaluated (-0.5 < _fc < 0.5)
 float iir_group_delay(float * _b,
                       unsigned int _nb,
                       float * _a,
                       unsigned int _na,
                       float _fc)
 {
-    // TODO : validate input
+    // validate input
+    if (_nb == 0) {
+        fprintf(stderr,"error: iir_group_delay(), numerator length must be greater than zero\n");
+        exit(1);
+    } else if (_na == 0) {
+        fprintf(stderr,"error: iir_group_delay(), denominator length must be greater than zero\n");
+        exit(1);
+    } else if (_fc < -0.5 || _fc > 0.5) {
+        fprintf(stderr,"error: iir_group_delay(), _fc must be in [-0.5,0.5]\n");
+        exit(1);
+    }
 
     // compute c = conv(b,fliplr(a))
     //         c(z) = b(z)*a(1/z)*z^(-_na)
@@ -66,6 +92,10 @@ float iir_group_delay(float * _b,
         }
     }
 
+    // compute 
+    //      sum(c[i] * exp(j 2 pi fc i) * i)
+    //      --------------------------------
+    //      sum(c[i] * exp(j 2 pi fc i))
     float complex t0=0.0f;
     float complex t1=0.0f;
     for (i=0; i<nc; i++) {
@@ -73,10 +103,12 @@ float iir_group_delay(float * _b,
         t1 += c[i] * cexpf(_Complex_I*2*M_PI*_fc*i);
     }
 
+    // prevent divide-by-zero (check magnitude for tolerance range)
     float tol = 1e-5f;
     if (cabsf(t1)<tol)
         return 0.0f;
 
+    // return result, scaled by length of denominator
     return crealf(t0/t1) - (_na - 1);
 }
 
