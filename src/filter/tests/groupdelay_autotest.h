64d43706454396cd28d438fe1ddc2028a2e1bac7
Joseph D. Gaeddert <jgaedder@vt.edu>
2010-06-30 13:48:16 -0400

    adding iir gropu delay autotest

--- src/filter/tests/groupdelay_autotest.h
+++ src/filter/tests/groupdelay_autotest.h
@@ -25,6 +25,9 @@
 #include "autotest/autotest.h"
 #include "liquid.h"
 
+//
+// AUTOTEST : fir group delay, n=3
+//
 void autotest_fir_groupdelay_n3()
 {
     // create coefficients array
@@ -63,6 +66,50 @@ void autotest_fir_groupdelay_n3()
 
     // destroy filter
     firfilt_rrrf_destroy(filter);
+}
+
+
+//
+// AUTOTEST : iir group delay, n=3
+//
+void autotest_iir_groupdelay_n3()
+{
+    // create coefficients array
+    float b[3] = {0.20657210,  0.41314420, 0.20657210};
+    float a[3] = {1.00000000, -0.36952737, 0.19581573};
+
+    float tol = 1e-3f;
+    unsigned int i;
+
+    // create testing vectors
+    float fc[4] = { 0.000,
+                    0.125,
+                    0.250,
+                    0.375};
+    
+    float g0[4] = { 0.973248939389634,
+                    1.366481121240365,
+                    1.227756735863196,
+                    0.651058521306726};
+
+    // run tests
+    float g;
+    for (i=0; i<4; i++) {
+        g = iir_group_delay(b, 3, a, 3, fc[i]);
+        CONTEND_DELTA( g, g0[i], tol );
+    }
+
+    // create filter
+    iirfilt_rrrf filter = iirfilt_rrrf_create(b,3,a,3);
+
+    // run tests again
+    for (i=0; i<4; i++) {
+        g = iirfilt_rrrf_groupdelay(filter, fc[i]);
+        CONTEND_DELTA( g, g0[i], tol );
+    }
+
+    // destroy filter
+    iirfilt_rrrf_destroy(filter);
 }
 
 #endif // __LIQUID_GROUPDELAY_AUTOTEST_H__
