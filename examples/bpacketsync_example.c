5697bdeba16fb05e23d3680df65a2a0d116651d2
Joseph Gaeddert <jgaedder@vt.edu>
2011-02-01 13:29:05 -0500

    bpacketsync_example: adding delay

--- examples/bpacketsync_example.c
+++ examples/bpacketsync_example.c
@@ -17,6 +17,14 @@ int callback(unsigned char * _payload,
     printf("callback invoked, payload (%u bytes) : %s\n",
             _payload_len,
             _payload_valid ? "valid" : "INVALID!");
+
+    // copy data if valid
+    if (_payload_valid) {
+        unsigned char * msg_dec = (unsigned char*) _userdata;
+
+        memmove(msg_dec, _payload, _payload_len*sizeof(unsigned char));
+    }
+
     return 0;
 }
 
@@ -27,6 +35,7 @@ void usage()
     printf("bpacketsync_example [options]\n");
     printf("  u/h   : print usage\n");
     printf("  n     : input data size (number of uncoded bytes): 8 default\n");
+    printf("  d     : received sequence delay [bits], default: 13\n");
     printf("  v     : data integrity check: crc32 default\n");
     // print all available CRC schemes
     for (i=0; i<LIQUID_NUM_CRC_SCHEMES; i++)
@@ -43,23 +52,18 @@ int main(int argc, char*argv[]) {
     // options
     unsigned int n=8;                   // original data message length
     crc_scheme check = CRC_32;          // data integrity check
-    fec_scheme fec0 = FEC_HAMMING74;    // inner code
+    fec_scheme fec0 = FEC_HAMMING128;   // inner code
     fec_scheme fec1 = FEC_NONE;         // outer code
+    unsigned int delay = 13;            // number of bits in delay
 
     // read command-line options
     int dopt;
-    while((dopt = getopt(argc,argv,"uhn:v:c:k:")) != EOF){
+    while((dopt = getopt(argc,argv,"uhn:d:v:c:k:")) != EOF){
         switch (dopt) {
         case 'h':
         case 'u': usage(); return 0;
-        case 'n':
-            n = atoi(optarg);
-            if (n < 1) {
-                printf("error: packet length must be positive\n");
-                usage();
-                exit(-1);
-            }
-            break;
+        case 'n': n = atoi(optarg);     break;
+        case 'd': delay = atoi(optarg); break;
         case 'v':
             // data integrity check
             check = liquid_getopt_str2crc(optarg);
@@ -90,22 +94,33 @@ int main(int argc, char*argv[]) {
         }
     }
 
-    // TODO: validate input
+    // validate input
+    if (n == 1) {
+        fprintf(stderr,"error: %s, packet length must be greater than zero\n", argv[0]);
+        exit(1);
+    }
 
     // create packet generator
     bpacketgen pg = bpacketgen_create(0, n, check, fec0, fec1);
-
-    // create packet synchronizer
-    bpacketsync ps = bpacketsync_create(0, callback, NULL);
+    bpacketgen_print(pg);
 
     unsigned int i;
+
+    // compute packet length
     unsigned int k = bpacketgen_get_packet_len(pg);
+
+    unsigned int dbytes = delay / 8;    // number of bytes in delay
+    unsigned int dbits = delay % 8;     // number of additional bits in delay
+    unsigned int w = k + dbytes + (dbits != 0);
 
     // initialize arrays
     unsigned char msg_org[n];   // original message
     unsigned char msg_enc[k];   // encoded message
-    unsigned char msg_rec[k];   // recieved message
+    unsigned char msg_rec[w];   // recieved message
     unsigned char msg_dec[n];   // decoded message
+
+    // create packet synchronizer
+    bpacketsync ps = bpacketsync_create(0, callback, (void*)msg_dec);
 
     // initialize original data message
     for (i=0; i<n; i++)
@@ -115,13 +130,26 @@ int main(int argc, char*argv[]) {
     bpacketgen_encode(pg,msg_org,msg_enc);
 
     // add delay and error(s)
-    memmove(msg_rec, msg_enc, k*sizeof(unsigned char));
-    //msg_rec[0] ^= 0x01;
+    for (i=0; i<dbytes; i++)
+        msg_rec[i] = rand() & 0xff;
+    liquid_rmemmove(&msg_rec[dbytes], msg_enc, k*sizeof(unsigned char), dbits);
+#if 0
+    // add random errors
+    for (i=0; i<w; i++) {
+        if (randf() < 0.02f)
+            msg_rec[i] ^= 1 << (rand()%8);
+    }
+#endif
 
     // run packet synchronizer
-    for (i=0; i<k; i++) {
+    for (i=0; i<w; i++)
         bpacketsync_execute(ps, msg_rec[i]);
-    }
+
+    // count errors
+    unsigned int num_bit_errors = 0;
+    for (i=0; i<n; i++)
+        num_bit_errors += count_bit_errors(msg_org[i], msg_dec[i]);
+    printf("number of bit errors received:    %4u / %4u\n", num_bit_errors, n*8);
 
     // clean up allocated objects
     bpacketgen_destroy(pg);
