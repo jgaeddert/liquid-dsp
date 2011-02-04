b82b59064416af498bcc0d0100004d2f697e75a6
Joseph Gaeddert <jgaedder@vt.edu>
2011-02-04 15:45:28 -0500

    ofdmframesync_example: adding multi-path channel

--- examples/ofdmframesync_example.c
+++ examples/ofdmframesync_example.c
@@ -16,7 +16,7 @@ static int callback(float complex * _X,
                     unsigned int _n,
                     void * _userdata);
 
-int main() {
+int main(int argc, char*argv[]) {
     srand(time(NULL));
 
     // options
@@ -25,24 +25,28 @@ int main() {
     unsigned int num_symbols_S0 = 2;    // number of S0 symbols
     unsigned int num_symbols_S1 = 2;    // number of S0 symbols
     unsigned int num_symbols_data = 8;  // number of data symbols
+    unsigned int hc_len = 4;            // channel filter length
+    float hc_std = 0.10f;               // channel filter standard deviation
     modulation_scheme ms = MOD_QAM;
     unsigned int bps = 4;
-    float SNRdB = 30.0f;    // signal-to-noise ratio [dB]
-    float phi   = 0.0f;     // phase offset
-    float dphi  = 0.005f;   // frequency offset
+    float noise_floor = -30.0f;         // noise floor [dB]
+    float SNRdB = 20.0f;                // signal-to-noise ratio [dB]
+    float phi   = 0.0f;                 // carrier phase offset
+    float dphi  = 0.005f;               // carrier frequency offset
+
+    // validate input
+    if (hc_len < 1) {
+        fprintf(stderr,"error: %s, channel filter must have at least 1 tap\n", argv[0]);
+        exit(1);
+    }
 
     // derived values
     unsigned int frame_len = M + cp_len;
-#if 0
-    unsigned int num_symbols = num_symbols_S0 +
-                               num_symbols_S1 +
-                               num_symbols_data;
-    unsigned int num_samples = frame_len*num_symbols;
-#else
     unsigned int num_samples = (num_symbols_S0 + num_symbols_S1)*M +
                                 num_symbols_data * frame_len;
-#endif
-    float nstd = powf(10.0f, -SNRdB/10.0f);
+    float nstd = powf(10.0f, noise_floor/10.0f);
+    float gamma = powf(10.0f, (SNRdB + noise_floor)/10.0f);
+    printf("gamma : %f\n", gamma);
 
     // initialize subcarrier allocation
     unsigned int p[M];
@@ -90,17 +94,30 @@ int main() {
         n += frame_len;
     }
 
+    // create channel filter (random delay taps)
+    float complex hc[hc_len];
+    for (i=0; i<hc_len; i++) {
+        hc[i] = (i==0) ? 1.0f : hc_std*randnf()*cexpf(_Complex_I*2*M_PI*randf());
+        hc[i] *= gamma;
+    }
+    firfilt_cccf fc = firfilt_cccf_create(hc,hc_len);
+    // fill with noise
+    for (i=0; i<hc_len; i++)
+        firfilt_cccf_push(fc, nstd*randnf()*cexpf(_Complex_I*2*M_PI*randf()));
+
     // add noise, carrier offset
     for (i=0; i<num_samples; i++) {
+        // push through channel filter
+        firfilt_cccf_push(fc, y[i]);
+        firfilt_cccf_execute(fc, &y[i]);
+
         // add carrier offset
         y[i] *= cexpf(_Complex_I*(phi + dphi*i));
-
-        // add channel gain
-        y[i] *= 0.1f;
 
         // add noise
         y[i] += nstd*randnf()*cexp(_Complex_I*2*M_PI*randf());
     }
+    firfilt_cccf_destroy(fc);
 
     // push noise into synchronizer
     unsigned int d=1000;
@@ -129,17 +146,25 @@ int main() {
     fprintf(fid,"M = %u;\n", M);
     fprintf(fid,"cp_len = %u;\n", cp_len);
     fprintf(fid,"num_samples = %u;\n", num_samples);
-    fprintf(fid,"y = zeros(1,num_samples);\n");
+    fprintf(fid,"noise_floor = %f;\n", noise_floor);
+    fprintf(fid,"SNRdB = %f;\n", SNRdB);
 
-    //
+    // 
+    fprintf(fid,"y = zeros(1,num_samples);\n");
     for (i=0; i<num_samples; i++)
         fprintf(fid,"y(%3u) = %12.4e + j*%12.4e;\n", i+1, crealf(y[i]), cimagf(y[i]));
+
+    // save channel response
+    fprintf(fid,"hc_len = %u;\n", hc_len);
+    fprintf(fid,"hc = zeros(1,hc_len);\n");
+    for (i=0; i<hc_len; i++)
+        fprintf(fid,"hc(%3u) = %12.4e + j*%12.4e;\n", i+1, crealf(hc[i]), cimagf(hc[i]));
 
     fprintf(fid,"\n\n");
     fprintf(fid,"%% compute spectral periodigram\n");
     fprintf(fid,"nfft=256;\n");
     fprintf(fid,"f=[0:(nfft-1)]/nfft - 0.5;\n");
-    //fprintf(fid,"H = 20*log10(abs(fftshift(fft(h,nfft))));\n");
+    fprintf(fid,"Hc = 10*log10(abs(fftshift(fft(hc,nfft))));\n");
     fprintf(fid,"Y = zeros(1,nfft);\n");
     fprintf(fid,"v0=1; v1=v0+round(nfft); k=0;\n");
     fprintf(fid,"while v1 <= num_samples,\n");
@@ -148,11 +173,10 @@ int main() {
     fprintf(fid,"    v1 = v1 + round(nfft/4);\n");
     fprintf(fid,"    k = k+1;\n");
     fprintf(fid,"end;\n");
-    fprintf(fid,"Y = 20*log10(abs(fftshift(Y/k)));\n");
+    fprintf(fid,"Y  = 10*log10(abs(fftshift(Y/k)));\n");
     fprintf(fid,"figure;\n");
-    //fprintf(fid,"plot(f,Y,f,H);\n");
-    fprintf(fid,"plot(f,Y);\n");
-    fprintf(fid,"axis([-0.5 0.5 -40 10]);\n");
+    fprintf(fid,"plot(f,Y,f,Hc);\n");
+    fprintf(fid,"axis([-0.5 0.5 (noise_floor-5) (noise_floor+SNRdB+5)]);\n");
     fprintf(fid,"xlabel('Normalized Frequency');\n");
     fprintf(fid,"ylabel('Power Spectral Density [dB]');\n");
     fprintf(fid,"title('Multipath channel response');\n");
