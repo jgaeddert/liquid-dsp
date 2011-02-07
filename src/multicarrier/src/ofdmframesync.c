11dcea965e47dd0516db4f404d468d9fe44e0bcb
Joseph Gaeddert <jgaedder@vt.edu>
2011-02-07 11:36:17 -0500

    ofdmframesync: ensuring timing backoff does not exceed cyclic prefix length

--- src/multicarrier/src/ofdmframesync.c
+++ src/multicarrier/src/ofdmframesync.c
@@ -175,7 +175,7 @@ ofdmframesync ofdmframesync_create(unsigned int _M,
     q->B  = (float complex*) malloc((q->M)*sizeof(float complex));
 
     // timing backoff
-    q->backoff = 2;
+    q->backoff = q->cp_len < 2 ? q->cp_len : 2;
     float phi = (float)(q->backoff)*2.0f*M_PI/(float)(q->M);
     unsigned int i;
     for (i=0; i<q->M; i++)
