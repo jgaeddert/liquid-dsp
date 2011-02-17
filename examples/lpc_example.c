b1937b292957afff7883a7dae2cb4159eb01527b
Joseph Gaeddert <jgaedder@vt.edu>
2011-02-17 09:46:58 -0500

    lpc|example: adding prediction output, prediction error, auto-corr

--- examples/lpc_example.c
+++ examples/lpc_example.c
@@ -13,10 +13,10 @@
 int main() {
     // options
     unsigned int n = 200;   // input sequence length
-    unsigned int p = 3;     // prediction filter order
+    unsigned int p = 4;     // prediction filter order
 
     // original filter
-#if 0
+#if 1
     // autoregressive moving average filter
     // ./examples/iirdes_example -t butter -n3 -otf -f0.2
     float b[4] = {0.01809893, 0.05429679, 0.05429679, 0.01809893};
@@ -24,7 +24,7 @@ int main() {
 #else
     // autoregressive filter
     float b[4] = {1.0f, 0.0f, 0.0f, 0.0f};
-    float a[4] = {1.0f, 0.5f, 0.4f, -0.3f};
+    float a[4] = {1.0f, 0.5f, 0.4f, 0.3f};
 #endif
 
     // create filter object
@@ -55,9 +55,44 @@ int main() {
     // run linear prediction algorithm
     liquid_lpc(y,n,p,a_hat,g_hat);
 
+    // run prediction filter
+    float a_lpc[p+1];
+    float b_lpc[p+1];
+    for (i=0; i<p+1; i++) {
+        a_lpc[i] = (i==0) ? 1.0f : 0.0f;
+        b_lpc[i] = (i==0) ? 0.0f : -a_hat[i];
+    }
+    f = iirfilt_rrrf_create(b_lpc,p+1, a_lpc,p+1);
+    iirfilt_rrrf_print(f);
+    float y_hat[n];
+    for (i=0; i<n; i++)
+        iirfilt_rrrf_execute(f, y[i], &y_hat[i]);
+    iirfilt_rrrf_destroy(f);
+
+    // compute prediction error
+    float err[n];
+    for (i=0; i<n; i++)
+        err[i] = y[i] - y_hat[i];
+
+    // compute autocorrelation of prediction error
+    float lag[n];
+    float rxx[n];
+    for (i=0; i<n; i++) {
+        lag[i] = (float)i;
+        rxx[i] = 0.0f;
+        unsigned int j;
+        for (j=i; j<n; j++)
+            rxx[i] += err[j] * err[j-i];
+    }
+    float rxx0 = rxx[0];
+    for (i=0; i<n; i++)
+        rxx[i] /= rxx0;
+
     // print results
     for (i=0; i<p+1; i++)
         printf("  a[%3u] = %12.8f, g[%3u] = %12.8f\n", i, a_hat[i], i, g_hat[i]);
+
+    printf("  prediction rmse = %12.8f\n", sqrtf(rxx0 / n));
 
     // 
     // plot results to output file
@@ -69,13 +104,27 @@ int main() {
     fprintf(fid,"\n");
     fprintf(fid,"p=%u;\n", p);
     fprintf(fid,"n=%u;\n",n);
-    fprintf(fid,"x=zeros(1,n);\n");
-    fprintf(fid,"y=zeros(1,n);\n");
 
+#if 0
+    fprintf(fid,"b  = zeros(1,p+1);\n");
+    fprintf(fid,"a  = zeros(1,p+1);\n");
+    for (i=0; i<p+1; i++) {
+        fprintf(fid,"b(%4u) = %12.4e;\n", i+1, b_lpc[i]);
+        fprintf(fid,"a(%4u) = %12.4e;\n", i+1, a_lpc[i]);
+    }
+#endif
+
+    fprintf(fid,"x      = zeros(1,n);\n");
+    fprintf(fid,"y      = zeros(1,n);\n");
+    fprintf(fid,"y_hat  = zeros(1,n);\n");
+    fprintf(fid,"lag    = zeros(1,n);\n");
+    fprintf(fid,"rxx    = zeros(1,n);\n");
     for (i=0; i<n; i++) {
-        //printf("%4u : %12.8f + j*%12.8f\n", i, crealf(y), cimagf(y));
         fprintf(fid,"x(%4u) = %12.4e + j*%12.4e;\n", i+1, crealf(x[i]), cimagf(x[i]));
         fprintf(fid,"y(%4u) = %12.4e + j*%12.4e;\n", i+1, crealf(y[i]), cimagf(y[i]));
+        fprintf(fid,"y_hat(%4u) = %12.4e + j*%12.4e;\n", i+1, crealf(y_hat[i]), cimagf(y_hat[i]));
+        fprintf(fid,"lag(%4u) = %12.4e + j*%12.4e;\n", i+1, crealf(lag[i]), cimagf(lag[i]));
+        fprintf(fid,"rxx(%4u) = %12.4e + j*%12.4e;\n", i+1, crealf(rxx[i]), cimagf(rxx[i]));
     }
 
     // plot output
@@ -87,6 +136,20 @@ int main() {
     fprintf(fid,"  ylabel('real');\n");
     fprintf(fid,"  legend('input','filtered output',1);\n");
     fprintf(fid,"  grid on;\n");
+
+    fprintf(fid,"figure;\n");
+    fprintf(fid,"subplot(5,1,1:3);\n");
+    fprintf(fid,"  plot(t,y,t,y_hat);\n");
+    fprintf(fid,"  ylabel('signal');\n");
+    fprintf(fid,"  legend('y','lpc estimate',1);\n");
+    fprintf(fid,"  grid on;\n");
+    fprintf(fid,"subplot(5,1,4);\n");
+    fprintf(fid,"  plot(t,y-y_hat);\n");
+    fprintf(fid,"  ylabel('error');\n");
+    fprintf(fid,"subplot(5,1,5);\n");
+    fprintf(fid,"  plot(t,rxx);\n");
+    fprintf(fid,"  xlabel('time');\n");
+    fprintf(fid,"  ylabel('r_{xx}(e)');\n");
 
     fclose(fid);
     printf("results written to %s.\n", OUTPUT_FILENAME);
