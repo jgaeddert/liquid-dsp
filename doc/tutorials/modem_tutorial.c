a5304f72a53a790d958f542fbbcec60c935c797a
Joseph Gaeddert <jgaedder@vt.edu>
2011-02-24 17:07:10 -0500

    doc: adding modem tutorial example

---
+++ doc/tutorials/modem_tutorial.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <complex.h>
+#include <math.h>
+#include <liquid/liquid.h>
+
+int main() {
+    // 
+    unsigned int bps = 2;
+    modulation_scheme ms = MOD_PSK;
+    float SNRdB = 3.0f;
+    unsigned int num_symbols = 1000;
+
+    // create the modem objects
+    modem mod   = modem_create(ms, bps);
+    modem demod = modem_create(ms, bps);
+
+    modem_print(mod);
+
+    float nstd = powf(10.0f, -SNRdB/20.0f);
+
+    unsigned int i; // modulated symbol
+    unsigned int s0; // demodulated symbol
+    unsigned int s1; // demodulated symbol
+    unsigned int M = 1<<bps;
+    float complex x;
+    unsigned int num_sym_errors = 0;
+    unsigned int num_bit_errors = 0;
+
+    for (i=0; i<num_symbols; i++) {
+        
+        s0 = rand()%M;
+        modem_modulate(mod, s0, &x);
+
+        // add noise
+        x += nstd*randnf()*cexpf(_Complex_I*2*M_PI*randf());
+
+        modem_demodulate(demod, x, &s1);
+
+        num_sym_errors += s0 == s1 ? 0 : 1;
+        num_bit_errors += count_bit_errors(s0,s1);
+    }
+    printf("num sym errors: %4u / %4u\n", num_sym_errors, num_symbols);
+    printf("num bit errors: %4u / %4u\n", num_bit_errors, num_symbols*bps);
+
+
+    modem_destroy(mod);
+    modem_destroy(demod);
+
+    printf("done.\n");
+    return 0;
+}
