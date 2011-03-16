880b6f989233cd306b4cae783f22e9f55204157d
Joseph Gaeddert <jgaedder@vt.edu>
2011-03-16 14:48:00 -0400

    doc|filter: adding interpolator description

--- doc/listings/decim.example.c
+++ doc/listings/decim.example.c
@@ -5,11 +5,12 @@ int main() {
     // options
     unsigned int D = 4;         // decimation factor
     unsigned int h_len = 21;    // filter length
-    float h[h_len];             // filter coefficients
 
-    // create decimator object
+    // design filter and create decimator object
+    float h[h_len];             // filter coefficients
     decim_crcf q = decim_crcf_create(D,h,h_len);
 
+    // generate input signal and decimate
     float x[D];                 // input samples
     float y;                    // output sample
 
