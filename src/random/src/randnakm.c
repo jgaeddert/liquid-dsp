f6385b18c9978842d15dad17cce814d4def82566
Joseph Gaeddert <jgaedder@vt.edu>
2011-03-22 09:57:50 -0400

    random|nakagami-m: fixing PDF, CDF computation

--- src/random/src/randnakm.c
+++ src/random/src/randnakm.c
@@ -33,10 +33,10 @@ float randnakmf(float _m,
 {
     // validate input
     if (_m < 0.5f) {
-        fprintf(stderr,"error: randnakmf_pdf(), m cannot be less than 0.5\n");
+        fprintf(stderr,"error: randnakmf(), m cannot be less than 0.5\n");
         exit(1);
     } else if (_omega <= 0.0f) {
-        fprintf(stderr,"error: randnakmf_pdf(), omega must be greater than zero\n");
+        fprintf(stderr,"error: randnakmf(), omega must be greater than zero\n");
         exit(1);
     }
 
@@ -73,12 +73,12 @@ float randnakmf_pdf(float _x,
     if (_x <= 0.0f)
         return 0.0f;
 
-    float t0 = liquid_gammaf(_m);
-    float t1 = powf( _m/_omega, _m );
-    float t2 = powf( _x, 2*_m - 1.0f );
-    float t3 = expf( -(_m/_omega)*_x*_x );
+    float t0 = liquid_lngammaf(_m);
+    float t1 = _m * logf(_m/_omega);
+    float t2 = (2*_m - 1.0f) * logf(_x);
+    float t3 = -(_m/_omega)*_x*_x;
 
-    return 2.0f * t1 * t2 * t3 / t0;
+    return 2.0f * expf( -t0 + t1 + t2 + t3 );
 }
 
 // Nakagami-m distribution cumulative distribution function
@@ -93,17 +93,19 @@ float randnakmf_cdf(float _x,
 {
     // validate input
     if (_m < 0.5f) {
-        fprintf(stderr,"error: randnakmf_pdf(), m cannot be less than 0.5\n");
+        fprintf(stderr,"error: randnakmf_cdf(), m cannot be less than 0.5\n");
         exit(1);
     } else if (_omega <= 0.0f) {
-        fprintf(stderr,"error: randnakmf_pdf(), omega must be greater than zero\n");
+        fprintf(stderr,"error: randnakmf_cdf(), omega must be greater than zero\n");
         exit(1);
     }
 
     if (_x <= 0.0f)
         return 0.0f;
 
-    return liquid_lowergammaf(_m, _x*_x*_m/_omega) / liquid_gammaf(_m);
+    float t0 = liquid_lnlowergammaf(_m, _x*_x*_m/_omega);
+    float t1 = liquid_lngammaf(_m);
+    return expf( t0 - t1 );
 }
 
 
