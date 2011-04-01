06076f4540b6ce091d910285070fec087edf66b3
Joseph Gaeddert <jgaedder@vt.edu>
2011-04-01 16:38:20 -0400

    doc|resamp2: cleaning up interface description

---
+++ doc/listings/resamp2_crcf.example.c
@@ -0,0 +1,24 @@
+// file: doc/listings/resamp2_crcf.example.c
+
+int main() {
+    // options
+    unsigned int m = 7;         // filter semi-length
+    float As=-60.0f;            // resampling filter stop-band attenuation
+
+    // create half-band resampler
+    resamp2_crcf q = resamp2_crcf_create(m,0.0f,As);
+
+    float complex x;            // complex input
+    float complex y[2];         // output buffer
+
+    // ... initialize input ...
+    {
+        // execute half-band resampler as interpolator
+        resamp_crcf_interp_execute(q, x, y);
+    }
+
+    // ... repeat as necessary ...
+
+    // clean up allocated objects
+    resamp2_crcf_destroy(q);
+}
