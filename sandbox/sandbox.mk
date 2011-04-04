3fb588912faa935489a2f69a8acfebf869ea908f
Joseph D. Gaeddert <jgaedder@vt.edu>
2011-04-04 13:44:58 -0400

    sandbox: adding minserch tests

--- sandbox/sandbox.mk
+++ sandbox/sandbox.mk
@@ -29,6 +29,8 @@ local_src	:=				\
 	math_cexpf_test.c			\
 	math_clogf_test.c			\
 	math_csqrtf_test.c			\
+	minsearch_test.c			\
+	minsearch2_test.c			\
 	ofdmoqam_firpfbch_test.c		\
 	ofdmoqam_firpfbch_cfo_test.c		\
 	quasinewton_test.c			\
