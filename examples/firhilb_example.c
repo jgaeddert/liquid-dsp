59cb406e58faea3e86c71206cac31c391456a749
Joseph Gaeddert <jgaedder@vt.edu>
2011-04-07 14:04:31 -0400

    firhilb|example: fixing regular Hilbert transform example

--- examples/firhilb_example.c
+++ examples/firhilb_example.c
@@ -22,23 +22,29 @@ int main() {
     unsigned int m=5;               // Hilbert filter semi-length
     float As=60.0f;                 // stop-band attenuation [dB]
     float fc=0.37f;                 // signal center frequency
-    unsigned int num_samples=256;   // number of samples
+    unsigned int num_samples=64;    // number of samples
+
+    // derived values
+    unsigned int h_len = 4*m+1;
+    unsigned int N = num_samples + h_len;
 
     // create Hilbert transform object
     firhilbf q = firhilbf_create(m,As);
     firhilbf_print(q);
 
     // data arrays
-    float x[num_samples];           // real input
-    float complex y[num_samples];   // complex output
-    float z[num_samples];           // real output
+    float x[N];           // real input
+    float complex y[N];   // complex output
+    float z[N];           // real output
 
     // initialize input array
     unsigned int i;
-    for (i=0; i<num_samples; i++)
+    for (i=0; i<N; i++) {
         x[i] = cosf(2*M_PI*fc*i);
+        x[i] *= (i < num_samples) ? 1.855f*hamming(i,num_samples) : 0.0f;
+    }
 
-    for (i=0; i<num_samples; i++) {
+    for (i=0; i<N; i++) {
         // execute real-to-complex conversion
         firhilbf_r2c_execute(q, x[i], &y[i]);
 
@@ -60,28 +66,36 @@ int main() {
     fprintf(fid,"close all;\n");
     fprintf(fid,"h_len=%u;\n", 4*m+1);
     fprintf(fid,"num_samples=%u;\n", num_samples);
+    fprintf(fid,"N=%u;\n", N);
+    fprintf(fid,"t = 0:(N-1);\n");
 
-    for (i=0; i<num_samples; i++) {
+    for (i=0; i<N; i++) {
         // print results
         fprintf(fid,"x(%3u) = %12.4e;\n", i+1, x[i]);
         fprintf(fid,"y(%3u) = %12.4e + j*%12.4e;\n", i+1, crealf(y[i]), cimagf(y[i]));
         fprintf(fid,"z(%3u) = %12.4e;\n", i+1, z[i]);
     }
 
+    fprintf(fid,"figure;\n");
+    fprintf(fid,"  subplot(3,1,1); plot(t,x);\n");
+    fprintf(fid,"  subplot(3,1,2); plot(t,real(y), t,imag(y));\n");
+    fprintf(fid,"  subplot(3,1,3); plot(t,z);\n");
+
     // plot results
     fprintf(fid,"nfft=512;\n");
     fprintf(fid,"%% compute normalized windowing functions\n");
-    fprintf(fid,"wx = 1.8534/num_samples*hamming(length(x)).'; %% x window\n");
-    fprintf(fid,"wy = 1.8534/num_samples*hamming(length(y)).'; %% y window\n");
-    fprintf(fid,"X=20*log10(abs(fftshift(fft(x.*wx,nfft))));\n");
-    fprintf(fid,"Y=20*log10(abs(        (fft(y.*wy,nfft))));\n");
+    fprintf(fid,"X=20*log10(abs(fftshift(fft(x/num_samples,nfft))));\n");
+    fprintf(fid,"Y=20*log10(abs(fftshift(fft(y/num_samples,nfft))));\n");
+    fprintf(fid,"Z=20*log10(abs(fftshift(fft(z/num_samples,nfft))));\n");
     fprintf(fid,"f =[0:(nfft-1)]/nfft-0.5;\n");
-    fprintf(fid,"figure; plot(f,X,'Color',[0.5 0.5 0.5],f,Y,'LineWidth',2);\n");
+    fprintf(fid,"figure; plot(f,X,'LineWidth',1,'Color',[0.50 0.50 0.50],...\n");
+    fprintf(fid,"             f,Y,'LineWidth',2,'Color',[0.00 0.50 0.25],...\n");
+    fprintf(fid,"             f,Z,'LineWidth',1,'Color',[0.00 0.25 0.50]);\n");
     fprintf(fid,"grid on;\n");
     fprintf(fid,"axis([-0.5 0.5 -80 20]);\n");
     fprintf(fid,"xlabel('normalized frequency');\n");
     fprintf(fid,"ylabel('PSD [dB]');\n");
-    fprintf(fid,"legend('original/real','transformed/decimated',1);");
+    fprintf(fid,"legend('original/real','transformed/complex','regenerated/real',1);");
 
     fclose(fid);
     printf("results written to %s\n", OUTPUT_FILENAME);
