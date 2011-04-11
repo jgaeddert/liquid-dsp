bd9fef6cc52517b6a26f70a3c4a01d1aa2ce535d
Joseph Gaeddert <jgaedder@vt.edu>
2011-04-11 13:54:29 -0400

    framesync64 example: fixing bug in framesyncstats (SNR -> EVM)

--- examples/framesync64_example.c
+++ examples/framesync64_example.c
@@ -135,7 +135,7 @@ static int callback(unsigned char * _rx_header,
                     void * _userdata)
 {
     printf("*** callback invoked ***\n");
-    printf("    SNR                 : %12.8f dB\n", _stats.SNR);
+    printf("    error vector mag.   : %12.8f dB\n", _stats.evm);
     printf("    rssi                : %12.8f dB\n", _stats.rssi);
     printf("    mod. scheme         : %s\n", modulation_scheme_str[_stats.mod_scheme][1]);
     printf("    mod. depth          : %u\n", _stats.mod_bps);
