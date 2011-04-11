bad8764a7c00255b609db2cb3f74d1015249c69c
Joseph Gaeddert <jgaedder@vt.edu>
2011-04-11 17:19:48 -0400

    doc|gradsearch: cleaning up listing (adding utility function)

--- doc/listings/gradsearch.example.c
+++ doc/listings/gradsearch.example.c
@@ -1,28 +1,33 @@
 // file: doc/listings/gradsearch.example.c
 
+// user-defined utility function
+float myutility(void * _userdata, float * _v, unsigned int _n)
+{
+    float u = 0.0f;
+    unsigned int i;
+    for (i=0; i<_n; i++)
+        u += fabsf(_v[i]);
+    return u;
+}
+
 int main() {
     unsigned int num_parameters = 8;    // search dimensionality
     unsigned int num_iterations = 100;  // number of iterations to run
     float target_utility = 0.01f;       // target utility
 
-    float optimum_vect[num_parameters];
+    float v[num_parameters];            // optimum vector
 
-    // ...
+    // ... intialize v ...
 
     // create gradsearch object
     gradsearch gs = gradsearch_create(NULL,
-                                      optimum_vect,
+                                      v,
                                       num_parameters,
-                                      &rosenbrock,
+                                      &myutility,
                                       LIQUID_OPTIM_MINIMIZE);
 
     // execute batch search
     gradsearch_execute(gs, num_iterations, target_utility);
-
-    // execute search one iteration at a time
-    unsigned int i;
-    for (i=0; i<num_iterations; i++)
-        gradsearch_step(gs);
 
     // clean it up
     gradsearch_destroy(gs);
