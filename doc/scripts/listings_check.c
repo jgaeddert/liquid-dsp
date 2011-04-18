e9a59aa2a6a1dd3a2635f3b3c73e034ad5ee6eb1
Joseph Gaeddert <jgaedder@vt.edu>
2011-04-18 12:21:21 -0400

    doc|listings-check: script now removed "#include <liquid/liquid.h>"

--- doc/scripts/listings_check.c
+++ doc/scripts/listings_check.c
@@ -8,6 +8,8 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+#define BUFFER_LEN (256)
+
 // print usage/help message
 void usage()
 {
@@ -39,11 +41,49 @@ int main(int argc, char*argv[])
     fprintf(fid,"#include <stdlib.h>\n");
     fprintf(fid,"#include <string.h>\n");
     fprintf(fid,"#include <math.h>\n");
-    fprintf(fid,"#include <complex.h>\n");
     fprintf(fid,"#include \"liquid.h\"\n");
     fprintf(fid,"#include \"liquid.experimental.h\"\n");
     fprintf(fid,"\n");
-    fprintf(fid,"#include \"%s\"\n", input_filename);
+    
+    fprintf(fid,"//////////////////////////////////////////\n");
+    fprintf(fid,"// BEGIN INPUT FILE\n");
+    fprintf(fid,"//////////////////////////////////////////\n");
+
+    char buffer[BUFFER_LEN];    // buffer
+    char * cptr = NULL;         // readline return value
+    unsigned int n=0;           // input line number
+
+    // parse input file and strip out occurrences of "#include <liquid/liquid.h>"
+    FILE * fin = fopen(input_filename,"r");
+    if (!fin) {
+        fprintf(stderr,"error: %s, could not open '%s' for reading\n", argv[0], input_filename);
+        exit(1);
+    }
+
+    // parse input file
+    do {
+        // read line
+        cptr = fgets(buffer, BUFFER_LEN, fin);
+
+        if (cptr != NULL) {
+            // strip out certain lines
+            if (strcmp(cptr,"#include <liquid/liquid.h>\n") == 0) {
+                fprintf(fid,"// LINE (%3u) REMOVED: %s", n, cptr);
+                continue;
+            } else {
+                fprintf(fid,"%s", cptr);
+                n++;
+            }
+        } else {
+            break;
+        }
+    } while (!feof(fin));
+
+    fclose(fin);
+
+    fprintf(fid,"//////////////////////////////////////////\n");
+    fprintf(fid,"// END INPUT FILE\n");
+    fprintf(fid,"//////////////////////////////////////////\n");
 
     // close output file
     fclose(fid);
