67d6ef474784c444b305344efba90723c44c3825
Joseph Gaeddert <jgaedder@vt.edu>
2011-04-18 09:14:44 -0400

    doc|modem: adding modem example listing

---
+++ doc/listings/modem.example.c
@@ -0,0 +1,29 @@
+// file: doc/listings/agc.example.c
+
+int main() {
+    // create mod/demod objects
+    unsigned int bps=2;
+    modulation_scheme ms = LIQUID_MODEM_PSK;
+
+    // create the modem objects
+    modem mod   = modem_create(ms, bps);    // modulator
+    modem demod = modem_create(ms, bps);    // demodulator
+    modem_print(mod);
+
+    unsigned int sym_in;    // input symbol
+    float complex x;        // modulated sample
+    unsigned int sym_out;   // demodulated symbol
+
+    // ...repeat as necessary...
+    {
+        // modulate symbol
+        modem_modulate(mod, sym_in, &x);
+
+        // demodulate symbol
+        modem_demodulate(demod, x, &sym_out);
+    }
+
+    // destroy modem objects
+    modem_destroy(mod);
+    modem_destroy(demod);
+}
