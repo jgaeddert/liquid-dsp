7352b121c12f0b11e5c4985492bfc1ff8bfd7c57
Joseph Gaeddert <jgaedder@vt.edu>
2011-04-28 14:27:01 -0400

    autotest: adding null autotest (pass unconditionally)

---
+++ autotest/null_autotest.h
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) 2011 Joseph Gaeddert
+ * Copyright (c) 2011 Virginia Polytechnic Institute & State University
+ *
+ * This file is part of liquid.
+ *
+ * liquid is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * liquid is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with liquid.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "autotest.h"
+
+void autotest_null()
+{
+    // pass unconditionally
+    AUTOTEST_PASS();
+}
+
