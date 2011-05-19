423305edddcab1c6741ee05008ba7d9abd2ebc17
Joseph Gaeddert <jgaedder@vt.edu>
2011-05-19 17:01:32 -0400

    doc|interp: cleaning up interpolator figure script

--- doc/src/filter_interp_crcf.c
+++ doc/src/filter_interp_crcf.c
@@ -13,39 +13,35 @@
 
 int main() {
     // options
-    unsigned int k=4;                   // samples/symbol
+    unsigned int M=4;                   // samples/symbol
     unsigned int m=3;                   // filter delay
-    float beta = 0.3f;                  // filter excess bandwidth
+    float As = 40.0f;                   // filter stop-band attenuation
     unsigned int num_data_symbols=32;   // number of data symbols
 
     // derived values
-    unsigned int h_len = 2*k*m+1;
     unsigned int num_symbols = num_data_symbols + 2*m;
-    unsigned int num_samples = k*num_symbols;
+    unsigned int num_samples = M*num_symbols;
 
-    // design filter and create interpolator
-    float h[h_len];
-    design_rcos_filter(k,m,beta,0.0f,h);
-    interp_crcf q = interp_crcf_create(k,h,h_len);
+    // create interpolator
+    interp_crcf q = interp_crcf_create_prototype(M,m,As);
 
     // generate input signal and interpolate
     float complex x[num_symbols];
     float complex y[num_samples];
     unsigned int i;
-    float theta;
-    for (i=0; i<num_data_symbols; i++) {
-        theta = M_PI * (0.5f*(rand()%4) + 0.25f);
-        x[i] = sqrtf(2.0f) * cexpf(_Complex_I * theta);
-    }
-    for ( ; i<num_symbols; i++)
-        x[i] = 0.0f;
-
-    unsigned int ny=0;
     for (i=0; i<num_symbols; i++) {
-        interp_crcf_execute(q, x[i], &y[ny]);
-        ny += k;
+        float complex sym = (rand() % 2 ? 1.0f : -1.0f) +
+                            (rand() % 2 ? 1.0f : -1.0f)*_Complex_I;
+
+        x[i] = (i < num_data_symbols) ? sym : 0.0f;
     }
 
+    for (i=0; i<num_symbols; i++)
+        interp_crcf_execute(q, x[i], &y[i*M]);
+
+    // destroy interpolator object
+    interp_crcf_destroy(q);
+    
     // open output file
     FILE * fid = fopen(OUTPUT_FILENAME,"w");
 
@@ -69,7 +65,7 @@ int main() {
     fprintf(fid,"plot '-' using 1:2 with linespoints pointtype 7 pointsize 0.3 linecolor rgb '%s' linewidth 1 title 'interp',\\\n",LIQUID_DOC_COLOR_GRAY);
     fprintf(fid,"     '-' using 1:2 with points pointtype 7 linecolor rgb '%s' title 'symbols'\n",LIQUID_DOC_COLOR_GREEN);
     for (i=0; i<num_samples; i++)
-        fprintf(fid,"%12.4e %12.4e\n", (float)i / k - (float)m, crealf(y[i]));
+        fprintf(fid,"%12.4e %12.4e\n", (float)i / M - (float)m, crealf(y[i]));
     fprintf(fid,"e\n");
 
     for (i=0; i<num_symbols; i++)
@@ -81,7 +77,7 @@ int main() {
     fprintf(fid,"plot '-' using 1:2 with linespoints pointtype 7 pointsize 0.3 linecolor rgb '%s' linewidth 1 title 'interp',\\\n",LIQUID_DOC_COLOR_GRAY);
     fprintf(fid,"     '-' using 1:2 with points pointtype 7 linecolor rgb '%s' title 'symbols'\n", LIQUID_DOC_COLOR_RED);
     for (i=0; i<num_samples; i++)
-        fprintf(fid,"%12.4e %12.4e\n", (float)i / k - (float)m, cimagf(y[i]));
+        fprintf(fid,"%12.4e %12.4e\n", (float)i / M - (float)m, cimagf(y[i]));
     fprintf(fid,"e\n");
     for (i=0; i<num_symbols; i++)
         fprintf(fid,"%4u %12.4e\n", i, cimagf(x[i]));
@@ -90,7 +86,5 @@ int main() {
     // close output file
     fclose(fid);
 
-    interp_crcf_destroy(q);
-    
     return 0;
 }
