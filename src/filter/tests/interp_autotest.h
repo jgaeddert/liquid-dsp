24ddb5ad508c9cff90c577ca19a0a78f982261e9
Joseph Gaeddert <jgaedder@vt.edu>
2011-05-20 10:33:51 -0400

    interp|autotest: initializing to random filter
      * eliminates symmetry
      * this helps guarantee functional correctness

--- src/filter/tests/interp_autotest.h
+++ src/filter/tests/interp_autotest.h
@@ -18,9 +18,6 @@
  * along with liquid.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#ifndef __INTERP_AUTOTEST_H__
-#define __INTERP_AUTOTEST_H__
-
 #include "autotest/autotest.h"
 #include "liquid.h"
 
@@ -29,25 +26,45 @@
 //
 void autotest_interp_rrrf_generic()
 {
-    float h[] = {0, 0.25, 0.5, 0.75, 1.0, 0.75, 0.5, 0.25, 0}; 
-    unsigned int h_len = 9; // filter length
+    float h[9] = {
+      -0.2762293319046737,
+       1.4757679031218007,
+       0.1432569489572376,
+      -0.2142368750177835,
+       1.3471241294836864,
+       0.1166010284926269,
+       0.0536534505390281,
+       0.1412672462812405,
+      -0.0991854372394269};
+
     unsigned int M = 4;     // interp factor
-    interp_rrrf q = interp_rrrf_create(4,h,h_len);
+    interp_rrrf q = interp_rrrf_create(M,h,9);
 
     float x[] = {1.0, -1.0, 1.0, 1.0};
     float y[16];
     float test[16] = {
-         0.00,  0.25,  0.50,  0.75,
-         1.00,  0.50,  0.00, -0.50,
-        -1.00, -0.50,  0.00,  0.50,
-         1.00,  1.00,  1.00,  1.00};
+      -0.2762293319046737,
+       1.4757679031218007,
+       0.1432569489572376,
+      -0.2142368750177835,
+       1.6233534613883602,
+      -1.3591668746291738,
+      -0.0896034984182095,
+       0.3555041212990241,
+      -1.7225388986277870,
+       1.3591668746291738,
+       0.0896034984182095,
+      -0.3555041212990241,
+       1.1700802348184398,
+       1.5923689316144276,
+       0.1969103994962658,
+      -0.0729696287365430};
+
     float tol = 1e-6;
 
-    unsigned int i, n=0;
-    for (i=0; i<4; i++) {
-        interp_rrrf_execute(q, x[i], &y[n]);
-        n+=M;
-    }   
+    unsigned int i;
+    for (i=0; i<4; i++)
+        interp_rrrf_execute(q, x[i], &y[i*M]);
 
     for (i=0; i<16; i++) {
         CONTEND_DELTA(y[i], test[i], tol);
@@ -59,16 +76,26 @@ void autotest_interp_rrrf_generic()
     if (liquid_autotest_verbose)
         interp_rrrf_print(q);
 
+    // destroy interpolator object
     interp_rrrf_destroy(q);
 }
 
 void autotest_interp_crcf_generic()
 {
     // h = [0, 0.25, 0.5, 0.75, 1.0, 0.75, 0.5, 0.25, 0];
-    float h[] = {0, 0.25, 0.5, 0.75, 1.0, 0.75, 0.5, 0.25, 0}; 
-    unsigned int h_len = 9; // filter length
+    float h[9] = {
+      -0.7393353832652201,
+       0.1909821993029451,
+      -1.7013834621383086,
+      -0.6157406339062349,
+       0.5806218191269317,
+       0.0576963976148674,
+      -1.0958217797368455,
+      -0.6379821629743743,
+       0.7019489165905530};
+
     unsigned int M = 4;     // interp factor
-    interp_crcf q = interp_crcf_create(4,h,h_len);
+    interp_crcf q = interp_crcf_create(M,h,9);
 
     //  x = [1+j*0.2, -0.2+j*1.3, 0.5+j*0.3, 1.1-j*0.2]
     float complex x[4] = {
@@ -83,31 +110,28 @@ void autotest_interp_crcf_generic()
     // z = [x(1) 0 0 0 x(2) 0 0 0 x(3) 0 0 0 x(4) 0 0 0];
     // test = filter(h,1,z)
     float complex test[16] = {
-      0.0000+  0.0000*_Complex_I, 
-      0.2500+  0.0500*_Complex_I, 
-      0.5000+  0.1000*_Complex_I, 
-      0.7500+  0.1500*_Complex_I, 
-      1.0000+  0.2000*_Complex_I, 
-      0.7000+  0.4750*_Complex_I, 
-      0.4000+  0.7500*_Complex_I, 
-      0.1000+  1.0250*_Complex_I, 
-     -0.2000+  1.3000*_Complex_I, 
-     -0.0250+  1.0500*_Complex_I, 
-      0.1500+  0.8000*_Complex_I, 
-      0.3250+  0.5500*_Complex_I, 
-      0.5000+  0.3000*_Complex_I, 
-      0.6500+  0.1750*_Complex_I, 
-      0.8000+  0.0500*_Complex_I, 
-      0.9500+ -0.0750*_Complex_I
-    };
+      -0.7393353832652201 - 0.1478670766530440*_Complex_I,
+       0.1909821993029451 + 0.0381964398605890*_Complex_I,
+      -1.7013834621383086 - 0.3402766924276617*_Complex_I,
+      -0.6157406339062349 - 0.1231481267812470*_Complex_I,
+       0.7284888957799757 - 0.8450116344193997*_Complex_I,
+       0.0194999577542784 + 0.2598161386168021*_Complex_I,
+      -0.7555450873091838 - 2.4309628567271702*_Complex_I,
+      -0.5148340361931273 - 0.9280592566729803*_Complex_I,
+       0.2161568611325566 + 0.6733975332035558*_Complex_I,
+       0.0839518201284991 + 0.1322999766902112*_Complex_I,
+      -0.6315273751217851 - 1.9349833522993918*_Complex_I,
+      -0.1802738843582426 - 1.0140990020385570*_Complex_I,
+      -0.6633477953463869 + 1.2345872139588425*_Complex_I,
+       0.2389286180406733 - 0.0208875205761288*_Complex_I,
+      -2.4194326982205623 + 0.0115301585066081*_Complex_I,
+      -0.9963057787840456 - 0.0682465221110653*_Complex_I };
 
     float tol = 1e-6;
 
-    unsigned int i, n=0;
-    for (i=0; i<4; i++) {
-        interp_crcf_execute(q, x[i], &y[n]);
-        n+=M;
-    }   
+    unsigned int i;
+    for (i=0; i<4; i++)
+        interp_crcf_execute(q, x[i], &y[i*M]);
 
     for (i=0; i<16; i++) {
         CONTEND_DELTA( crealf(y[i]), crealf(test[i]), tol);
@@ -120,8 +144,7 @@ void autotest_interp_crcf_generic()
     if (liquid_autotest_verbose)
         interp_crcf_print(q);
 
+    // destroy interpolator object
     interp_crcf_destroy(q);
 }
-
-#endif 
 
