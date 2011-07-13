e3920b1192fc308f395a86cb3c1efdea426f67fd
Joseph D. Gaeddert <jgaedder@vt.edu>
2011-07-13 16:20:17 -0400

    sandbox: adding generic Hamming encoder/decoder test

--- sandbox/sandbox.mk
+++ sandbox/sandbox.mk
@@ -8,6 +8,7 @@ local_src	:=				\
 	ellip_func_test.c			\
 	ellip_test.c				\
 	eqlms_cccf_test.c			\
+	fec_hamming_test.c			\
 	fec_hamming128_example.c		\
 	fec_hamming74_gentab.c			\
 	fec_hamming84_gentab.c			\
