37c868c8cb1d413e1f154640b7b3e6f55afb0dcb
Joseph Gaeddert <jgaedder@vt.edu>
2011-07-28 11:28:29 -0400

    fec|soft: rep3 has soft decoding (same Eb/N0 performance as BPSK)

--- include/liquid.internal.h
+++ include/liquid.internal.h
@@ -279,6 +279,10 @@ void fec_rep3_decode(fec _q,
                      unsigned int _dec_msg_len,
                      unsigned char * _msg_enc,
                      unsigned char * _msg_dec);
+void fec_rep3_decode_soft(fec _q,
+                          unsigned int _dec_msg_len,
+                          unsigned char * _msg_enc,
+                          unsigned char * _msg_dec);
 
 // Repeat (5)
 fec fec_rep5_create(void *_opts);
