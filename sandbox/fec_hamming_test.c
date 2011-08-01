bb2a2ba41bcf55889b6df6ac7f9a012fe33dba99
Joseph Gaeddert <jgaedder@vt.edu>
2011-08-01 09:21:57 -0400

    sandbox|fec: adding Hamming(31,26) code test

--- sandbox/fec_hamming_test.c
+++ sandbox/fec_hamming_test.c
@@ -19,7 +19,7 @@
  */
 
 //
-// ?-rate (?,?) Hamming code
+//  Hamming code parity check matrix
 //
 //  bit position    1   2   3   4   5   6   7   8   9   10  11  12  13  14  15  16  17  18  19  20  21  22  23  24  25  26  27  28  29  30  31  32  33  34  35  36  37  38
 //  encoded bits    P1  P2  1   P4  2   3   4   P8  5   6   7   8   9   10  11  P16 12  13  14  15  16  17  18  19  20  21  22  23  24  25  26  P32 27  28  29  30  31  32
@@ -66,7 +66,9 @@
 #endif
 
 
-
+//
+// Hamming(15,11)
+//
 // parity bit coverage mask for encoder (collapsed version of figure
 // above, stripping out parity bits P1, P2, P4, P8 and only including
 // data bits 1:11)
