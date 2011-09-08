016800ba7ec284ba90dbb7979159badf92596378
Joseph Gaeddert <jgaedder@vt.edu>
2011-09-08 12:41:55 -0400

    sandbox|firdes_gmskrx: improving 'gain' filter parameters

--- sandbox/firdes_gmskrx_test.c
+++ sandbox/firdes_gmskrx_test.c
@@ -48,28 +48,38 @@ int main() {
 
     // temporary arrays
     float h_primef[h_len];          // temporary buffer for real coefficients
+    float g_primef[h_len];          // 
 
     float complex h_tx[h_len];      // impulse response of transmit filter
     float complex h_prime[h_len];   // impulse response of 'ideal' filter
+    float complex g_prime[h_len];   // impulse response of 'gain' filter
     float complex h_hat[h_len];     // impulse response of receive filter
     
     float complex H_tx[h_len];      // frequency response of transmit filter
     float complex H_prime[h_len];   // frequency response of 'ideal' filter
+    float complex G_prime[h_len];   // frequency response of 'gain' filter
     float complex H_hat[h_len];     // frequency response of receive filter
 
     // create 'ideal' matched filter
     // for now use raised-cosine
     design_nyquist_filter(LIQUID_NYQUIST_RCOS,k,m,beta,0.0f,h_primef);
 
+    // create 'gain' filter to improve stop-band rejection
+    float fc = (0.7f + 0.1*beta) / (float)k;
+    float As = 40.0f;
+    firdes_kaiser_window(h_len, fc, As, 0.0f, g_primef);
+
     // copy to fft input buffer, shifting appropriately
     // TODO : check if this fft shift is really necesary
     for (i=0; i<h_len; i++) {
         h_prime[i] = h_primef[ (i+k*m)%h_len ];
+        g_prime[i] = g_primef[ (i+k*m)%h_len ];
         h_tx[i]    = ht[       (i+k*m)%h_len ];
     }
 
     // run ffts
     fft_run(h_len, h_prime, H_prime, FFT_FORWARD, 0);
+    fft_run(h_len, g_prime, G_prime, FFT_FORWARD, 0);
     fft_run(h_len, h_tx,    H_tx,    FFT_FORWARD, 0);
 
 #if 0
@@ -78,23 +88,31 @@ int main() {
         printf("Ht(%3u) = %12.8f + j*%12.8f;\n", i+1, crealf(H_tx[i]), cimagf(H_tx[i]));
     for (i=0; i<h_len; i++)
         printf("H_prime(%3u) = %12.8f + j*%12.8f;\n", i+1, crealf(H_prime[i]), cimagf(H_prime[i]));
+    for (i=0; i<h_len; i++)
+        printf("G_prime(%3u) = %12.8f + j*%12.8f;\n", i+1, crealf(G_prime[i]), cimagf(G_prime[i]));
 #endif
 
     // find minimum of reponses
     float H_tx_min = 0.0f;
     float H_prime_min = 0.0f;
+    float G_prime_min = 0.0f;
     for (i=0; i<h_len; i++) {
         if (i==0 || crealf(H_tx[i]) < H_tx_min)
             H_tx_min = crealf(H_tx[i]);
 
         if (i==0 || crealf(H_prime[i]) < H_prime_min)
             H_prime_min = crealf(H_prime[i]);
+
+        if (i==0 || crealf(G_prime[i]) < G_prime_min)
+            G_prime_min = crealf(G_prime[i]);
     }
 
     // compute 'ideal' response, removing minima, and add correction factor
     // TODO : include additional term to add stop-band suppression
-    for (i=0; i<h_len; i++)
+    for (i=0; i<h_len; i++) {
         H_hat[i] = crealf(H_prime[i] - H_prime_min + delta) / crealf(H_tx[i] - H_tx_min + delta);
+        H_hat[i] *= crealf(G_prime[i] - G_prime_min) / crealf(G_prime[0]);
+    }
 
     // compute ifft and copy response
     fft_run(h_len, H_hat, h_hat, FFT_REVERSE, 0);
@@ -151,7 +169,7 @@ int main() {
     fprintf(fid,"xlabel('Normalized Frequency');\n");
     fprintf(fid,"ylabel('PSD');\n");
     fprintf(fid,"grid on;\n");
-    fprintf(fid,"axis([-0.5 0.5 -100 40]);\n");
+    fprintf(fid,"axis([-0.5 0.5 -100 20]);\n");
 
     fprintf(fid,"\n");
     fprintf(fid,"figure;\n");
@@ -164,7 +182,7 @@ int main() {
     fprintf(fid,"  xlabel('Time');\n");
     fprintf(fid,"  ylabel('GMSK Tx/Rx Filters');\n");
     fprintf(fid,"  grid on;\n");
-    fprintf(fid,"  axis([-2*m 2*m -0.3 1.2*max(hr)]);\n");
+    fprintf(fid,"  axis([-2*m 2*m -0.3 1.2*max([hr ht])]);\n");
     fprintf(fid,"subplot(2,1,2);\n");
     fprintf(fid,"  plot(tc,hc,'-x', tc(ic),hc(ic),'or');\n");
     fprintf(fid,"  xlabel('Time');\n");
