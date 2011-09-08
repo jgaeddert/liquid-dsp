1ba5be26f88700e748855fd6c3784b0657ac12aa
Joseph Gaeddert <jgaedder@vt.edu>
2011-09-08 10:35:59 -0400

    sandbox|firdes_gmskrx: adding filter design
      * uses frequency method to generate filter from 'ideal' response
      * 'ideal' response template uses raised-cosine filter
      * works relatively well, but needs better stop-band rejection

--- sandbox/firdes_gmskrx_test.c
+++ sandbox/firdes_gmskrx_test.c
@@ -34,11 +34,85 @@ int main() {
     // design transmit filter
     liquid_firdes_gmsktx(k,m,BT,0.0f,ht);
 
-    // design receive filter
-    // TODO : use 'optimal' method
-    liquid_firdes_gmskrx(k,m,BT,0.0f,hr);
+    // print results to screen
+    printf("gmsk transmit filter:\n");
+    for (i=0; i<h_len; i++)
+        printf("  ht(%3u) = %12.8f;\n", i+1, ht[i]);
 
-     // open output file
+    //
+    // start of filter design procedure
+    //
+
+    float beta = BT;        // prototype filter cut-off
+    float delta = 1e-2f;    // filter design correction factor
+
+    // temporary arrays
+    float h_primef[h_len];          // temporary buffer for real coefficients
+
+    float complex h_tx[h_len];      // impulse response of transmit filter
+    float complex h_prime[h_len];   // impulse response of 'ideal' filter
+    float complex h_hat[h_len];     // impulse response of receive filter
+    
+    float complex H_tx[h_len];      // frequency response of transmit filter
+    float complex H_prime[h_len];   // frequency response of 'ideal' filter
+    float complex H_hat[h_len];     // frequency response of receive filter
+
+    // create 'ideal' matched filter
+    // for now use raised-cosine
+    design_nyquist_filter(LIQUID_NYQUIST_RCOS,k,m,beta,0.0f,h_primef);
+
+    // copy to fft input buffer, shifting appropriately
+    // TODO : check if this fft shift is really necesary
+    for (i=0; i<h_len; i++) {
+        h_prime[i] = h_primef[ (i+k*m)%h_len ];
+        h_tx[i]    = ht[       (i+k*m)%h_len ];
+    }
+
+    // run ffts
+    fft_run(h_len, h_prime, H_prime, FFT_FORWARD, 0);
+    fft_run(h_len, h_tx,    H_tx,    FFT_FORWARD, 0);
+
+#if 0
+    // print results
+    for (i=0; i<h_len; i++)
+        printf("Ht(%3u) = %12.8f + j*%12.8f;\n", i+1, crealf(H_tx[i]), cimagf(H_tx[i]));
+    for (i=0; i<h_len; i++)
+        printf("H_prime(%3u) = %12.8f + j*%12.8f;\n", i+1, crealf(H_prime[i]), cimagf(H_prime[i]));
+#endif
+
+    // find minimum of reponses
+    float H_tx_min = 0.0f;
+    float H_prime_min = 0.0f;
+    for (i=0; i<h_len; i++) {
+        if (i==0 || crealf(H_tx[i]) < H_tx_min)
+            H_tx_min = crealf(H_tx[i]);
+
+        if (i==0 || crealf(H_prime[i]) < H_prime_min)
+            H_prime_min = crealf(H_prime[i]);
+    }
+
+    // compute 'ideal' response, removing minima, and add correction factor
+    // TODO : include additional term to add stop-band suppression
+    for (i=0; i<h_len; i++)
+        H_hat[i] = crealf(H_prime[i] - H_prime_min + delta) / crealf(H_tx[i] - H_tx_min + delta);
+
+    // compute ifft and copy response
+    fft_run(h_len, H_hat, h_hat, FFT_REVERSE, 0);
+    for (i=0; i<h_len; i++)
+        hr[i] = crealf( h_hat[(i+k*m+1)%h_len] ) / (float)(k*h_len);
+
+    //
+    // end of filter design procedure
+    //
+
+    // print results to screen
+    printf("gmsk receive filter:\n");
+    for (i=0; i<h_len; i++)
+        printf("  hr(%3u) = %12.8f;\n", i+1, hr[i]);
+
+    // 
+    // export output file
+    //
     FILE*fid = fopen(OUTPUT_FILENAME,"w");
     fprintf(fid,"%% %s : auto-generated file\n", OUTPUT_FILENAME);
     fprintf(fid,"clear all;\n");
@@ -50,39 +124,53 @@ int main() {
     fprintf(fid,"h_len = 2*k*m+1;\n");
     fprintf(fid,"nfft = 1024;\n");
     fprintf(fid,"ht = zeros(1,h_len);\n");
+    fprintf(fid,"hp = zeros(1,h_len);\n");
     fprintf(fid,"hr = zeros(1,h_len);\n");
 
     // print results
     for (i=0; i<h_len; i++)   fprintf(fid,"ht(%3u) = %12.4e;\n", i+1, ht[i]);
     for (i=0; i<h_len; i++)   fprintf(fid,"hr(%3u) = %12.4e;\n", i+1, hr[i]);
+    for (i=0; i<h_len; i++)   fprintf(fid,"hp(%3u) = %12.4e;\n", i+1, h_primef[i]);
     
-    fprintf(fid,"hc = conv(ht,hr);\n");
+    fprintf(fid,"hc = k*conv(ht,hr);\n");
 
     // plot results
     fprintf(fid,"f = [0:(nfft-1)]/nfft - 0.5;\n");
-    fprintf(fid,"Ht = 20*log10(abs(fftshift(fft(ht,nfft))));\n");
-    fprintf(fid,"Hr = 20*log10(abs(fftshift(fft(hr,nfft))));\n");
-    fprintf(fid,"Hc = 20*log10(abs(fftshift(fft(hc,nfft))));\n");
+    fprintf(fid,"Ht = 20*log10(abs(fftshift(fft(ht,  nfft))));\n");
+    fprintf(fid,"Hp = 20*log10(abs(fftshift(fft(hp/k,nfft))));\n");
+    fprintf(fid,"Hr = 20*log10(abs(fftshift(fft(hr,  nfft))));\n");
+    fprintf(fid,"Hc = 20*log10(abs(fftshift(fft(hc/k,nfft))));\n");
     fprintf(fid,"\n");
     fprintf(fid,"figure;\n");
-    fprintf(fid,"plot(f,Ht,'LineWidth',2,...\n");
-    fprintf(fid,"     f,Hr,'LineWidth',1,...\n");
-    fprintf(fid,"     f,Hc,'LineWidth',1);\n");
-    fprintf(fid,"legend('transmit','receive','composite',0);\n");
+    fprintf(fid,"plot(f,Ht,'LineWidth',1,'Color',[0.00 0.25 0.50],...\n");
+    fprintf(fid,"     f,Hp,'LineWidth',1,'Color',[0.80 0.80 0.80],...\n");
+    fprintf(fid,"     f,Hr,'LineWidth',1,'Color',[0.00 0.50 0.25],...\n");
+    fprintf(fid,"     f,Hc,'LineWidth',2,'Color',[0.50 0.00 0.00],...\n");
+    fprintf(fid,"     [-0.5/k 0.5/k], [1 1]*20*log10(0.5),'or');\n");
+    fprintf(fid,"legend('transmit','ideal','receive','composite','alias points',1);\n");
     fprintf(fid,"xlabel('Normalized Frequency');\n");
     fprintf(fid,"ylabel('PSD');\n");
     fprintf(fid,"grid on;\n");
-    fprintf(fid,"axis([-0.5 0.5 -100 20]);\n");
+    fprintf(fid,"axis([-0.5 0.5 -100 40]);\n");
 
     fprintf(fid,"\n");
     fprintf(fid,"figure;\n");
     fprintf(fid,"tr = [  -k*m:k*m]/k;\n");
     fprintf(fid,"tc = [-2*k*m:2*k*m]/k;\n");
-    fprintf(fid,"plot(tr,hr,'-x', tc,hc,'-x');\n");
-    fprintf(fid,"xlabel('Time');\n");
-    fprintf(fid,"ylabel('Filter Impulse Response');\n");
-    fprintf(fid,"grid on;\n");
-    fprintf(fid,"legend('receive','composite',0);\n");
+    fprintf(fid,"ic = [0:k:(4*k*m)]+1;\n");
+    fprintf(fid,"subplot(2,1,1);\n");
+    fprintf(fid,"  plot(tr,ht,'-x', tr,hr,'-x');\n");
+    fprintf(fid,"  legend('transmit','receive',1);\n");
+    fprintf(fid,"  xlabel('Time');\n");
+    fprintf(fid,"  ylabel('GMSK Tx/Rx Filters');\n");
+    fprintf(fid,"  grid on;\n");
+    fprintf(fid,"  axis([-2*m 2*m -0.3 1.2*max(hr)]);\n");
+    fprintf(fid,"subplot(2,1,2);\n");
+    fprintf(fid,"  plot(tc,hc,'-x', tc(ic),hc(ic),'or');\n");
+    fprintf(fid,"  xlabel('Time');\n");
+    fprintf(fid,"  ylabel('GMSK Composite Response');\n");
+    fprintf(fid,"  grid on;\n");
+    fprintf(fid,"  axis([-2*m 2*m -0.2 1.2]);\n");
 
     fclose(fid);
     printf("results written to %s.\n", OUTPUT_FILENAME);
