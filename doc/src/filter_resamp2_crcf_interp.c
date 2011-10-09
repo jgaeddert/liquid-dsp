26d8014cf82b72892d965e8f7ba9a042e9f31155
Joseph Gaeddert <jgaedder@vt.edu>
2011-10-09 14:41:42 -0400

    doc|resamp2: cleaning up figure
      * using pulse rather than filtered noise
      * changing point types for original points

--- doc/src/filter_resamp2_crcf_interp.c
+++ doc/src/filter_resamp2_crcf_interp.c
@@ -14,9 +14,9 @@
 #define OUTPUT_FILENAME_FREQ "figures.gen/filter_resamp2_crcf_interp_freq.gnu"
 
 int main() {
-    unsigned int m=5;           // filter semi-length (h_len = 4*m+1)
-    unsigned int n=100;         // number of input samples
-    float As=60.0f;             // stop-band attenuation [dB]
+    unsigned int m=5;   // filter semi-length (h_len = 4*m+1)
+    unsigned int n=37;  // number of input samples
+    float As=60.0f;     // stop-band attenuation [dB]
 
     unsigned int i;
 
@@ -28,22 +28,11 @@ int main() {
     float complex x[nx];
     float complex y[ny];
 
-    // generate input signal (filtered noise)
-    int ftype           = LIQUID_IIRDES_ELLIP;
-    int btype           = LIQUID_IIRDES_LOWPASS;
-    int format          = LIQUID_IIRDES_SOS;
-    unsigned int order  = 7;
-    float fc            = 0.17f;
-    float f0            = 0.0f;
-    float ap            = 1.0f;
-    float as            = 80.0f;
-    iirfilt_crcf f = iirfilt_crcf_create_prototype(ftype, btype, format, order, fc, f0, ap, as);
-    for (i=0; i<nx; i++) {
-        float complex z = (i < n) ? randnf() + _Complex_I*randnf() : 0.0f;
-        iirfilt_crcf_execute(f, z, &x[i]);
-        //x[i] *= (i < n) ? hamming(i,n) : 0.0f;
-    }
-    iirfilt_crcf_destroy(f);
+    // generate input signal (filter pulse with frequency offset)
+    float hf[n];
+    liquid_firdes_kaiser(n, 0.02f, 80.0f, 0, hf);
+    for (i=0; i<nx; i++)
+        x[i] = (i < n) ? hf[i]*cexpf(_Complex_I*0.77f*i) : 0.0f;
 
     // create resampler
     resamp2_crcf q = resamp2_crcf_create(m,0.0f,As);
@@ -72,20 +61,19 @@ int main() {
     // TODO : switch terminal types here
     fprintf(fid,"set terminal postscript eps enhanced color solid rounded\n");
     //fprintf(fid,"set xrange [0:%u];\n",n);
-    fprintf(fid,"set yrange [-1.5:1.5]\n");
+    fprintf(fid,"set yrange [-1.2:1.2]\n");
     fprintf(fid,"set size ratio 0.3\n");
     fprintf(fid,"set xlabel 'Input Sample Index'\n");
     fprintf(fid,"set key top right nobox\n");
     fprintf(fid,"set ytics -5,1,5\n");
     fprintf(fid,"set grid xtics ytics\n");
-    fprintf(fid,"set pointsize 0.6\n");
     fprintf(fid,"set grid linetype 1 linecolor rgb '%s' lw 1\n", LIQUID_DOC_COLOR_GRID);
     fprintf(fid,"set multiplot layout 2,1 scale 1.0,1.0\n");
 
     fprintf(fid,"# real\n");
     fprintf(fid,"set ylabel 'Real'\n");
-    fprintf(fid,"plot '-' using 1:2 with linespoints pointtype 7 linetype 1 linewidth 1 linecolor rgb '#999999' title 'original',\\\n");
-    fprintf(fid,"     '-' using 1:2 with points pointtype 7 linecolor rgb '#008000' title 'interpolated'\n");
+    fprintf(fid,"plot '-' using 1:2 with linespoints pointtype 6 pointsize 0.9 linetype 1 linewidth 1 linecolor rgb '#666666' title 'original',\\\n");
+    fprintf(fid,"     '-' using 1:2 with points pointtype 7 pointsize 0.6 linecolor rgb '#008000' title 'interpolated'\n");
     // export input signal
     for (i=0; i<nx; i++)
         fprintf(fid,"%12.4e %12.4e %12.4e\n", (float)i, crealf(x[i]), cimagf(x[i]));
@@ -98,8 +86,8 @@ int main() {
 
     fprintf(fid,"# imag\n");
     fprintf(fid,"set ylabel 'Imag'\n");
-    fprintf(fid,"plot '-' using 1:3 with linespoints pointtype 7 linetype 1 linewidth 1 linecolor rgb '#999999' title 'original',\\\n");
-    fprintf(fid,"     '-' using 1:3 with points pointtype 7 linecolor rgb '#800000' title 'interpolated'\n");
+    fprintf(fid,"plot '-' using 1:3 with linespoints pointtype 6 pointsize 0.9 linetype 1 linewidth 1 linecolor rgb '#666666' title 'original',\\\n");
+    fprintf(fid,"     '-' using 1:3 with points pointtype 7 pointsize 0.6 linecolor rgb '#800000' title 'interpolated'\n");
 
     // export input signal
     for (i=0; i<nx; i++)
@@ -127,13 +115,12 @@ int main() {
     float complex H[nfft];
     float h[4*m+1];
     liquid_firdes_kaiser(4*m+1, 0.25f, As, 0.0f, h);
-    liquid_doc_compute_psdcf(x,nx,X,nfft,LIQUID_DOC_PSDWINDOW_HANN,0);
-    liquid_doc_compute_psdcf(y,ny,Y,nfft,LIQUID_DOC_PSDWINDOW_HANN,0);
-    liquid_doc_compute_psdf( h,4*m+1,H,nfft,LIQUID_DOC_PSDWINDOW_NONE,0);
+    liquid_doc_compute_psdcf(x,nx,    X, nfft, LIQUID_DOC_PSDWINDOW_NONE, 1);
+    liquid_doc_compute_psdcf(y,ny,    Y, nfft, LIQUID_DOC_PSDWINDOW_NONE, 1);
+    liquid_doc_compute_psdf( h,4*m+1, H, nfft, LIQUID_DOC_PSDWINDOW_NONE, 1);
     fft_shift(X,nfft);
     fft_shift(Y,nfft);
     fft_shift(H,nfft);
-    float scaling_factor = 10*log10f(nfft);
 
     fprintf(fid,"# %s: auto-generated file\n\n", OUTPUT_FILENAME_FREQ);
     fprintf(fid,"reset\n");
@@ -156,17 +143,17 @@ int main() {
     // export output
     for (i=0; i<nfft; i++) {
         float fx = ((float)(i) / (float)nfft - 0.5f)*0.5f;
-        fprintf(fid,"%12.8f %12.4e\n", fx, 20*log10f(cabsf(X[i])) - scaling_factor);
+        fprintf(fid,"%12.8f %12.4e\n", fx, 20*log10f(cabsf(X[i])));
     }
     fprintf(fid,"e\n");
     for (i=0; i<nfft; i++) {
         float fy = ((float)(i) / (float)nfft - 0.5f);
-        fprintf(fid,"%12.8f %12.4e\n", fy, 20*log10f(cabsf(Y[i])) - scaling_factor - 20*log10(2.0f));
+        fprintf(fid,"%12.8f %12.4e\n", fy, 20*log10f(cabsf(Y[i])));
     }
     fprintf(fid,"e\n");
     for (i=0; i<nfft; i++) {
         float fh = ((float)(i) / (float)nfft - 0.5f);
-        fprintf(fid,"%12.8f %12.4e\n", fh, 20*log10f(cabsf(H[i])/2.0f));
+        fprintf(fid,"%12.8f %12.4e\n", fh, 20*log10f(cabsf(H[i])));
     }
     fprintf(fid,"e\n");
 
