cd72ef83a7eeed85c0127e597a1889dc6807ac05
Joseph Gaeddert <jgaedder@vt.edu>
2011-10-21 18:16:16 -0400

    fec|golay: incorporating Golay(24,12) codec into autotest

--- src/fec/tests/fec_autotest.c
+++ src/fec/tests/fec_autotest.c
@@ -96,6 +96,9 @@ void autotest_fec_h74()     { fec_test_codec(LIQUID_FEC_HAMMING74,     64, NULL)
 void autotest_fec_h84()     { fec_test_codec(LIQUID_FEC_HAMMING84,     64, NULL); }
 void autotest_fec_h128()    { fec_test_codec(LIQUID_FEC_HAMMING128,    64, NULL); }
 
+// Golay block codes
+void autotest_fec_g2412()   { fec_test_codec(LIQUID_FEC_GOLAY2412,     64, NULL); }
+
 // convolutional codes
 void autotest_fec_v27()     { fec_test_codec(LIQUID_FEC_CONV_V27,      64, NULL); }
 void autotest_fec_v29()     { fec_test_codec(LIQUID_FEC_CONV_V29,      64, NULL); }
