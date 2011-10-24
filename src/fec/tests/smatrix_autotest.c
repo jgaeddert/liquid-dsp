83f65a17ff8bef5037d9d4250847b540d54ca6ce
Joseph Gaeddert <jgaedder@vt.edu>
2011-10-24 14:47:27 -0400

    smatrix: adding autotest for matrix multiplication

--- src/fec/tests/smatrix_autotest.c
+++ src/fec/tests/smatrix_autotest.c
@@ -27,7 +27,7 @@
 // AUTOTESTS: basic encode/decode functionality
 //
 
-// repeat codes
+// test sparse binary matrix methods
 void autotest_fec_smatrix()
 {
     // A = [
@@ -108,5 +108,99 @@ void autotest_fec_smatrix()
 
     // destroy matrix object
     smatrix_destroy(A);
+}
+
+// test sparse binary matrix multiplication
+void autotest_fec_smatrix_mul()
+{
+    smatrix a = smatrix_create( 8,12);
+    smatrix b = smatrix_create(12, 5);
+    smatrix c = smatrix_create( 8, 5);
+
+    // initialize 'a'
+    // 0 0 0 0 1 0 0 0 0 0 0 0
+    // 0 0 0 0 0 0 0 0 0 0 0 0
+    // 0 0 0 1 0 0 0 1 0 0 0 0
+    // 0 0 0 0 1 0 0 0 0 0 0 0
+    // 0 0 0 1 0 0 1 1 0 0 0 0
+    // 0 0 0 0 0 0 0 0 0 0 0 0
+    // 0 1 0 0 0 0 0 0 0 0 0 0
+    // 0 0 0 0 0 0 0 1 1 0 0 0
+    smatrix_set(a,0,4);
+    smatrix_set(a,2,3);
+    smatrix_set(a,2,7);
+    smatrix_set(a,3,4);
+    smatrix_set(a,4,3);
+    smatrix_set(a,4,6);
+    smatrix_set(a,4,7);
+    smatrix_set(a,6,1);
+    smatrix_set(a,7,7);
+    smatrix_set(a,7,8);
+
+    // initialize 'b'
+    // 1 1 0 0 0
+    // 0 0 0 0 1
+    // 0 0 0 0 0
+    // 0 0 0 0 0
+    // 0 0 0 0 0
+    // 0 0 0 0 1
+    // 0 0 0 1 0
+    // 0 0 0 1 0
+    // 0 0 0 0 0
+    // 0 1 0 0 1
+    // 1 0 0 1 0
+    // 0 1 0 0 0
+    smatrix_set(b,0,0);
+    smatrix_set(b,0,1);
+    smatrix_set(b,1,4);
+    smatrix_set(b,5,4);
+    smatrix_set(b,6,3);
+    smatrix_set(b,7,3);
+    smatrix_set(b,9,1);
+    smatrix_set(b,9,4);
+    smatrix_set(b,10,0);
+    smatrix_set(b,11,1);
+
+    // compute 'c'
+    // 0 0 0 0 0
+    // 0 0 0 0 0
+    // 0 0 0 1 0
+    // 0 0 0 0 0
+    // 0 0 0(0)0
+    // 0 0 0 0 0
+    // 0 0 0 0 1
+    // 0 0 0 1 0
+    smatrix_mul(a,b,c);
+
+    // valid output
+    unsigned char c_test[40] = {
+        0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0,
+        0, 0, 0, 1, 0,
+        0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0,
+        0, 0, 0, 0, 1,
+        0, 0, 0, 1, 0};
+
+    // print results (verbose)
+    if (liquid_autotest_verbose) {
+        printf("a:\n"); smatrix_print_expanded(a);
+        printf("b:\n"); smatrix_print_expanded(b);
+        printf("c:\n"); smatrix_print_expanded(c);
+    }
+
+    unsigned int i;
+    unsigned int j;
+    for (i=0; i<8; i++) {
+        for (j=0; j<5; j++) {
+            CONTEND_EQUALITY( smatrix_get(c,i,j), c_test[i*5+j]);
+        }
+    }
+    
+    // destroy objects
+    smatrix_destroy(a);
+    smatrix_destroy(b);
+    smatrix_destroy(c);
 }
 
