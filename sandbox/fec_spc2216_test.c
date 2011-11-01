42214ffe8c91efc81aa9f21c5275edbe5ea3938e
Joseph Gaeddert <jgaedder@vt.edu>
2011-11-01 14:46:39 -0400

    sandbox|spc2216: adding methods to pack, unpack, and print blocks

--- sandbox/fec_spc2216_test.c
+++ sandbox/fec_spc2216_test.c
@@ -40,9 +40,15 @@
 
 #include "liquid.internal.h"
 
+// encode message
+//  _msg_org    :   original message [size: 32 bytes]
+//  _msg_enc    :   encoded message  [size: 61 bytes]
 void spc2216_encode(unsigned char * _msg_org,
                     unsigned char * _msg_enc);
 
+// decode message
+//  _msg_rec    :   received message [size: 61 bytes]
+//  _msg_dec    :   decoded message  [size: 32 bytes]
 void spc2216_decode(unsigned char * _msg_rec,
                     unsigned char * _msg_dec);
 
@@ -61,10 +67,10 @@ void spc2216_pack(unsigned char * _m,
 //  _pr :   row parities    [size: 16 bytes, @ 6 bits]
 //  _pc :   col parities    [size: 22 bytes, @ 6 bits]
 //  _m  :   output message  [size: 32 bytes]
-void spc2216_unpack(unsigned char * _m,
+void spc2216_unpack(unsigned char * _v,
                     unsigned char * _pr,
                     unsigned char * _pc,
-                    unsigned char * _v);
+                    unsigned char * _m);
 
 // transpose raw message block
 //void spc2216_transpose_row(unsigned char * _m...);
@@ -80,12 +86,12 @@ void spc2216_print_unpacked(unsigned char * _m,
                             unsigned char * _pr,
                             unsigned char * _pc);
 
-void print_bitstring_short(unsigned char _x,
-                           unsigned char _n)
+void print_bitstring(unsigned char _x,
+                     unsigned char _n)
 {
     unsigned int i;
     for (i=0; i<_n; i++)
-        printf("%1u", (_x >> (_n-i-1)) & 1);
+        printf("%2u", (_x >> (_n-i-1)) & 1);
 }
 
 int main(int argc, char*argv[])
@@ -106,11 +112,23 @@ int main(int argc, char*argv[])
     unsigned char m_hat[32];// estimated original message
 
     // generate random transmitted message
+#if 0
     for (i=0; i<32; i++)
         m[i] = rand() & 0xff;
+#else
+    for (i=0; i<32; i++)
+        m[i] = (i % 2) == 0 ? 0x80 >> (i/2) : 0;
+#endif
+    printf("m (original message):\n");
+    spc2216_print_decoded(m);
 
     // encode
     spc2216_encode(m, v);
+    printf("v (encoded message):\n");
+    spc2216_print_encoded(v);
+
+    //printf("e (error message):\n");
+    //spc2216_print_encoded(e);
 
     // add errors
     for (i=0; i<61; i++)
@@ -130,12 +148,18 @@ int main(int argc, char*argv[])
 void spc2216_encode(unsigned char * _msg_org,
                     unsigned char * _msg_enc)
 {
+    unsigned int i;
+
     unsigned char parity_row[16];
     unsigned char parity_col[22];
 
     // compute row parities
+    for (i=0; i<16; i++)
+        parity_row[i] = i;
 
     // compute column parities
+    for (i=0; i<22; i++)
+        parity_col[i] = i;
 
     // pack encoded message
     spc2216_pack(_msg_org, parity_row, parity_col, _msg_enc);
@@ -168,7 +192,19 @@ void spc2216_pack(unsigned char * _m,
     // copy input message to begining of encoded vector
     memmove(_v, _m, 32*sizeof(unsigned char));
 
-    // pack 16 six-bit bytes
+    // pack row parities
+    unsigned int i;
+    unsigned int k=0;
+    for (i=0; i<16; i++) {
+        liquid_pack_array(&_v[32], 29, k, 6, _pr[i]);
+        k += 6;
+    }
+
+    // pack column parities
+    for (i=0; i<22; i++) {
+        liquid_pack_array(&_v[32], 29, k, 6, _pc[i]);
+        k += 6;
+    }
 }
 
 // unpack message
@@ -176,16 +212,42 @@ void spc2216_pack(unsigned char * _m,
 //  _pr :   row parities    [size: 16 bytes, @ 6 bits]
 //  _pc :   col parities    [size: 22 bytes, @ 6 bits]
 //  _m  :   output message  [size: 32 bytes]
-void spc2216_unpack(unsigned char * _m,
+void spc2216_unpack(unsigned char * _v,
                     unsigned char * _pr,
                     unsigned char * _pc,
-                    unsigned char * _v)
+                    unsigned char * _m)
 {
+    // copy input message to begining of encoded vector
+    memmove(_m, _v, 32*sizeof(unsigned char));
+
+    // unpack row parities
+    unsigned int i;
+    unsigned int k=0;
+    for (i=0; i<16; i++) {
+        //liquid_unpack_array(&_pr[k], &_v[32*8+k], 
+        liquid_unpack_array(&_v[32], 29, k, 6, &_pr[i]);
+        k += 6;
+    }
+
+    // unpack column parities
+    for (i=0; i<22; i++) {
+        //liquid_unpack_array(&_pr[k], &_v[32*8+k], 
+        liquid_unpack_array(&_v[32], 29, k, 6, &_pc[i]);
+        k += 6;
+    }
 }
 
 // print decoded block
 void spc2216_print_decoded(unsigned char * _m)
 {
+    unsigned int i;
+    
+    for (i=0; i<16; i++) {
+        printf("    ");
+        print_bitstring(_m[2*i+0],8);
+        print_bitstring(_m[2*i+1],8);
+        printf("\n");
+    }
 }
 
 // print encoded block
@@ -211,10 +273,10 @@ void spc2216_print_unpacked(unsigned char * _m,
     
     for (i=0; i<16; i++) {
         printf("    ");
-        liquid_print_bitstring(_m[2*i+0],8);
-        liquid_print_bitstring(_m[2*i+1],8);
+        print_bitstring(_m[2*i+0],8);
+        print_bitstring(_m[2*i+1],8);
         printf(" ");
-        liquid_print_bitstring(_pr[i], 6);
+        print_bitstring(_pr[i], 6);
         printf("\n");
     }
     printf("\n");
@@ -224,7 +286,7 @@ void spc2216_print_unpacked(unsigned char * _m,
     for (j=0; j<8; j++) {
         printf("    ");
         for (i=0; i<22; i++) {
-            printf("%1u", ((_pc[i] >> j) & 0x01) ? 1 : 0);
+            printf("%2u", ((_pc[i] >> j) & 0x01) ? 1 : 0);
 
             if (i==15) printf(" ");
         }
