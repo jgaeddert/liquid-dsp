7d966f7c1826e93cb48ee008136845227c4738de
Joseph Gaeddert <jgaedder@vt.edu>
2011-11-01 13:35:46 -0400

    fec|secded2216: adding autotest for double-error detection

--- src/fec/tests/fec_secded2216_autotest.c
+++ src/fec/tests/fec_secded2216_autotest.c
@@ -85,3 +85,62 @@ void autotest_secded2216_codec_e1()
     }
 }
 
+//
+// AUTOTEST: SEC-DEC (22,16) codec (double error detection)
+//
+void autotest_secded2216_codec_e2()
+{
+    // total combinations of double errors: nchoosek(22,2) = 231
+
+    unsigned int j;
+    unsigned int k;
+
+    for (j=0; j<21; j++) {
+        if (liquid_autotest_verbose)
+            printf("***** %2u *****\n", j);
+        
+        for (k=0; k<22-j-1; k++) {
+            // generate symbol
+            unsigned char sym_org[2];
+            sym_org[0] = rand() & 0xffff;
+            sym_org[1] = rand() & 0xffff;
+
+            // encoded symbol
+            unsigned char sym_enc[3];
+            fec_secded2216_encode_symbol(sym_org, sym_enc);
+
+            // generate error vector (single error)
+            unsigned char e[3] = {0,0,0};
+
+            div_t dj = div(j,8);
+            e[3-dj.quot-1] |= 1 << dj.rem;
+
+            div_t dk = div(k+j+1,8);
+            e[3-dk.quot-1] |= 1 << dk.rem;
+
+            // received symbol
+            unsigned char sym_rec[3];
+            sym_rec[0] = sym_enc[0] ^ e[0];
+            sym_rec[1] = sym_enc[1] ^ e[1];
+            sym_rec[2] = sym_enc[2] ^ e[2];
+
+            // decoded symbol
+            unsigned char sym_dec[2];
+            int syndrome_flag = fec_secded2216_decode_symbol(sym_rec, sym_dec);
+
+
+            if (liquid_autotest_verbose) {
+                // print error vector
+                printf("%3u, e = ", k);
+                liquid_print_bitstring(e[0], 6);
+                liquid_print_bitstring(e[1], 8);
+                liquid_print_bitstring(e[2], 8);
+                printf(" flag=%2d\n", syndrome_flag);
+            }
+
+            // validate syndrome flag is '2'
+            CONTEND_EQUALITY(syndrome_flag, 2);
+        }
+    }
+}
+
