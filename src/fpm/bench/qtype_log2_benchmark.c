5a45541d1c17d28eb4b7f95a9a27a26ed50dbf68
Joseph Gaeddert <jgaedder@vt.edu>
2012-02-01 16:10:34 -0500

    fpm: adding log2 shift/add benchmarks

--- src/fpm/bench/qtype_log2_benchmark.c
+++ src/fpm/bench/qtype_log2_benchmark.c
@@ -26,25 +26,86 @@
 #include "liquidfpm.internal.h"
 
 // 
-// BENCHMARK: q16 log2
+// BENCHMARK: q16 log2 (shift/add)
 //
-void benchmark_q16_log2(struct rusage *_start,
-                        struct rusage *_finish,
-                        unsigned long int *_num_iterations,
-                        unsigned int _n)
+void benchmark_q16_log2_shiftadd(struct rusage *_start,
+                                 struct rusage *_finish,
+                                 unsigned long int *_num_iterations,
+                                 unsigned int _n)
 {
     // initialize arrays
-    q16_t x = rand() & 0x7fff;
+    q16_t x0 = rand() & 0x7fff;
+    q16_t x1 = rand() & 0x7fff;
+    q16_t x2 = rand() & 0x7fff;
+    q16_t x3 = rand() & 0x7fff;
+    q16_t y;
+    unsigned int precision = 16;
+
+    // start trials
+    unsigned long int i;
+    getrusage(RUSAGE_SELF, _start);
+    for (i=0; i<(*_num_iterations); i++) {
+        y = q16_log2_shiftadd(x0, precision);
+        y = q16_log2_shiftadd(x1, precision);
+        y = q16_log2_shiftadd(x2, precision);
+        y = q16_log2_shiftadd(x3, precision);
+    }
+    getrusage(RUSAGE_SELF, _finish);
+    *_num_iterations *= 4;
+}
+
+// 
+// BENCHMARK: q16 log2 (look-up table)
+//
+void benchmark_q16_log2_frac(struct rusage *_start,
+                             struct rusage *_finish,
+                             unsigned long int *_num_iterations,
+                             unsigned int _n)
+{
+    // initialize arrays
+    q16_t x0 = rand() & 0x7fff;
+    q16_t x1 = rand() & 0x7fff;
+    q16_t x2 = rand() & 0x7fff;
+    q16_t x3 = rand() & 0x7fff;
     q16_t y;
 
     // start trials
     unsigned long int i;
     getrusage(RUSAGE_SELF, _start);
     for (i=0; i<(*_num_iterations); i++) {
-        y = q16_log2_frac(x);
-        y = q16_log2_frac(x);
-        y = q16_log2_frac(x);
-        y = q16_log2_frac(x);
+        y = q16_log2_frac(x0);
+        y = q16_log2_frac(x1);
+        y = q16_log2_frac(x2);
+        y = q16_log2_frac(x3);
+    }
+    getrusage(RUSAGE_SELF, _finish);
+    *_num_iterations *= 4;
+}
+
+// 
+// BENCHMARK: q32 log2 (shift/add)
+//
+void benchmark_q32_log2_shiftadd(struct rusage *_start,
+                                 struct rusage *_finish,
+                                 unsigned long int *_num_iterations,
+                                 unsigned int _n)
+{
+    // initialize arrays
+    q32_t x0 = rand() & 0x7fff;
+    q32_t x1 = rand() & 0x7fff;
+    q32_t x2 = rand() & 0x7fff;
+    q32_t x3 = rand() & 0x7fff;
+    q32_t y;
+    unsigned int precision = 32;
+
+    // start trials
+    unsigned long int i;
+    getrusage(RUSAGE_SELF, _start);
+    for (i=0; i<(*_num_iterations); i++) {
+        y = q32_log2_shiftadd(x0, precision);
+        y = q32_log2_shiftadd(x1, precision);
+        y = q32_log2_shiftadd(x2, precision);
+        y = q32_log2_shiftadd(x3, precision);
     }
     getrusage(RUSAGE_SELF, _finish);
     *_num_iterations *= 4;
@@ -53,23 +114,26 @@ void benchmark_q16_log2(struct rusage *_start,
 // 
 // BENCHMARK: q32 log2
 //
-void benchmark_q32_log2(struct rusage *_start,
-                        struct rusage *_finish,
-                        unsigned long int *_num_iterations,
-                        unsigned int _n)
+void benchmark_q32_log2_frac(struct rusage *_start,
+                             struct rusage *_finish,
+                             unsigned long int *_num_iterations,
+                             unsigned int _n)
 {
     // initialize arrays
-    q32_t x = rand() & 0x7fffffff;
+    q32_t x0 = rand() & 0x7fffffff;
+    q32_t x1 = rand() & 0x7fffffff;
+    q32_t x2 = rand() & 0x7fffffff;
+    q32_t x3 = rand() & 0x7fffffff;
     q32_t y;
 
     // start trials
     unsigned long int i;
     getrusage(RUSAGE_SELF, _start);
     for (i=0; i<(*_num_iterations); i++) {
-        y = q32_log2_frac(x);
-        y = q32_log2_frac(x);
-        y = q32_log2_frac(x);
-        y = q32_log2_frac(x);
+        y = q32_log2_frac(x0);
+        y = q32_log2_frac(x1);
+        y = q32_log2_frac(x2);
+        y = q32_log2_frac(x3);
     }
     getrusage(RUSAGE_SELF, _finish);
     *_num_iterations *= 4;
@@ -85,17 +149,20 @@ void benchmark_log2f(struct rusage *_start,
                      unsigned int _n)
 {
     // initialize arrays
-    float x = randf();
+    float x0 = randf();
+    float x1 = randf();
+    float x2 = randf();
+    float x3 = randf();
     float y;
 
     // start trials
     unsigned long int i;
     getrusage(RUSAGE_SELF, _start);
     for (i=0; i<(*_num_iterations); i++) {
-        y = log2f(x);
-        y = log2f(x);
-        y = log2f(x);
-        y = log2f(x);
+        y = log2f(x0);
+        y = log2f(x1);
+        y = log2f(x2);
+        y = log2f(x3);
     }
     getrusage(RUSAGE_SELF, _finish);
     *_num_iterations *= 4;
