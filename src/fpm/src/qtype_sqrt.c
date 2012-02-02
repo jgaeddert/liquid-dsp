b5f07ec2256fb6e410b31844d8227312f18b7554
Joseph Gaeddert <jgaedder@vt.edu>
2012-02-02 11:25:03 -0500

    fpm: adding sqrt method using log|exp (shift|add)

--- src/fpm/src/qtype_sqrt.c
+++ src/fpm/src/qtype_sqrt.c
@@ -29,6 +29,7 @@
 #include "liquidfpm.internal.h"
 
 #define DEBUG_SQRT_NEWTON 0
+#define DEBUG_SQRT_LOGEXP 0
 
 // use iterative process via Newton's method
 Q(_t) Q(_sqrt_newton)( Q(_t) _x, unsigned int _n)
@@ -66,5 +67,39 @@ Q(_t) Q(_sqrt_newton)( Q(_t) _x, unsigned int _n)
 
     //return x1 << (Q(_fracbits)/2);
     return Q(_abs)(x1);
+}
+
+//
+// use log2 and exp2 functions to approximate square root
+//
+Q(_t) Q(_sqrt_logexp_shiftadd)(Q(_t) _x,
+                               unsigned int _precision)
+{
+    if (_x < 0) {
+        fprintf(stderr,"error: qxx_sqrt(), x must be > 0");
+        exit(1);
+    } else if (_x == 0) {
+        return 0;
+    }
+
+    // TODO : ensure precision is maintained by splitting log2
+    //        output into base and fractional components
+
+    // compute logarithm
+    Q(_t) log2x = Q(_log2_shiftadd)(_x,_precision);
+
+    // divide by 2 (logical bit shift)
+    Q(_t) log2x_by_2 = log2x >> 1;
+
+    // compute exponential
+    Q(_t) qsqrt = Q(_exp2_shiftadd)(log2x_by_2,_precision);
+
+#if DEBUG_SQRT_LOGEXP
+    printf("    x           = %12.10f\n", Q(_fixed_to_float)(_x));
+    printf("    log2(x)     = %12.10f\n", Q(_fixed_to_float)(log2x));
+    printf("    log2(x)/2   = %12.10f\n", Q(_fixed_to_float)(log2x_by_2));
+    printf("    sqrt(x)     = %12.10f\n", Q(_fixed_to_float)(qsqrt));
+#endif
+    return qsqrt;
 }
 
--- src/fpm/tests/qtype_sqrt_autotest.c
+++ src/fpm/tests/qtype_sqrt_autotest.c
@@ -28,7 +28,7 @@
 // API definition macro; helper function to keep code base small
 #define LIQUIDFPM_AUTOTEST_SQRT_API(Q)                              \
                                                                     \
-/* sqrt using newton's method */                                    \
+/* sqrt using Newton's method */                                    \
 void Q(_test_sqrt_newton)(float        _xf,                         \
                           unsigned int _precision,                  \
                           float        _tol)                        \
@@ -50,12 +50,39 @@ void Q(_test_sqrt_newton)(float        _xf,                         \
                                                                     \
     /* run comparison */                                            \
     CONTEND_DELTA(ytest, yf, _tol);                                 \
+}                                                                   \
+                                                                    \
+/* sqrt using log|exp shift|add method */                           \
+void Q(_test_sqrt_logexp_shiftadd)(float        _xf,                \
+                                   unsigned int _precision,         \
+                                   float        _tol)               \
+{                                                                   \
+    /* convert to fixed-point */                                    \
+    Q(_t) x = Q(_float_to_fixed)(_xf);                              \
+                                                                    \
+    /* execute operation */                                         \
+    Q(_t) y = Q(_sqrt_logexp_shiftadd)(x, _precision);              \
+    float yf = sqrtf(_xf);                                          \
+                                                                    \
+    /* convert to floating-point */                                 \
+    float ytest = Q(_fixed_to_float)(y);                            \
+                                                                    \
+    if (liquid_autotest_verbose) {                                  \
+        printf("  sqrt(%12.8f) = %12.8f (%12.8f), e=%12.8f\n",      \
+                      _xf,      ytest,  yf,       ytest-yf);        \
+    }                                                               \
+                                                                    \
+    /* run comparison */                                            \
+    CONTEND_DELTA(ytest, yf, _tol);                                 \
 }
 
 // define autotest API
 LIQUIDFPM_AUTOTEST_SQRT_API(LIQUIDFPM_MANGLE_Q32)
 LIQUIDFPM_AUTOTEST_SQRT_API(LIQUIDFPM_MANGLE_Q16)
 
+//
+// q16
+//
 
 void autotest_q16_sqrt_newton()
 {
@@ -77,6 +104,30 @@ void autotest_q16_sqrt_newton()
     }
 }
 
+void autotest_q16_sqrt_logexp_shiftadd()
+{
+    unsigned int precision = 16;    // precision
+    unsigned int num_steps = 77;    // number of steps in test
+    float xmin = 0.0f;
+    float xmax = q16_fixed_to_float(q16_max) * 0.99f;
+    float dx   = (xmax - xmin) / ((float)num_steps);
+    float tol  = expf(-sqrtf(q16_fracbits));
+
+    unsigned int i;
+    float x = xmin;
+    for (i=0; i<num_steps; i++) {
+        // run test
+        q16_test_sqrt_logexp_shiftadd(x, precision, tol);
+
+        // increment input parameter
+        x += dx;
+    }
+}
+
+//
+// q16
+//
+
 void autotest_q32_sqrt_newton()
 {
     unsigned int precision = 16;    // precision
@@ -91,6 +142,26 @@ void autotest_q32_sqrt_newton()
     for (i=0; i<num_steps; i++) {
         // run test
         q32_test_sqrt_newton(x, precision, tol);
+
+        // increment input parameter
+        x += dx;
+    }
+}
+
+void autotest_q32_sqrt_logexp_shiftadd()
+{
+    unsigned int precision = 16;    // precision
+    unsigned int num_steps = 77;    // number of steps in test
+    float xmin = 0.0f;
+    float xmax = q32_fixed_to_float(q32_max) * 0.99f;
+    float dx   = (xmax - xmin) / ((float)num_steps);
+    float tol  = expf(-sqrtf(q32_fracbits));
+
+    unsigned int i;
+    float x = xmin;
+    for (i=0; i<num_steps; i++) {
+        // run test
+        q32_test_sqrt_logexp_shiftadd(x, precision, tol);
 
         // increment input parameter
         x += dx;
