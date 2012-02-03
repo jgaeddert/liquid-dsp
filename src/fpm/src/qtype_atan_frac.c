b5bb3337c6741fd2ffcab9faa4c89c7e15d4f927
Joseph Gaeddert <jgaedder@vt.edu>
2012-02-03 12:03:23 -0500

    fpm: cleaning up code for atan2 method using look-up tables

--- src/fpm/src/qtype_atan_frac.c
+++ src/fpm/src/qtype_atan_frac.c
@@ -26,11 +26,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-#include <assert.h>
 
 #include "liquidfpm.internal.h"
 
 #define DEBUG_ATAN_FRAC 0
+
+#if DEBUG_ATAN_FRAC
+#  include <assert.h>
+#endif
 
 Q(_t) Q(_atan2_frac)( Q(_t) _y, Q(_t) _x )
 {
@@ -74,34 +77,30 @@ Q(_t) Q(_atan2_frac)( Q(_t) _y, Q(_t) _x )
     // Ultimately we want to calculate log2(abs(y/x)).  The following
     // block of code achieves this with simple bit shifts, comparisons
     // and a 32-value lookup table.
-
-    // TODO : explain what is going on here to compute log2(abs(y/x))
-
-    // Extract location of MSB for _y and _x; keep values in register as
-    // operations on them will be computed very quickly
-
+#if DEBUG_ATAN_FRAC
     assert(_x >   0);
     assert(_y >= _x);
+#endif
 
     // 
-    // compute logs
+    // compute log2(_y) and log2(_x) using look-up tables
+    // NOTE: see qtype_log2_frac() function for details
     //
 
     // base indices
     int bx = liquid_msb_index(_x) - 1;
     int by = liquid_msb_index(_y) - 1;
 
-    // compute 8-bit fractional portions
+    // compute 8-bit fractional portions (indices for table)
     Q(_t) fx = (bx < 8) ? (_x<<(8-bx)) : (_x>>(bx-8));
     Q(_t) fy = (by < 8) ? (_y<<(8-by)) : (_y>>(by-8));
 
     // apply 8-bit mask (resolution) and extract fractional value from
     // look-up table
-    fx = Q(_log2_frac_gentab)[fx & 0x00ff];
-    fy = Q(_log2_frac_gentab)[fy & 0x00ff];
+    fx = Q(_log2_frac_gentab)[fx & 0xff];
+    fy = Q(_log2_frac_gentab)[fy & 0xff];
 
-    // compute log2(y) - log2(x) to 8 significant digits
-    // (resolution of atan2 look-up table)
+    // compute log2(y) - log2(x)
     int   b = by - bx;
     Q(_t) f = fy - fx;
     Q(_t) logdiff;
@@ -119,7 +118,13 @@ Q(_t) Q(_atan2_frac)( Q(_t) _y, Q(_t) _x )
         b--;
     }
 
+    // NOTE: log2(_y) - log2(_x) = b + f
+    //    b: integer component
+    //    f: fractional component, 0 <= f < 1
 #if DEBUG_ATAN_FRAC
+    assert(b >= 0);
+    assert(f >= 0);
+
     float yf = Q(_fixed_to_float)(_y);
     float xf = Q(_fixed_to_float)(_x);
     printf("    log2(%12.8f) = %3d + %12.10f (y)\n", Q(_fixed_to_float)(_y), by - Q(_fracbits), Q(_fixed_to_float)(fy));
@@ -127,12 +132,7 @@ Q(_t) Q(_atan2_frac)( Q(_t) _y, Q(_t) _x )
     printf("    logdiff            = %3d + %12.10f (%12.8f)\n", b, Q(_fixed_to_float)(f), log2f(yf/xf));
 #endif
 
-    // look-up table can be indexed from 'b' and 'f' relatively easily
-    assert(b >= 0);
-    assert(f >= 0);
-
-    // check boundary conditions...
-
+    // check boundary conditions (high ratio approximation)
     if ( b >= 8 ) {
         // use high ratio approximation
 #if DEBUG_ATAN_FRAC
@@ -144,15 +144,20 @@ Q(_t) Q(_atan2_frac)( Q(_t) _y, Q(_t) _x )
         else        return (negate      ? -Q(_pi_by_2) : Q(_pi_by_2));
     }
 
-    assert(Q(_fracbits) >= 3);
-    unsigned int index = (b << 5) | ( (f >> (Q(_fracbits)-3)) & 0x1f );
-    assert(index < 256);
-
+    // Look-up table can be indexed from 'b' and 'f' relatively easily.
+    // If the table has 2^8=256 values, then the index is the three
+    // most-significant bits of 'b' appended with the five most-
+    // significant bits of 'f'.
 #if DEBUG_ATAN_FRAC
+    assert(Q(_fracbits) >= 3);
+#endif
+    unsigned int index = ((b << 5) | ( (f >> (Q(_fracbits)-3)) & 0x1f )) & 0xff;
+#if DEBUG_ATAN_FRAC
+    assert(index < 256);
     printf("    index = %4u\n", index);
 #endif
 
-    // look-up table
+    // extract base phase from look-up table using index
     Q(_t) phi = Q(_atan2tab)[index];
 
     // invert if necessary
