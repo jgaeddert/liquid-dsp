63c0d9ea4373f0fc2cdb634410d6514c29a8f583
Joseph Gaeddert <jgaedder@vt.edu>
2012-02-29 15:34:37 -0500

    fft|sandbox-rader: fleshing out method to find primitive root
      * adding modpow() function to compute c = b^e (mod n)
      * fast, low-memory implementation

--- sandbox/fft_rader_prime_test.c
+++ sandbox/fft_rader_prime_test.c
@@ -62,6 +62,11 @@ int is_prime(unsigned int _n);
 // find smallest primitive root of _n (assuming _n is prime)
 unsigned int primitive_root(unsigned int _n);
 
+// compute c = base^exp (mod n)
+unsigned int modpow(unsigned int _base,
+                    unsigned int _exp,
+                    unsigned int _n);
+
 int main(int argc, char*argv[]) {
     // transform size (must be prime)
     unsigned int nfft = 17;
@@ -82,10 +87,21 @@ int main(int argc, char*argv[]) {
         exit(1);
     }
 
+#if 1
     // compute primitive root of nfft
     unsigned int g = primitive_root(nfft);
     printf("computed primitive root of %u as %u\n", nfft, g);
-    return 0;
+#else
+    {
+    unsigned int g = 3;
+    unsigned int i;
+    printf("s = [");
+    for (i=1; i<nfft; i++)
+        printf("%4u", modpow(g,i,nfft));
+    printf("]\n");
+    }
+#endif
+    //return 0;
 
     unsigned int i;
 
@@ -199,13 +215,49 @@ unsigned int primitive_root(unsigned int _n)
         }
     } while (n > 1 && num_unique_factors < MAX_FACTORS);
 
+#if 0
     // print unique factors
     printf("found %u unique factors of n-1 = %u\n", num_unique_factors, _n-1);
     for (k=0; k<num_unique_factors; k++)
         printf("  %3u\n", unique_factors[k]);
+#endif
 
-    // search for minimum integer...
+    // search for minimum integer for which
+    //   g^( (_n-1)/m ) != 1 (mod _n)
+    // for all unique roots 'm'
+    unsigned int g;
+    int root_found = 0;
+    for (g=2; g<_n; g++) {
+        int is_root = 1;
+        for (k=0; k<num_unique_factors; k++) {
+            unsigned int e = (_n-1) / unique_factors[k];
+            if ( modpow(g,e,_n) == 1 ) {
+                // not a root
+                is_root = 0;
+                break;
+            }
+        }
 
-    return 0;
+        if (is_root) {
+            //printf("  %u is a primitive root!\n", g);
+            root_found = 1;
+            break;
+        }
+    }
+
+    return g;
+}
+
+// compute c = base^exp (mod n)
+unsigned int modpow(unsigned int _base,
+                    unsigned int _exp,
+                    unsigned int _n)
+{
+    unsigned int c = 1;
+    unsigned int i;
+    for (i=0; i<_exp; i++)
+        c = (c * _base) % _n;
+
+    return c;
 }
 
