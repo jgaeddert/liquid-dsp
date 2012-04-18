945cd41cb6e421802692c4d2a9aee470648a5dfd
Joseph Gaeddert <jgaedder@vt.edu>
2012-04-17 21:16:01 -0400

    examples|fft: adding command-line options for FFT example

--- examples/fft_example.c
+++ examples/fft_example.c
@@ -8,12 +8,37 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <getopt.h>
 #include "liquid.h"
 
-int main() {
+// print usage/help message
+void usage()
+{
+    printf("fft_example [options]\n");
+    printf("  h     : print help\n");
+    printf("  v/q   : verbose/quiet\n");
+    printf("  n     : fft size\n");
+}
+
+int main(int argc, char*argv[])
+{
     // options
     unsigned int n=16;  // input data size
     int method=0;       // fft method (ignored)
+    int verbose = 0;    // verbose output?
+
+    int dopt;
+    while ((dopt = getopt(argc,argv,"hvqn:")) != EOF) {
+        switch (dopt) {
+        case 'h': usage();          return 0;
+        case 'v': verbose = 1;      break;
+        case 'q': verbose = 0;      break;
+        case 'n': n = atoi(optarg); break;
+        default:
+            exit(1);
+        }
+    }
 
     // allocated memory arrays
     float complex x[n]; // time-domain 'signal'
@@ -36,7 +61,7 @@ int main() {
 
     // print fft plans
     fft_print_plan(pf);
-    fft_print_plan(pr);
+    //fft_print_plan(pr);
 
     // execute fft plans
     fft_execute(pf);
@@ -51,16 +76,27 @@ int main() {
     for (i=0; i<n; i++)
         z[i] /= (float) n;
 
-    // print results
-    printf("original signal, x[n]:\n");
-    for (i=0; i<n; i++)
-        printf("  x[%3u] = %8.4f + j%8.4f\n", i, crealf(x[i]), cimagf(x[i]));
-    printf("y[n] = fft( x[n] ):\n");
-    for (i=0; i<n; i++)
-        printf("  y[%3u] = %8.4f + j%8.4f\n", i, crealf(y[i]), cimagf(y[i]));
-    printf("z[n] = ifft( y[n] ):\n");
-    for (i=0; i<n; i++)
-        printf("  z[%3u] = %8.4f + j%8.4f\n", i, crealf(z[i]), cimagf(z[i]));
+    if (verbose) {
+        // print results
+        printf("original signal, x[n]:\n");
+        for (i=0; i<n; i++)
+            printf("  x[%3u] = %8.4f + j%8.4f\n", i, crealf(x[i]), cimagf(x[i]));
+        printf("y[n] = fft( x[n] ):\n");
+        for (i=0; i<n; i++)
+            printf("  y[%3u] = %8.4f + j%8.4f\n", i, crealf(y[i]), cimagf(y[i]));
+        printf("z[n] = ifft( y[n] ):\n");
+        for (i=0; i<n; i++)
+            printf("  z[%3u] = %8.4f + j%8.4f\n", i, crealf(z[i]), cimagf(z[i]));
+    }
+
+    // compute RMSE between original and result
+    float rmse = 0.0f;
+    for (i=0; i<n; i++) {
+        float complex d = x[i] - z[i];
+        rmse += crealf(d * conjf(d));
+    }
+    rmse = sqrtf( rmse / (float)n );
+    printf("rmse = %12.4e\n", rmse);
 
     printf("done.\n");
     return 0;
