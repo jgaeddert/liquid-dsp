c7d5cf7f1c42062beaa9b4f507ddecb454741738
Joseph Gaeddert <jgaedder@vt.edu>
2012-04-21 18:30:52 -0400

    modem: porting DPSK modem to macros

--- src/modem/src/modem_dpsk.c
+++ src/modem/src/modem_dpsk.c
@@ -24,76 +24,76 @@
 //
 
 // create a dpsk (differential phase-shift keying) modem object
-modem modem_create_dpsk(unsigned int _bits_per_symbol)
+MODEM() MODEM(_create_dpsk)(unsigned int _bits_per_symbol)
 {
-    modem mod = (modem) malloc( sizeof(struct modem_s) );
+    MODEM() q = (MODEM()) malloc( sizeof(struct MODEM(_s)) );
     
     switch (_bits_per_symbol) {
-    case 1: mod->scheme = LIQUID_MODEM_DPSK2;   break;
-    case 2: mod->scheme = LIQUID_MODEM_DPSK4;   break;
-    case 3: mod->scheme = LIQUID_MODEM_DPSK8;   break;
-    case 4: mod->scheme = LIQUID_MODEM_DPSK16;  break;
-    case 5: mod->scheme = LIQUID_MODEM_DPSK32;  break;
-    case 6: mod->scheme = LIQUID_MODEM_DPSK64;  break;
-    case 7: mod->scheme = LIQUID_MODEM_DPSK128; break;
-    case 8: mod->scheme = LIQUID_MODEM_DPSK256; break;
+    case 1: q->scheme = LIQUID_MODEM_DPSK2;   break;
+    case 2: q->scheme = LIQUID_MODEM_DPSK4;   break;
+    case 3: q->scheme = LIQUID_MODEM_DPSK8;   break;
+    case 4: q->scheme = LIQUID_MODEM_DPSK16;  break;
+    case 5: q->scheme = LIQUID_MODEM_DPSK32;  break;
+    case 6: q->scheme = LIQUID_MODEM_DPSK64;  break;
+    case 7: q->scheme = LIQUID_MODEM_DPSK128; break;
+    case 8: q->scheme = LIQUID_MODEM_DPSK256; break;
     default:
         fprintf(stderr,"error: modem_create_dpsk(), cannot support DPSK with m > 8\n");
         exit(1);
     }
 
-    modem_init(mod, _bits_per_symbol);
+    MODEM(_init)(q, _bits_per_symbol);
 
-    mod->alpha = M_PI/(float)(mod->M);
+    q->alpha = M_PI/(T)(q->M);
 
     unsigned int k;
-    for (k=0; k<(mod->m); k++)
-        mod->ref[k] = (1<<k) * mod->alpha;
+    for (k=0; k<(q->m); k++)
+        q->ref[k] = (1<<k) * q->alpha;
 
-    mod->d_phi = M_PI*(1.0f - 1.0f/(float)(mod->M));
+    q->d_phi = M_PI*(1.0f - 1.0f/(T)(q->M));
 
     // reset modem
-    modem_reset(mod);
+    MODEM(_reset)(q);
 
-    mod->modulate_func = &modem_modulate_dpsk;
-    mod->demodulate_func = &modem_demodulate_dpsk;
+    q->modulate_func = &MODEM(_modulate_dpsk);
+    q->demodulate_func = &MODEM(_demodulate_dpsk);
 
-    return mod;
+    return q;
 }
 
 // modulate DPSK
-void modem_modulate_dpsk(modem _mod,
-                         unsigned int symbol_in,
-                         float complex *y)
+void MODEM(_modulate_dpsk)(MODEM()      _q,
+                           unsigned int _sym_in,
+                           TC *         _y)
 {
     // 'encode' input symbol (actually gray decoding)
-    symbol_in = gray_decode(symbol_in);
+    _sym_in = gray_decode(_sym_in);
 
     // compute phase difference between this symbol and the previous
-    _mod->dpsk_phi += symbol_in * 2 * _mod->alpha;
+    _q->dpsk_phi += _sym_in * 2 * _q->alpha;
 
     // limit phase
-    _mod->dpsk_phi -= (_mod->dpsk_phi > 2*M_PI) ? 2*M_PI : 0.0f;
+    _q->dpsk_phi -= (_q->dpsk_phi > 2*M_PI) ? 2*M_PI : 0.0f;
     
     // compute output sample
-    *y = liquid_cexpjf(_mod->dpsk_phi);
+    *_y = liquid_cexpjf(_q->dpsk_phi);
 
     // save symbol state
-    _mod->r = *y;
+    _q->r = *_y;
 }
 
 
-void modem_demodulate_dpsk(modem _demod,
-                           float complex _x,
-                           unsigned int * _symbol_out)
+void MODEM(_demodulate_dpsk)(MODEM()        _q,
+                             TC             _x,
+                             unsigned int * _sym_out)
 {
     // compute angle differencd
-    float theta = cargf(_x);
-    float d_theta = cargf(_x) - _demod->dpsk_phi;
-    _demod->dpsk_phi = theta;
+    T theta = cargf(_x);
+    T d_theta = cargf(_x) - _q->dpsk_phi;
+    _q->dpsk_phi = theta;
 
     // subtract phase offset, ensuring phase is in [-pi,pi)
-    d_theta -= _demod->d_phi;
+    d_theta -= _q->d_phi;
     if (d_theta > M_PI)
         d_theta -= 2*M_PI;
     else if (d_theta < -M_PI)
@@ -101,15 +101,15 @@ void modem_demodulate_dpsk(modem _demod,
 
     // demodulate on linearly-spaced array
     unsigned int s;             // demodulated symbol
-    float demod_phase_error;    // demodulation phase error
-    modem_demodulate_linear_array_ref(d_theta, _demod->m, _demod->ref, &s, &demod_phase_error);
+    T demod_phase_error;    // demodulation phase error
+    MODEM(_demodulate_linear_array_ref)(d_theta, _q->m, _q->ref, &s, &demod_phase_error);
 
     // 'decode' output symbol (actually gray encoding)
-    *_symbol_out = gray_encode(s);
+    *_sym_out = gray_encode(s);
 
     // re-modulate symbol (accounting for differential rotation)
     // and store state
-    _demod->x_hat = liquid_cexpjf(theta - demod_phase_error);
-    _demod->r = _x;
+    _q->x_hat = liquid_cexpjf(theta - demod_phase_error);
+    _q->r = _x;
 }
 
