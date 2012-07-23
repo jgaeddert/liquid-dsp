4b9eb7e5c66c5336bdd8055dd0f7dc8dccddd3af
Joseph Gaeddert <jgaedder@vt.edu>
2012-07-23 14:21:24 -0400

    dotprod/sumsq: adding MMX/SSE version

---
+++ src/dotprod/src/sumsq.mmx.c
@@ -0,0 +1,113 @@
+/*
+ * Copyright (c) 2007, 2008, 2009, 2010, 2012 Joseph Gaeddert
+ * Copyright (c) 2007, 2008, 2009, 2010, 2012 Virginia Polytechnic
+ *                                      Institute & State University
+ *
+ * This file is part of liquid.
+ *
+ * liquid is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * liquid is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with liquid.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+//
+// sumsq.mmx.c : floating-point sum of squares (MMX)
+//
+
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+#include "liquid.internal.h"
+
+// include proper SIMD extensions for x86 platforms
+// NOTE: these pre-processor macros are defined in config.h
+
+#if HAVE_MMINTRIN_H
+#include <mmintrin.h>   // MMX
+#endif
+
+#if HAVE_XMMINTRIN_H
+#include <xmmintrin.h>  // SSE
+#endif
+
+#if HAVE_EMMINTRIN_H
+#include <emmintrin.h>  // SSE2
+#endif
+
+#if HAVE_PMMINTRIN_H
+#include <pmmintrin.h>  // SSE3
+#endif
+
+// sum squares, basic loop
+//  _v      :   input array [size: 1 x _n]
+//  _n      :   input length
+float liquid_sumsqf(float *      _v,
+                    unsigned int _n)
+{
+    // first cut: ...
+    __m128 v;   // input vector
+    __m128 s;   // dot product
+    __m128 sum = _mm_setzero_ps(); // load zeros into sum register
+
+    // t = 4*(floor(_n/4))
+    unsigned int t = (_n >> 2) << 2;
+
+    //
+    unsigned int i;
+    for (i=0; i<t; i+=4) {
+        // load inputs into register (unaligned)
+        v = _mm_loadu_ps(&_v[i]);
+
+        // compute multiplication
+        s = _mm_mul_ps(v, v);
+       
+        // parallel addition
+        sum = _mm_add_ps( sum, s );
+    }
+
+    // aligned output array
+    float w[4] __attribute__((aligned(16)));
+
+#if HAVE_PMMINTRIN_H
+    // fold down into single value
+    __m128 z = _mm_setzero_ps();
+    sum = _mm_hadd_ps(sum, z);
+    sum = _mm_hadd_ps(sum, z);
+   
+    // unload single (lower value)
+    _mm_store_ss(w, sum);
+    float total = w[0];
+#else
+    // unload packed array
+    _mm_store_ps(w, sum);
+    float total = w[0] + w[1] + w[2] + w[3];
+#endif
+
+    // cleanup
+    for (; i<_n; i++)
+        total += _v[i] * _v[i];
+
+    // set return value
+    return total;
+}
+
+// sum squares, basic loop
+//  _v      :   input array [size: 1 x _n]
+//  _n      :   input length
+float liquid_sumsqcf(float complex * _v,
+                     unsigned int    _n)
+{
+    float * v = (float*) _v;
+
+    return liquid_sumsqf(v, 2*_n);
+}
