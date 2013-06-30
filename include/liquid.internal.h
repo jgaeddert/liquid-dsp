47039cd800ff81c4f71a9d18b92f507ae50877de
Joseph D. Gaeddert <joseph.gaeddert@fastmail.fm>
2013-06-30 10:08:48 -0400

    symsync: moving method prototypes from global internal header

--- include/liquid.internal.h
+++ include/liquid.internal.h
@@ -866,22 +866,6 @@ LIQUID_IIRFILTSOS_DEFINE_INTERNAL_API(IIRFILTSOS_MANGLE_CCCF,
                                       liquid_float_complex)
 
 
-// symsync
-#define LIQUID_SYMSYNC_DEFINE_INTERNAL_API(SYMSYNC,TO,TC,TI)    \
-void SYMSYNC(_step)(SYMSYNC() _q,                               \
-                    TI _x,                                      \
-                    TO *_y,                                     \
-                    unsigned int *_ny);                         \
-void SYMSYNC(_advance_internal_loop)(SYMSYNC() _q,              \
-                                     TO _mf,                    \
-                                     TO _dmf);                  \
-void SYMSYNC(_output_debug_file)(SYMSYNC() _q,                  \
-                                 const char * _filename);
-
-LIQUID_SYMSYNC_DEFINE_INTERNAL_API(SYMSYNC_MANGLE_RRRF, float, float, float)
-LIQUID_SYMSYNC_DEFINE_INTERNAL_API(SYMSYNC_MANGLE_CRCF, liquid_float_complex, float, liquid_float_complex)
-
-
 // firdes : finite impulse response filter design
 
 // Find approximate bandwidth adjustment factor rho based on
