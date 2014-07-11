f10cec2eaea8ceef61708ec84b4437420632cb65
Joseph Gaeddert <jgaedder@vt.edu>
2014-07-11 18:01:42 -0400

    build: adding basic copy for test license

--- scripts/update_copyright.py
+++ scripts/update_copyright.py
@@ -19,8 +19,24 @@ import re
 # new copyright
 newcopy = []
 newcopy.append('Copyright (c) 2007 - 2014 Joseph Gaeddert\n')
-newcopy.append('You may use this for whatever you want\n')
-newcopy.append('Hokies rule.\n')
+newcopy.append('\n')
+newcopy.append('Permission is hereby granted, free of charge, to any person obtaining a copy\n')
+newcopy.append('of this software and associated documentation files (the "Software"), to deal\n')
+newcopy.append('in the Software without restriction, including without limitation the rights\n')
+newcopy.append('to use, copy, modify, merge, publish, distribute, sublicense, and/or sell\n')
+newcopy.append('copies of the Software, and to permit persons to whom the Software is\n')
+newcopy.append('furnished to do so, subject to the following conditions:\n')
+newcopy.append('\n')
+newcopy.append('The above copyright notice and this permission notice shall be included in\n')
+newcopy.append('all copies or substantial portions of the Software.\n')
+newcopy.append('\n')
+newcopy.append('THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR\n')
+newcopy.append('IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,\n')
+newcopy.append('FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE\n')
+newcopy.append('AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER\n')
+newcopy.append('LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,\n')
+newcopy.append('OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN\n')
+newcopy.append('THE SOFTWARE.\n')
 
 rootdir = sys.argv[1]
 
@@ -86,9 +102,11 @@ def update_copyright(filename=""):
     for i in range(len(newcopy)):
         contents.insert(index_start+i, comment + ' ' + newcopy[i])
 
+    '''
     # print...
     for line in contents:
         print line.rstrip()
+    '''
 
     # open original file for writing
     with open( filename, 'w' ) as f:
