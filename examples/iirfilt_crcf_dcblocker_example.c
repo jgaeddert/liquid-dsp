15398af4324b6195ae24c5bfb4c68ee54b061c76
Joseph D. Gaeddert <support@liquidsdr.org>
2015-02-15 09:31:16 -0500

    iirfilt/dcblocker-example: cleaning up and improving example
      * comparing orignal input to output after DC blocking filter applied
      * this is better example of how the DC blocker is applied
      * resulting plot gives input, input + DC, output

--- examples/iirfilt_crcf_dcblocker_example.c
+++ examples/iirfilt_crcf_dcblocker_example.c
@@ -23,17 +23,21 @@ int main() {
     iirfilt_crcf_print(q);
 
     // allocate memory for data arrays
-    float complex x[num_samples];   // input
-    float complex y[num_samples];   // output
+    float complex x[num_samples];   // original input
+    float complex y[num_samples];   // input with DC offset
+    float complex z[num_samples];   // DC-blocked result
 
-    // generate input signal (sine wave with slowly-varying DC offset)
+    // generate signals
     unsigned int i;
     for (i=0; i<num_samples; i++) {
-        x[i] =       cexpf( 0.400f*_Complex_I*i );
-        x[i] += 2.0f*cexpf( 0.009f*_Complex_I*i );
+        // original input signal
+        x[i] = cexpf( (0.070f*i + 1e-4f*i*i)*_Complex_I );
 
-        // run filter
-        iirfilt_crcf_execute(q, x[i], &y[i]);
+        // add DC offset
+        y[i] = x[i] + 2.0f*cexpf( 0.007f*_Complex_I*i );
+
+        // run filter to try to remove DC offset
+        iirfilt_crcf_execute(q, y[i], &z[i]);
     }
 
     // destroy filter object
@@ -55,25 +59,40 @@ int main() {
     for (i=0; i<num_samples; i++) {
         fprintf(fid,"x(%4u) = %12.4e + j*%12.4e;\n", i+1, crealf(x[i]), cimagf(x[i]));
         fprintf(fid,"y(%4u) = %12.4e + j*%12.4e;\n", i+1, crealf(y[i]), cimagf(y[i]));
+        fprintf(fid,"z(%4u) = %12.4e + j*%12.4e;\n", i+1, crealf(z[i]), cimagf(z[i]));
     }
 
     // plot output
     fprintf(fid,"t=0:(num_samples-1);\n");
     fprintf(fid,"figure;\n");
-    fprintf(fid,"subplot(2,1,1);\n");
+    fprintf(fid,"subplot(3,1,1);\n");
     fprintf(fid,"  plot(t,real(x),'-','Color',[1 1 1]*0.5,'LineWidth',1,...\n");
-    fprintf(fid,"       t,real(y),'-','Color',[0 0.5 0.25],'LineWidth',2);\n");
+    fprintf(fid,"       t,imag(x),'-','Color',[0 0.2 0.5],'LineWidth',2);\n");
     fprintf(fid,"  xlabel('time');\n");
-    fprintf(fid,"  ylabel('real');\n");
-    fprintf(fid,"  legend('input','filtered output','location','northeast');\n");
+    fprintf(fid,"  ylabel('original input');\n");
+    fprintf(fid,"  legend('real','imag','location','southwest');\n");
+    fprintf(fid,"  axis([0 num_samples -3 3]);\n");
     fprintf(fid,"  grid on;\n");
-    fprintf(fid,"subplot(2,1,2);\n");
-    fprintf(fid,"  plot(t,imag(x),'-','Color',[1 1 1]*0.5,'LineWidth',1,...\n");
-    fprintf(fid,"       t,imag(y),'-','Color',[0 0.25 0.5],'LineWidth',2);\n");
+    fprintf(fid,"subplot(3,1,2);\n");
+    fprintf(fid,"  plot(t,real(y),'-','Color',[1 1 1]*0.5,'LineWidth',1,...\n");
+    fprintf(fid,"       t,imag(y),'-','Color',[0 0.5 0.2],'LineWidth',2);\n");
     fprintf(fid,"  xlabel('time');\n");
-    fprintf(fid,"  ylabel('imag');\n");
-    fprintf(fid,"  legend('input','filtered output','location','northeast');\n");
+    fprintf(fid,"  ylabel('input with DC offset');\n");
+    fprintf(fid,"  legend('real','imag','location','southwest');\n");
+    fprintf(fid,"  axis([0 num_samples -3 3]);\n");
     fprintf(fid,"  grid on;\n");
+    fprintf(fid,"subplot(3,1,3);\n");
+    fprintf(fid,"  plot(t,real(z),'-','Color',[1 1 1]*0.5,'LineWidth',1,...\n");
+    fprintf(fid,"       t,imag(z),'-','Color',[0 0.2 0.5],'LineWidth',2);\n");
+    fprintf(fid,"  xlabel('time');\n");
+    fprintf(fid,"  ylabel('DC-blocked output');\n");
+    fprintf(fid,"  legend('real','imag','location','southwest');\n");
+    fprintf(fid,"  axis([0 num_samples -3 3]);\n");
+    fprintf(fid,"  grid on;\n");
+
+    // close output file
+    fclose(fid);
+    printf("results written to '%s'\n", OUTPUT_FILENAME);
 
     printf("done.\n");
     return 0;
