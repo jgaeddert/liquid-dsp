48f2040c8858e91891a657f0107b487fb9052cf0
Joseph D. Gaeddert <support@liquidsdr.org>
2015-02-21 14:27:03 -0500

    qpacketmodem/autotest: adding autotests for different modulation schemes

--- src/framing/tests/qpacketmodem_autotest.c
+++ src/framing/tests/qpacketmodem_autotest.c
@@ -26,31 +26,29 @@
 // 
 // AUTOTEST : test simple recovery of frame in noise
 //
-void autotest_qpacketmodem()
+void qpacketmodem_test(unsigned int _payload_len,
+                       int          _check,
+                       int          _fec0,
+                       int          _fec1,
+                       int          _ms)
 {
-    // options
-    // TODO: test different combinations
-    modulation_scheme ms     = LIQUID_MODEM_QPSK;       // mod. scheme
-    crc_scheme check         = LIQUID_CRC_32;           // data validity check
-    fec_scheme fec0          = LIQUID_FEC_GOLAY2412;    // fec (inner)
-    fec_scheme fec1          = LIQUID_FEC_NONE;         // fec (outer)
-    unsigned int payload_len = 400;                     // payload length
-
     // derived values
     unsigned int i;
 
     // create and configure packet encoder/decoder object
     qpacketmodem q = qpacketmodem_create();
-    qpacketmodem_configure(q, payload_len, check, fec0, fec1, ms);
+    qpacketmodem_configure(q, _payload_len, _check, _fec0, _fec1, _ms);
+    if (liquid_autotest_verbose)
+        qpacketmodem_print(q);
 
     // initialize payload
-    unsigned char payload_tx[payload_len];
-    unsigned char payload_rx[payload_len];
+    unsigned char payload_tx[_payload_len];
+    unsigned char payload_rx[_payload_len];
 
     // initialize payload
-    for (i=0; i<payload_len; i++) {
+    for (i=0; i<_payload_len; i++) {
         payload_tx[i] = rand() & 0xff;
-        payload_rx[i] = 0x00;
+        payload_rx[i] = rand() & 0xff;
     }
 
     // get frame length
@@ -70,6 +68,15 @@ void autotest_qpacketmodem()
 
     // check to see that frame was recovered
     CONTEND_EQUALITY( crc_pass, 1 );
-    CONTEND_SAME_DATA( payload_tx, payload_rx, payload_len );
+    CONTEND_SAME_DATA( payload_tx, payload_rx, _payload_len );
 }
+
+void autotest_qpacketmodem_bpsk()   { qpacketmodem_test(400,LIQUID_CRC_32,LIQUID_FEC_NONE,LIQUID_FEC_NONE, LIQUID_MODEM_QPSK);    }
+void autotest_qpacketmodem_qpsk()   { qpacketmodem_test(400,LIQUID_CRC_32,LIQUID_FEC_NONE,LIQUID_FEC_NONE, LIQUID_MODEM_QPSK);    }
+void autotest_qpacketmodem_psk8()   { qpacketmodem_test(400,LIQUID_CRC_32,LIQUID_FEC_NONE,LIQUID_FEC_NONE, LIQUID_MODEM_PSK8);    }
+void autotest_qpacketmodem_qam16()  { qpacketmodem_test(400,LIQUID_CRC_32,LIQUID_FEC_NONE,LIQUID_FEC_NONE, LIQUID_MODEM_QAM16);   }
+void autotest_qpacketmodem_sqam32() { qpacketmodem_test(400,LIQUID_CRC_32,LIQUID_FEC_NONE,LIQUID_FEC_NONE, LIQUID_MODEM_SQAM32);  }
+void autotest_qpacketmodem_qam64()  { qpacketmodem_test(400,LIQUID_CRC_32,LIQUID_FEC_NONE,LIQUID_FEC_NONE, LIQUID_MODEM_QAM64);   }
+void autotest_qpacketmodem_sqam128(){ qpacketmodem_test(400,LIQUID_CRC_32,LIQUID_FEC_NONE,LIQUID_FEC_NONE, LIQUID_MODEM_SQAM128); }
+void autotest_qpacketmodem_qam256() { qpacketmodem_test(400,LIQUID_CRC_32,LIQUID_FEC_NONE,LIQUID_FEC_NONE, LIQUID_MODEM_QAM256);  }
 
