c0bab164e488b4dccd21b369780d5fe407b2fd89
Joseph D. Gaeddert <joseph@liquidsdr.org>
2017-01-05 19:22:10 -0500

    asgram: increasing resolution of result
      * internally taking larger transform (4 times larger)
      * when display is requested, output reduces back down
      * currently averaging over PSD in dB
      * results in much tighter spectral display

--- src/fft/src/asgram.c
+++ src/fft/src/asgram.c
@@ -33,7 +33,9 @@
 #include "liquid.internal.h"
 
 struct ASGRAM(_s) {
-    unsigned int nfft;          // transform size
+    unsigned int nfft;          // transform size (display)
+    unsigned int nfftp;         // transform size (processing)
+    unsigned int p;             // over-sampling rate
     SPGRAM()     periodogram;   // spectral periodogram object
     TC *         X;             // spectral periodogram output
     float *      psd;           // power spectral density
@@ -57,14 +59,18 @@ ASGRAM() ASGRAM(_create)(unsigned int _nfft)
     // create main object
     ASGRAM() q = (ASGRAM()) malloc(sizeof(struct ASGRAM(_s)));
 
-    q->nfft = _nfft;
+    q->nfft  = _nfft;
+
+    // derived values
+    q->p     = 4;   // over-sampling rate
+    q->nfftp = q->nfft * q->p;
 
     // allocate memory for PSD estimate
-    q->X   = (TC *   ) malloc((q->nfft)*sizeof(TC)   );
-    q->psd = (float *) malloc((q->nfft)*sizeof(float));
+    q->X   = (TC *   ) malloc((q->nfftp)*sizeof(TC)   );
+    q->psd = (float *) malloc((q->nfftp)*sizeof(float));
 
     // create spectral periodogram object
-    q->periodogram = SPGRAM(_create_default)(q->nfft);
+    q->periodogram = SPGRAM(_create)(q->nfftp,LIQUID_WINDOW_HANN,q->nfft,q->nfft/2);
 
     // power spectral density levels
     q->num_levels = 10;
@@ -118,7 +124,7 @@ void ASGRAM(_set_scale)(ASGRAM() _q,
     }
 
     _q->ref = _ref;
-    _q->div  = _div;
+    _q->div = _div;
 
     unsigned int i;
     for (i=0; i<_q->num_levels; i++)
@@ -150,31 +156,58 @@ void ASGRAM(_write)(ASGRAM()     _q,
 
 // compute spectral periodogram output from current buffer contents
 //  _q          :   ascii spectrogram object
-//  _ascii      :   character buffer [size: 1 x n]
+//  _ascii      :   output ASCII string [size: _nfft x 1]
 //  _peakval    :   value at peak (returned value)
 //  _peakfreq   :   frequency at peak (returned value)
-void ASGRAM(_execute)(ASGRAM()  _q,
-                      char *    _ascii,
-                      float *   _peakval,
-                      float *   _peakfreq)
+void ASGRAM(_execute)(ASGRAM() _q,
+                      char *   _ascii,
+                      float *  _peakval,
+                      float *  _peakfreq)
 {
+    // check number of transforms
+    if (SPGRAM(_get_num_transforms)(_q->periodogram)==0) {
+        memset(_ascii,' ',_q->nfft);
+        *_peakval = 0.0f;
+        *_peakfreq = 0.0f;
+        return;
+    }
+
     // execute spectral periodogram
     SPGRAM(_get_psd)(_q->periodogram, _q->psd);
     SPGRAM(_reset)(_q->periodogram);
 
     unsigned int i;
     unsigned int j;
-    for (i=0; i<_q->nfft; i++) {
-        // find peak
+    // find peak
+    for (i=0; i<_q->nfftp; i++) {
         if (i==0 || _q->psd[i] > *_peakval) {
             *_peakval = _q->psd[i];
-            *_peakfreq = (float)(i) / (float)(_q->nfft) - 0.5f;
+            *_peakfreq = (float)(i) / (float)(_q->nfftp) - 0.5f;
         }
+    }
+
+    // down-sample from nfft*p frequency bins to just nfft by retaining
+    // one value (e.g. maximum or average) over range.
+    for (i=0; i<_q->nfft; i++) {
+#if 1
+        // find average
+        float psd_val = 0.0f;
+        for (j=0; j<_q->p; j++)
+            psd_val += _q->psd[(_q->p*i) + j];
+        psd_val /= (float)(_q->p);
+#else
+        // find maximum within 'p' samples
+        float psd_val = 0.0f;
+        for (j=0; j<_q->p; j++) {
+            unsigned int index = (_q->p*i) + j;
+            psd_val = (j==0 || _q->psd[index] > psd_val) ? _q->psd[index] : psd_val;
+        }
+#endif
 
         // determine ascii level (which character to use)
         _ascii[i] = _q->levelchar[0];
         for (j=0; j<_q->num_levels; j++) {
-            if ( _q->psd[i] > _q->levels[j] )
+            if ( psd_val > _q->levels[j] )
                 _ascii[i] = _q->levelchar[j];
         }
     }
