bd215d51ccfdc2472ec209ba94dcf2fe7cc6cfcc
Joseph D. Gaeddert <joseph@liquidsdr.org>
2019-08-20 10:50:29 -0400

    bind: moving to header-only implementation for simplicity

--- bind/firfilt.hh
+++ bind/firfilt.hh
@@ -13,8 +13,10 @@ class firfilt
     firfilt(unsigned int _n=51, float _fc=0.25, float _As=60.0f, float _mu=0.0f)
         { q = firfilt_crcf_create_kaiser(_n, _fc, _As, _mu); }
 
-    ~firfilt();
+    // destructor
+    ~firfilt() { firfilt_crcf_destroy(q); }
 
+    // reset object
     void reset() { firfilt_crcf_reset(q); }
 
     std::complex<float> execute(std::complex<float> _x)
@@ -25,12 +27,47 @@ class firfilt
         return y;
     }
 
-#ifdef PYTHONLIB
-    void py_execute(py::array_t<std::complex<float>> & _buf);
-#endif
-
   private:
     firfilt_crcf q;
+
+#ifdef PYTHONLIB
+  public:
+    void py_execute(py::array_t<std::complex<float>> & _buf)
+    {
+        // get buffer info
+        py::buffer_info info = _buf.request();
+
+        // verify input size and dimensions
+        if (info.itemsize != sizeof(std::complex<float>))
+            throw std::runtime_error("invalid input numpy size, use dtype=np.csingle");
+        if (info.ndim != 1)
+            throw std::runtime_error("invalid number of input dimensions, must be 1-D array");
+
+        // comptue sample size, number of samples in buffer, and stride between samples
+        size_t       s           = sizeof(std::complex<float>);
+        unsigned int num_samples = info.shape[0];
+        unsigned int stride      = info.strides[0]/s;
+
+        // convert buffers to pointers
+        std::complex<float> * ptr = (std::complex<float>*) info.ptr;
+
+        // run filter (in place)
+        for (auto i=0; i<num_samples; i++)
+            ptr[i*stride] = execute(ptr[i*stride]);
+    }
+#endif
 };
+
+#ifdef PYTHONLIB
+void init_firfilt(py::module &m)
+{
+    py::class_<firfilt> f(m, "firfilt");
+    f.def(py::init<unsigned int,float,float,float>(),
+          py::arg("n")=51, py::arg("fc")=0.25, py::arg("As")=60, py::arg("mu")=0)
+     .def("reset",   &firfilt::reset,      "reset object's internal state")
+     .def("execute", &firfilt::py_execute, "execute on a block of samples")
+     ;
+}
+#endif
 
 #endif //__FIRFILT_HH__
--- bind/liquid.python.cc
+++ bind/liquid.python.cc
@@ -1,5 +1,7 @@
 #include "liquid.python.hh"
 
+#include "firfilt.hh"
+
 PYBIND11_MODULE(liquid, m) {
     m.doc() = "software-defined radio signal processing library";
     init_firfilt(m);
