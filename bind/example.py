9f8ba8c154acc3df0f619dbee536230a83ba1192
Joseph D. Gaeddert <joseph@liquidsdr.org>
2019-09-27 13:38:00 -0400

    bind: adding more constructors to firfilt class

--- bind/example.py
+++ bind/example.py
@@ -3,6 +3,7 @@ import numpy as np
 import liquid as dsp
 
 filt = dsp.firfilt(n=13)
+filt.display()
 buf = np.zeros((13,), dtype=np.csingle)
 buf[0] = 1.
 filt.execute(buf)
--- bind/firfilt.hh
+++ bind/firfilt.hh
@@ -9,9 +9,16 @@
 class firfilt
 {
   public:
-    // kaiser
+    // external coefficients
+    firfilt(float * _h, unsigned int _n) { q = firfilt_crcf_create(_h, _n); }
+
+    // default: kaiser low-pass
     firfilt(unsigned int _n=51, float _fc=0.25, float _As=60.0f, float _mu=0.0f)
         { q = firfilt_crcf_create_kaiser(_n, _fc, _As, _mu); }
+
+    // rnyquist
+    firfilt(int _type, unsigned int _k, float _m=7, float _beta=0.25f, float _mu=0.0f)
+        { q = firfilt_crcf_create_rnyquist(_type, _k, _m, _beta, _mu); }
 
     // destructor
     ~firfilt() { firfilt_crcf_destroy(q); }
@@ -19,6 +26,10 @@ class firfilt
     // reset object
     void reset() { firfilt_crcf_reset(q); }
 
+    //
+    void display() { firfilt_crcf_print(q); }
+
+    // push/execute one sample at a time
     std::complex<float> execute(std::complex<float> _x)
     {
         std::complex<float> y;
@@ -62,11 +73,11 @@ class firfilt
 void init_firfilt(py::module &m)
 {
     py::class_<firfilt> f(m, "firfilt");
-    f.def(py::init<unsigned int,float,float,float>(),
-          py::arg("n")=51, py::arg("fc")=0.25, py::arg("As")=60, py::arg("mu")=0)
-     .def("reset",   &firfilt::reset,      "reset object's internal state")
-     .def("execute", &firfilt::py_execute, "execute on a block of samples")
-     ;
+        f.def(py::init<unsigned int,float,float,float>(),
+             py::arg("n")=51, py::arg("fc")=0.25, py::arg("As")=60, py::arg("mu")=0)
+        .def("reset",   &firfilt::reset,      "reset object's internal state")
+        .def("execute", &firfilt::py_execute, "execute on a block of samples")
+        ;
 }
 #endif
 
