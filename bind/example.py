c73108c04b92cb773fc03888bb092a0d50f42fd6
Joseph D. Gaeddert <joseph@liquidsdr.org>
2019-09-28 13:47:54 -0400

    bind: testing with different constructor/__init__ methods

--- bind/example.py
+++ bind/example.py
@@ -4,7 +4,7 @@ sys.path.append('.')
 import numpy as np
 import liquid as dsp
 
-filt = dsp.firfilt(n=13)
+filt = dsp.firfilt(ftype=7, m=2, k=4)
 filt.display()
 buf = np.zeros((13,), dtype=np.csingle)
 buf[0] = 1.
--- bind/firfilt.hh
+++ bind/firfilt.hh
@@ -17,8 +17,8 @@ class firfilt
         { q = firfilt_crcf_create_kaiser(_n, _fc, _As, _mu); }
 
     // rnyquist
-    firfilt(int _type, unsigned int _k, float _m=7, float _beta=0.25f, float _mu=0.0f)
-        { q = firfilt_crcf_create_rnyquist(_type, _k, _m, _beta, _mu); }
+    firfilt(int _ftype, unsigned int _k, unsigned int _m=7, float _beta=0.25f, float _mu=0.0f)
+        {  q = firfilt_crcf_create_rnyquist(_ftype, _k, _m, _beta, _mu); }
 
     // destructor
     ~firfilt() { firfilt_crcf_destroy(q); }
@@ -74,7 +74,9 @@ void init_firfilt(py::module &m)
 {
     py::class_<firfilt>(m, "firfilt")
         .def(py::init<unsigned int,float,float,float>(),
-            py::arg("n")=51, py::arg("fc")=0.25, py::arg("As")=60, py::arg("mu")=0)
+             py::arg("n")=51, py::arg("fc")=0.25, py::arg("As")=60, py::arg("mu")=0)
+        .def(py::init<int,unsigned int,unsigned int,float,float>(),
+             py::arg("ftype")=7, py::arg("k")=2, py::arg("m")=5, py::arg("beta")=0.25, py::arg("mu")=0)
         .def("reset",   &firfilt::reset,      "reset object's internal state")
         .def("display", &firfilt::display,    "print object properties to stdout")
         .def("execute", &firfilt::py_execute, "execute on a block of samples")
