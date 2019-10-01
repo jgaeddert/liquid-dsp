a4bfb9db52dfa6ac69d46e8c511a4baf83dc2c51
Joseph D. Gaeddert <joseph@liquidsdr.org>
2019-10-01 12:58:13 -0400

    bind: fixing c++ interface to main test program

--- bind/main.cc
+++ bind/main.cc
@@ -7,7 +7,7 @@ int main()
     firfilt filter;
     for (auto i=0; i<10; i++) {
         std::complex<float> x(i==0 ? 1.0 : 0.0, 0.0);
-        std::complex<float> y = filter.execute(x);
+        std::complex<float> y = filter.step(x);
         printf("%12.8f %12.8f\n", y.real(), y.imag());
     }
     return 0;
