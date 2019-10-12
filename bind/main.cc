5e834cac54e38a7ee02af9da949753f2e312af8a
Joseph D. Gaeddert <joseph@liquidsdr.org>
2019-10-12 17:33:26 +0000

    bind: specifying unsigned int in c++ test program loop counter

--- bind/main.cc
+++ bind/main.cc
@@ -7,7 +7,7 @@ namespace dsp = liquid;
 int main()
 {
     dsp::firfilt filter;
-    for (auto i=0; i<filter.get_length(); i++) {
+    for (auto i=0U; i<filter.get_length(); i++) {
         std::complex<float> x(i==0 ? 1.0 : 0.0, 0.0);
         std::complex<float> y = filter.step(x);
         printf("%12.8f %12.8f\n", y.real(), y.imag());
