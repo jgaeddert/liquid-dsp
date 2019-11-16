e6f0cf10107bc75c0c42199bd9756faf921b8948
Joseph D. Gaeddert <joseph@liquidsdr.org>
2019-11-16 16:34:31 -0500

    bind/example: plotting real part of impulse response

--- bind/example.py
+++ bind/example.py
@@ -6,14 +6,14 @@ import liquid as dsp
 import matplotlib.pyplot as plt
 
 # generate coefficients and pass them to filter object
-m = 40      # filter semi-length
-fc= 0.1     # filter cut-off frequency
-t = np.arange(2*m+1) - m
-h = np.sinc(fc*t) * np.cos(0.5*np.pi*t/m)**2
-filt = dsp.firfilt(h)
+#m = 40      # filter semi-length
+#fc= 0.1     # filter cut-off frequency
+#t = np.arange(2*m+1) - m
+#h = np.sinc(fc*t) * np.cos(0.5*np.pi*t/m)**2
+#filt = dsp.firfilt(h)
 
 # design prototype filter
-#filt = dsp.firfilt("lowpass", n=151, fc=0.07)
+filt = dsp.firfilt("lowpass", n=151, fc=0.07)
 #filt = dsp.firfilt("firdespm", n=151, fc=0.07)
 #filt = dsp.firfilt("rkaiser", k=8, m=12, beta=0.25)
 #filt = dsp.firfilt("rect", n=5)
@@ -33,7 +33,7 @@ psd = 20*np.log10(np.abs(np.fft.fftshift(np.fft.fft(buf, nfft))))
 
 # plot impulse and spectral responses
 fig, ax = plt.subplots(2,figsize=(8,8))
-ax[0].plot(np.arange(n)-n//2, buf)
+ax[0].plot(np.arange(n)-n//2, np.real(buf))
 ax[0].set_xlabel('Delay [samples]')
 ax[0].set_ylabel('Impulse Response')
 ax[0].grid(True, zorder=5)
