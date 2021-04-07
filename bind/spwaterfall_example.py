2308c7c1ce31f7797d69eb0ad90fb1dee85f32b5
Joseph D. Gaeddert <joseph@liquidsdr.org>
2021-04-07 09:24:36 -0400

    bind: simplifying spwaterfall example script to generate random frames

--- bind/spwaterfall_example.py
+++ bind/spwaterfall_example.py
@@ -7,8 +7,8 @@ import matplotlib.pyplot as plt
 
 num_samples = 20e3
 
-def add_noise(v,nstd=0.1):
-    return v + nstd*np.random.randn(2*len(v)).astype(np.single).view(np.csingle)*0.70711
+def noise(n,nstd=0.1):
+    return nstd*np.random.randn(2*n).astype(np.single).view(np.csingle)*0.70711
 
 # generate frame generator
 fg = dsp.fg64()
@@ -18,16 +18,15 @@ n  = fg.get_frame_length()
 psd   = dsp.spwaterfall(nfft=600,time=800,wlen=400,delay=1)
 frame = np.zeros((n,), dtype=np.csingle)
 while psd.num_samples_total < num_samples:
-    header  = np.random.randint(256, size= 8)
-    payload = np.random.randint(256, size=64)
-    fg.execute(header, payload, frame)
+    # run some noise through
+    psd.execute(noise(n))
 
-    psd.execute(add_noise(frame))
+    # generate frame with random payload
+    fg.execute(frame)
+    psd.execute(frame + noise(n))
 
-    # add some space in between
-    for i in range(4):
-        frame *= 0
-        psd.execute(add_noise(frame))
+    # run some noise through
+    psd.execute(noise(n))
 
 # get spectrum plot and display
 print(psd)
