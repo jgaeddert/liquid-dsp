55201fb89f45d8a3e77650ea0222e03dabec07ce
Joseph D. Gaeddert <joseph@liquidsdr.org>
2021-05-14 13:24:12 -0400

    dotprod: adding macro defining {TO,TC,TI} as complex/real

--- src/dotprod/src/dotprod_cccf.c
+++ src/dotprod/src/dotprod_cccf.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2007 - 2015 Joseph Gaeddert
+ * Copyright (c) 2007 - 2021 Joseph Gaeddert
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -31,5 +31,9 @@
 #define TO              float complex
 #define TC              float complex
 #define TI              float complex
+
+#define TO_COMPLEX      1
+#define TC_COMPLEX      1
+#define TI_COMPLEX      1
 
 #include "dotprod.c"
