034e34b30d0d7d0f0142fac314d00eefbcfa70b3
Joseph D. Gaeddert <joseph@liquidsdr.org>
2021-05-23 17:02:58 -0400

    math/poly: fixing typo with T being improperly defined as complex

--- src/math/src/poly.c
+++ src/math/src/poly.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2007 - 2015 Joseph Gaeddert
+ * Copyright (c) 2007 - 2021 Joseph Gaeddert
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -33,7 +33,7 @@
 #define T               double
 #define TC              double complex
 
-#define T_COMPLEX       1
+#define T_COMPLEX       0
 #define TI_COMPLEX      1
 
 #define T_ABS(X)        fabs(X)
