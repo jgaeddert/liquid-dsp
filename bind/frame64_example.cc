aeeda28ce2bb43f3b6ab44e92e9334c1978a1913
Joseph D. Gaeddert <joseph@liquidsdr.org>
2021-08-16 15:43:23 -0400

    bind/frame64_example: adding context, cleaning up example

--- bind/frame64_example.cc
+++ bind/frame64_example.cc
@@ -5,7 +5,10 @@
 
 namespace dsp = liquid;
 
-// TODO: make this easier for C++
+// context for passing to callback
+struct context_s { int num_detected; };
+
+// user-defined callback
 int my_callback(
         unsigned char *  _header,
         int              _header_valid,
@@ -16,33 +19,27 @@ int my_callback(
         void *           _userdata)
 {
     printf("callback invoked! payload: %s\n", _payload_valid ? "pass" : "FAIL");
-    printf(" header: ");
-    for (auto i=0; i<8; i++)
-        printf(" %.2x", _header[i]);
-    printf("\n payload:");
-    for (auto i=0; i<64; i++)
-        printf(" %.2x%s", _payload[i], (i+4)%11==0 || i==63 ? "\n" : "");
+    ((context_s *)_userdata)->num_detected++;
     return 0;
 }
 
 int main()
 {
-    // create frame generator
-    dsp::fg64 fg;
-
-    // create frame synchronizer
-    dsp::fs64 fs(my_callback, NULL);
-
-    // generate a frame
+    // generate header and payload bytes
     uint8_t header[8], payload[64];
     for (auto i=0; i< 8; i++) header[i]  = rand() & 0xff;
     for (auto i=0; i<64; i++) payload[i] = rand() & 0xff;
-    // frame buffer
+
+    // create frame generator and write frame to buffer
+    dsp::fg64 fg;
     unsigned int frame_len = fg.get_frame_length();
     std::complex<float> frame_buf[frame_len];
-
     fg.execute(header, payload, frame_buf);
-    fs.execute(frame_buf, frame_len);
 
+    // create frame synchronizer and receive frame
+    context_s context = {.num_detected=0};
+    dsp::fs64 fs(my_callback, &context);
+    fs.execute(frame_buf, frame_len);
+    printf("number of packets detected: %u\n", context.num_detected);
     return 0;
 }
