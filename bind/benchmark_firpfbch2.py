a4717ed09511c4383e69404a6e7ee7ba1964665d
Joseph D. Gaeddert <joseph@liquidsdr.org>
2021-08-18 22:09:32 -0400

    bind: adding firpfbch2 benchmark

---
+++ bind/benchmark_firpfbch2.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+import sys, time
+sys.path.append('.')
+import numpy as np
+import liquid as dsp
+
+def benchmark_firpfbch2(M,runtime=1.0):
+    # design prototype filter
+    qa = dsp.firpfbch2a(M, m=2, As=60)
+    qs = dsp.firpfbch2s(M, m=2, As=60)
+
+    # run in blocks until about time has elapsed
+    M2 = M//2
+    b0 = np.zeros((M2,), dtype=np.csingle)
+    b1 = np.zeros((M ,), dtype=np.csingle)
+    print('%4u:' % M, end='')
+    for q,desc,buf in ((qa,'analysis',b0),(qs,'synthesis',b1)):
+        tic = time.time()
+        n   = 0
+        while time.time() - tic < runtime:
+            q.execute(buf)
+            n += M2
+
+        # print results
+        t = time.time() - tic
+        print(' %s: %6.2fM/%5.3fs (%7.3f Ms/s)' % (desc,n*1e-6, t, n/t*1e-6), end='')
+    print('', end='\n')
+
+# run batch of benchmarks
+for M in (16,32,64,128,256,512,1024,2048,4096,):
+    benchmark_firpfbch2(M)
+
