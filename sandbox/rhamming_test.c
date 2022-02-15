a29fffe7d4339dc5b096d952a3bca429db2349fb
Joseph D. Gaeddert <joseph@liquidsdr.org>
2022-02-14 22:28:09 -0500

    sandbox/rhamming: adding verbose option to search method

--- sandbox/rhamming_test.c
+++ sandbox/rhamming_test.c
@@ -7,7 +7,7 @@
 
 #define OUTPUT_FILENAME "rhamming_test.m"
 
-float bisect_search(int _wtype, unsigned int _k, unsigned int _m, float _arg, float * _h);
+float bisect_search(int _wtype, unsigned int _k, unsigned int _m, float _arg, float * _h, int _verbose);
 float bisect_eval  (int _wtype, unsigned int _k, unsigned int _m, float _arg, float * _h, float _gamma);
 
 int main() {
@@ -16,12 +16,14 @@ int main() {
     unsigned int m     = 12;
     int          wtype = LIQUID_WINDOW_HAMMING;
     float        arg   = 0;
+    int          verbose = 1;
 
     // derived values
     unsigned int h_len = 2*k*m+1;
     float h[h_len];
 
-    float gamma_opt = bisect_search(wtype, k, m, arg, h);
+    float gamma_opt = bisect_search(wtype, k, m, arg, h, verbose);
+    printf(" k:%2u, m:%2u, gamma:%12.8f\n", k, m, gamma_opt);
 
     // re-design with optimum and save to file
     //printf("optimum RMS ISI of %.2f dB with gamma=%.6f\n", 20*log10(isi_opt), gamma_opt);
@@ -45,28 +47,27 @@ int main() {
     return 0;
 }
 
-float bisect_search(int _wtype, unsigned int _k, unsigned int _m, float _arg, float * _h)
+float bisect_search(int _wtype, unsigned int _k, unsigned int _m,
+                    float _arg, float * _h, int _verbose)
 {
     // design filter
     float g0 = 1.00f; float u0 = bisect_eval(_wtype,_k,_m,_arg,_h,g0);
     float g1 = 1.05f; float u1 = bisect_eval(_wtype,_k,_m,_arg,_h,g1);
-    float g2 = 1.10f; float u2 = bisect_eval(_wtype,_k,_m,_arg,_h,g2);
+    float g2 = 1.50f; float u2 = bisect_eval(_wtype,_k,_m,_arg,_h,g2);
 
     // assume u1 < {u0,u2}
     // { g0, ga, g1, gb, g2 }
-    unsigned int num_steps = 10;
+    unsigned int num_steps = 16;
     unsigned int i = 0;
     for (i=0; i<num_steps; i++) {
         float ga = sqrtf(g0*g1); float ua = bisect_eval(_wtype,_k,_m,_arg,_h,ga);
         float gb = sqrtf(g1*g2); float ub = bisect_eval(_wtype,_k,_m,_arg,_h,gb);
-        printf("g:{%8.6f, %8.6f, %8.6f, %8.6f, %8.6f}, isi:{%4.2f, %4.2f, %4.2f, %4.2f, %4.2f}\n",
-                g0, ga, g1, gb, g2,
-                u0, ua, u1, ub, u2);
+        if (_verbose) {
+            printf("g:{%8.6f, %8.6f, %8.6f, %8.6f, %8.6f}, isi:{%6.3f,%6.3f,%6.3f,%6.3f,%6.3f}\n",
+                    g0, ga, g1, gb, g2, u0, ua, u1, ub, u2);
+        }
 
         // find minimum of {ua, u1, ub}
-        // ga: { (g0  ga  g1) gb  g2 }
-        // g1: {  g0 (ga  g1  gb) g2 }
-        // gb: {  g0  ga (g1  gb  g2)}
         if (ua < u1 && ua < ub) {
             g2 = g1; u2 = u1;
             g1 = ga; u1 = ua;
@@ -78,12 +79,11 @@ float bisect_search(int _wtype, unsigned int _k, unsigned int _m, float _arg, fl
             g1 = gb; u1 = ub;
         }
     }
-
     return g1;
 }
 
-
-float bisect_eval  (int _wtype, unsigned int _k, unsigned int _m, float _arg, float * _h, float _gamma)
+float bisect_eval(int _wtype, unsigned int _k, unsigned int _m,
+                  float _arg, float * _h, float _gamma)
 {
     // design filter
     liquid_firdes_windowf(_wtype,2*_k*_m+1,_gamma*0.5f/(float)_k,_arg,_h);
