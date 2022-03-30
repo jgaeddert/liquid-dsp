c99aff9fadf4332fcf174effab268695fb5b9089
Joseph D. Gaeddert <joseph@liquidsdr.org>
2022-03-30 07:11:45 -0400

    resamp2/autotest: adding filter response tests

--- src/filter/tests/resamp2_crcf_autotest.c
+++ src/filter/tests/resamp2_crcf_autotest.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2007 - 2015 Joseph Gaeddert
+ * Copyright (c) 2007 - 2022 Joseph Gaeddert
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -23,9 +23,7 @@
 #include "autotest/autotest.h"
 #include "liquid.h"
 
-// 
-// AUTOTEST : test half-band filterbank (analyzer)
-//
+// test half-band filterbank (analyzer)
 void autotest_resamp2_analysis()
 {
     unsigned int m=5;       // filter semi-length (actual length: 4*m+1)
@@ -72,36 +70,32 @@ void autotest_resamp2_analysis()
 
 #if 0
     // debugging
-    FILE * fid = fopen("resamp2_test.m", "w");
+    const char filename[] = "autotest_resamp2_analysis_test.m";
+    FILE * fid = fopen(filename, "w");
     fprintf(fid,"clear all\n");
     fprintf(fid,"close all\n");
     for (i=0; i<2*n; i++)
         fprintf(fid,"x(%3u) = %12.4e + j*%12.4e;\n", i+1, crealf(x[i]), cimagf(x[i]));
-
     for (i=0; i<n; i++) {
         fprintf(fid,"y0(%3u) = %12.4e + j*%12.4e;\n", i+1, crealf(y0[i]), cimagf(y0[i]));
         fprintf(fid,"y1(%3u) = %12.4e + j*%12.4e;\n", i+1, crealf(y1[i]), cimagf(y1[i]));
     }
-
     // save expected values
     for (i=0; i<n; i++) {
         fprintf(fid,"z0(%3u) = %12.4e + j*%12.4e;\n", i+1, cosf(i*2*f0), sinf(i*2*f0));
         fprintf(fid,"z1(%3u) = %12.4e + j*%12.4e;\n", i+1, cosf(i*2*f1), sinf(i*2*f1));
     }
-
     fprintf(fid,"m = %u;\n", m);
     fprintf(fid,"figure;\n");
     fprintf(fid,"t = 0:(length(y0)-1);\n");
     //fprintf(fid,"plot(t,real(z0),t-m+0.5,real(y0));\n");
     fprintf(fid,"plot(t,real(z1),t-m+0.5,real(y1));\n");
     fclose(fid);
-    printf("results written to '%s'\n","resamp2_test.m");
+    printf("results written to '%s'\n",filename);
 #endif
 }
 
-// 
-// AUTOTEST : test half-band filterbank (synthesizer)
-//
+// test half-band filterbank (synthesizer)
 void autotest_resamp2_synthesis()
 {
     unsigned int m=5;       // filter semi-length (actual length: 4*m+1)
@@ -147,15 +141,14 @@ void autotest_resamp2_synthesis()
 
 #if 0
     // debugging
-    FILE * fid = fopen("resamp2_test.m", "w");
+    const char filename[] = "autotest_resamp2_synthesis_test.m";
+    FILE * fid = fopen(filename, "w");
     fprintf(fid,"clear all\n");
     fprintf(fid,"close all\n");
-
     for (i=0; i<n+2*m+1; i++) {
         fprintf(fid,"x0(%3u) = %12.4e + j*%12.4e;\n", i+1, crealf(x0[i]), cimagf(x0[i]));
         fprintf(fid,"x1(%3u) = %12.4e + j*%12.4e;\n", i+1, crealf(x1[i]), cimagf(x1[i]));
     }
-
     for (i=0; i<2*n; i++)
         fprintf(fid,"y(%3u) = %12.4e + j*%12.4e;\n", i+1, crealf(y[i]), cimagf(y[i]));
 
@@ -164,12 +157,60 @@ void autotest_resamp2_synthesis()
         fprintf(fid,"z(%3u) = %12.4e + j*%12.4e;\n", i+1, cosf(i*0.5f*f0) + cosf(i*(M_PI+0.5f*f1)),
                                                           sinf(i*0.5f*f0) + sinf(i*(M_PI+0.5f*f1)));
     }
-
     fprintf(fid,"m = %u;\n", m);
     fprintf(fid,"figure;\n");
     fprintf(fid,"t = 0:(length(y)-1);\n");
     fprintf(fid,"plot(t,real(z),t-2*m,real(y));\n");
     fclose(fid);
-    printf("results written to '%s'\n","resamp2_test.m");
+    printf("results written to '%s'\n",filename);
 #endif
 }
+
+// test half-band resampler filter response
+void testbench_resamp2_crcf_filter(unsigned int _m, float _As)
+{
+    // error tolerance [dB]
+    float tol = 0.5f;
+
+    // create the half-band resampler
+    resamp2_crcf q = resamp2_crcf_create(_m,0,_As);
+
+    // get impulse response
+    unsigned int h_len = 4*_m+1;
+    float complex h_0[h_len];   // low-frequency reponse
+    float complex h_1[h_len];   // high-frequency response
+    unsigned int i;
+    for (i=0; i<h_len; i++)
+        resamp2_crcf_filter_execute(q, i==0 ? 1 : 0, h_0+i, h_1+i);
+
+    // clean up allocated objects
+    resamp2_crcf_destroy(q);
+
+    // compute expected transition band (extend slightly for relaxed constraints)
+    float ft = estimate_req_filter_df(_As, h_len) * 1.1;
+
+    // verify low-pass frequency response
+    autotest_psd_s regions_h0[] = {
+      {.fmin=-0.5,       .fmax=-0.25-ft/2, .pmin= 0, .pmax=-_As+tol, .test_lo=0, .test_hi=1},
+      {.fmin=-0.25+ft/2, .fmax=+0.25-ft/2, .pmin=-1, .pmax=+1,       .test_lo=1, .test_hi=1},
+      {.fmin=+0.25+ft/2, .fmax=+0.5,       .pmin= 0, .pmax=-_As+tol, .test_lo=0, .test_hi=1},
+    };
+    liquid_autotest_validate_psd_signal(h_0, h_len, regions_h0, 3,
+        liquid_autotest_verbose ? "autotest_resamp2_crcf_filter_h0.m" : NULL);
+
+    // verify high-pass frequency response
+    autotest_psd_s regions_h1[] = {
+      {.fmin=-0.5,       .fmax=-0.25-ft/2, .pmin=-1, .pmax=+1,       .test_lo=1, .test_hi=1},
+      {.fmin=-0.25+ft/2, .fmax=+0.25-ft/2, .pmin= 0, .pmax=-_As+tol, .test_lo=0, .test_hi=1},
+      {.fmin=+0.25+ft/2, .fmax=+0.5,       .pmin=-1, .pmax=+1,       .test_lo=1, .test_hi=1},
+    };
+    liquid_autotest_validate_psd_signal(h_1, h_len, regions_h1, 3,
+        liquid_autotest_verbose ? "autotest_resamp2_crcf_filter_h1.m" : NULL);
+}
+
+// test different configurations
+void autotest_resamp2_crcf_filter_0(){ testbench_resamp2_crcf_filter( 4, 60.0f); }
+void autotest_resamp2_crcf_filter_1(){ testbench_resamp2_crcf_filter( 7, 60.0f); }
+void autotest_resamp2_crcf_filter_2(){ testbench_resamp2_crcf_filter(12, 60.0f); }
+void autotest_resamp2_crcf_filter_3(){ testbench_resamp2_crcf_filter(15, 80.0f); }
+
