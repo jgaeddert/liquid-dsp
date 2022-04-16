524f0adaab35c53fc76ef60df8ba07f831460256
Joseph D. Gaeddert <joseph@liquidsdr.org>
2022-04-16 14:19:33 -0400

    build/multichannel: changing filenames to indicate prototype

--- src/multichannel/src/firpfbch_cccf.c
+++ src/multichannel/src/firpfbch_cccf.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2007 - 2015 Joseph Gaeddert
+ * Copyright (c) 2007 - 2022 Joseph Gaeddert
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -48,6 +48,6 @@
 #define PRINTVAL_TC(X,F)    PRINTVAL_CFLOAT(X,F)
 #define PRINTVAL_TI(X,F)    PRINTVAL_CFLOAT(X,F)
 
-// source files
-#include "firpfbch.c"
+// prototypes
+#include "firpfbch.proto.c"
 
