7980c11d1cfd927bb0c16211ff45339d539b5b28
Joseph D. Gaeddert <joseph@liquidsdr.org>
2022-04-17 07:28:22 -0400

    build/dotprod: changing filenames to indicate prototype

--- src/dotprod/src/dotprod_rrrf.c
+++ src/dotprod/src/dotprod_rrrf.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2007 - 2021 Joseph Gaeddert
+ * Copyright (c) 2007 - 2022 Joseph Gaeddert
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -35,4 +35,4 @@
 #define TC_COMPLEX      0
 #define TI_COMPLEX      0
 
-#include "dotprod.c"
+#include "dotprod.proto.c"
