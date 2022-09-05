7c933601883575d211316826a5c3bc7c3da32597
Joseph D. Gaeddert <joseph@liquidsdr.org>
2022-09-05 09:07:23 -0400

    gmsk modem: adding base autotests

---
+++ src/modem/tests/gmskmodem_autotest.c
@@ -0,0 +1,100 @@
+/*
+ * Copyright (c) 2007 - 2022 Joseph Gaeddert
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+#include "autotest/autotest.h"
+#include "liquid.h"
+
+// Help function to keep code base small
+void gmskmodem_test_mod_demod(unsigned int _k,
+                              unsigned int _m,
+                              float        _bt)
+{
+    // create modulator/demodulator pair
+    gmskmod mod = gmskmod_create(_k, _m, _bt);
+    gmskdem dem = gmskdem_create(_k, _m, _bt);
+
+    // derived values
+    unsigned int delay = _m + _m;
+    
+    unsigned int  num_symbols = 80 + delay; // number of symbols to test
+
+    msequence ms = msequence_create_default(7);
+
+    float complex buf[_k];      // sample buffer
+    unsigned int  sym_in [num_symbols]; // symbol buffer
+    unsigned int  sym_out[num_symbols]; // symbol buffer
+
+    // modulate, demodulate, count errors
+    unsigned int i;
+    for (i=0; i<num_symbols; i++) {
+        // generate random symbol
+        sym_in[i] = msequence_generate_symbol(ms, 1);
+
+        // modulate
+        gmskmod_modulate(mod, sym_in[i], buf);
+
+        // demodulate
+        gmskdem_demodulate(dem, buf, sym_out + i);
+    }
+
+    // count errors
+    for (i=0; i<num_symbols; i++) {
+        if (i >= delay) {
+#if 0
+            // print results
+            if (liquid_autotest_verbose) {
+                printf("  %3u : input = %2u, output = %2u %s\n",
+                        i, sym_in[i-delay], sym_out[i],
+                        (sym_in[i-delay] == sym_out[i]) ? "" : "*");
+            }
+#endif
+
+            // check result
+            CONTEND_EQUALITY(sym_in[i-delay], sym_out[i]);
+        }
+    }
+
+    // clean it up
+    msequence_destroy(ms);
+    gmskmod_destroy(mod);
+    gmskdem_destroy(dem);
+}
+
+// base configuration
+void autotest_gmskmodem_k4_m3_b025() { gmskmodem_test_mod_demod( 4, 3, 0.25f); }
+
+// test different samples/symbol
+void autotest_gmskmodem_k2_m3_b025() { gmskmodem_test_mod_demod( 2, 3, 0.25f); }
+void autotest_gmskmodem_k3_m3_b025() { gmskmodem_test_mod_demod( 3, 3, 0.25f); }
+void autotest_gmskmodem_k5_m3_b025() { gmskmodem_test_mod_demod( 5, 3, 0.25f); }
+void autotest_gmskmodem_k8_m3_b033() { gmskmodem_test_mod_demod( 8, 3, 0.25f); }
+
+// test different filter semi-lengths
+void autotest_gmskmodem_k4_m1_b025() { gmskmodem_test_mod_demod( 4, 1, 0.25f); }
+void autotest_gmskmodem_k4_m2_b025() { gmskmodem_test_mod_demod( 4, 2, 0.25f); }
+void autotest_gmskmodem_k4_m8_b025() { gmskmodem_test_mod_demod( 4, 8, 0.25f); }
+
+// test different filter bandwidth factors
+void autotest_gmskmodem_k4_m3_b020() { gmskmodem_test_mod_demod( 4, 3, 0.20f); }
+void autotest_gmskmodem_k4_m3_b033() { gmskmodem_test_mod_demod( 4, 3, 0.25f); }
+void autotest_gmskmodem_k4_m3_b050() { gmskmodem_test_mod_demod( 4, 3, 0.25f); }
+
