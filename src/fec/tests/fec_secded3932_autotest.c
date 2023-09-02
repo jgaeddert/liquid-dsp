370f92fdebff5c798aa6f2dc18b5fe44265d69c0
Joseph D. Gaeddert <joseph@liquidsdr.org>
2023-09-02 15:49:27 -0400

    fec/secded3932: reducing verbosity in favor of logging

--- src/fec/tests/fec_secded3932_autotest.c
+++ src/fec/tests/fec_secded3932_autotest.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2007 - 2015 Joseph Gaeddert
+ * Copyright (c) 2007 - 2023 Joseph Gaeddert
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -26,9 +26,7 @@
 #include "autotest/autotest.h"
 #include "liquid.internal.h"
 
-//
-// AUTOTEST: SEC-DEC (39,32) codec (no errors)
-//
+// SEC-DEC (39,32) codec (no errors)
 void autotest_secded3932_codec_e0()
 {
     // generate symbol
@@ -53,9 +51,7 @@ void autotest_secded3932_codec_e0()
     CONTEND_EQUALITY(sym_org[3], sym_dec[3]);
 }
 
-//
-// AUTOTEST: SEC-DEC (39,32) codec (single error)
-//
+// SEC-DEC (39,32) codec (single error)
 void autotest_secded3932_codec_e1()
 {
     unsigned int k; // error location
@@ -97,9 +93,7 @@ void autotest_secded3932_codec_e1()
     }
 }
 
-//
-// AUTOTEST: SEC-DEC (39,32) codec (double error detection)
-//
+// SEC-DEC (39,32) codec (double error detection)
 void autotest_secded3932_codec_e2()
 {
     // total combinations of double errors: nchoosek(39,2) = 741
@@ -109,7 +103,6 @@ void autotest_secded3932_codec_e2()
 
     for (j=0; j<39-1; j++) {
         if (liquid_autotest_verbose)
-            printf("***** %2u *****\n", j);
         
         for (k=0; k<39-1-j; k++) {
             // generate symbol
@@ -144,17 +137,18 @@ void autotest_secded3932_codec_e2()
             unsigned char sym_dec[4];
             int syndrome_flag = fec_secded3932_decode_symbol(sym_rec, sym_dec);
 
-
+#if 0
             if (liquid_autotest_verbose) {
                 // print error vector
                 printf("%3u, e = ", k);
-                liquid_print_bitstring(e[0], 7);
+                liquid_print_bitstring(e[0], 6);
                 liquid_print_bitstring(e[1], 8);
                 liquid_print_bitstring(e[2], 8);
-                liquid_print_bitstring(e[3], 8);
-                liquid_print_bitstring(e[4], 8);
                 printf(" flag=%2d\n", syndrome_flag);
             }
+#endif
+            liquid_log_debug("secded(22,16), testing errors at indices (%2u,%2u), syndrome flag: %2u",
+                j, k, syndrome_flag);
 
             // validate syndrome flag is '2'
             CONTEND_EQUALITY(syndrome_flag, 2);
