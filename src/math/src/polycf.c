2cb6e09ac36eb26ee2bcf3249401652d102a2ae4
Joseph D. Gaeddert <joseph@liquidsdr.org>
2023-09-09 10:39:12 -0400

    math/poly: returning ENOIMP (not implemented error) for complex findroots

--- src/math/src/polycf.c
+++ src/math/src/polycf.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2007 - 2022 Joseph Gaeddert
+ * Copyright (c) 2007 - 2023 Joseph Gaeddert
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -52,6 +52,6 @@ int polycf_findroots(float complex * _p,
                      unsigned int    _k,
                      float complex * _roots)
 {
-    return liquid_error(LIQUID_EUMODE,"polycf_findroots(), complex root-finding not yet supported");
+    return liquid_error(LIQUID_ENOIMP,"polycf_findroots(), complex root-finding not yet supported");
 }
 
