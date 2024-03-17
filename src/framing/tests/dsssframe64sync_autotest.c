cdf0cdfab1b1560be20021c6b1b42cef59fcef32
Joseph D. Gaeddert <joseph@liquidsdr.org>
2024-03-17 14:47:22 -0400

    dsssframe64: simplifying autotest

--- src/framing/tests/dsssframe64sync_autotest.c
+++ src/framing/tests/dsssframe64sync_autotest.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2007 - 2022 Joseph Gaeddert
+ * Copyright (c) 2007 - 2024 Joseph Gaeddert
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -27,48 +27,25 @@
 #include "autotest/autotest.h"
 #include "liquid.h"
 
-static int callback_dsssframe64sync_autotest(
-    unsigned char *  _header,
-    int              _header_valid,
-    unsigned char *  _payload,
-    unsigned int     _payload_len,
-    int              _payload_valid,
-    framesyncstats_s _stats,
-    void *           _userdata)
-{
-    //printf("callback invoked, payload valid: %s\n", _payload_valid ? "yes" : "no");
-    int * frames_recovered = (int*) _userdata;
-
-    *frames_recovered += _header_valid && _payload_valid ? 1 : 0;
-    return 0;
-}
-
 // AUTOTEST : test simple recovery of frame in noise
 void autotest_dsssframe64sync()
 {
-    unsigned int i;
-    int frames_recovered = 0;
-
     // create objects
-    dsssframe64gen fg = dsssframe64gen_create();
-    dsssframe64sync fs = dsssframe64sync_create(callback_dsssframe64sync_autotest,
-            (void*)&frames_recovered);
-    dsssframe64sync_set_threshold(fs, 0.5f);
+    dsssframe64gen  fg = dsssframe64gen_create();
+    dsssframe64sync fs = dsssframe64sync_create(NULL, NULL);
 
     // generate the frame
     unsigned int frame_len = dsssframe64gen_get_frame_len(fg);
-    float complex frame[frame_len];
+    float complex * frame = (float complex *)malloc(frame_len*sizeof(float complex));
     dsssframe64gen_execute(fg, NULL, NULL, frame);
 
     // add some noise
+    unsigned int i;
     for (i=0; i<frame_len; i++)
         frame[i] += 1.0f*(randnf() + _Complex_I*randnf()) * M_SQRT1_2;
 
     // try to receive the frame
     dsssframe64sync_execute(fs, frame, frame_len);
-
-    // check to see that exactly one frame was recovered
-    CONTEND_EQUALITY( frames_recovered, 1 );
 
     // parse statistics
     framedatastats_s stats = dsssframe64sync_get_framedatastats(fs);
@@ -77,8 +54,9 @@ void autotest_dsssframe64sync()
     CONTEND_EQUALITY(stats.num_payloads_valid,  1);
     CONTEND_EQUALITY(stats.num_bytes_received, 64);
 
-    // destroy objects
+    // destroy objects and free memory
     dsssframe64gen_destroy(fg);
     dsssframe64sync_destroy(fs);
+    free(frame);
 }
 
