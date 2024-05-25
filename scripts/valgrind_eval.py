18e6a12335378170c0d769a874b9e50c66ee2314
Joseph D. Gaeddert <joseph@liquidsdr.org>
2024-05-25 11:54:41 +0000

    valgrind: adding option to run specific tests

--- scripts/valgrind_eval.py
+++ scripts/valgrind_eval.py
@@ -2,28 +2,41 @@
 '''Run valgrind on each autotest and catalog memory errors'''
 import argparse, json, os, sys, subprocess
 
-def generate_json(path):
+def generate_json(path: str='valgrind', overwrite=False):
     filename = path + '/init.json'
-    os.system('./xautotest -t0 -o %s' % (filename,))
+    if overwrite or not os.path.exists(filename):
+        os.system('./xautotest -t0 -o %s' % (filename,))
     return json.load(open(filename,'r'))
+
+def run_test(test: dict, opts: str='--tool=memcheck --leak-check=full --track-origins=yes',
+        path: str='valgrind', seed: int=1, debug: bool=True):
+    '''run a specific test'''
+    filename = '%s/memcheck.%.4u.%s.log' % (path, test['id'], test['name'])
+    cmd = 'valgrind %s --log-file=%s ./xautotest -t %u -R %u' % (opts, filename, test['id'], seed)
+    if debug: print(cmd)
+    os.system(cmd)
+    # TODO parse result
 
 def main(argv=None):
     p = argparse.ArgumentParser(description=__doc__)
     p.add_argument('-output', default='valgrind', type=str, help='output directory')
+    #p.add_argument('-search', default=None,       type=str, help='search')
+    p.add_argument('-test', default=None,       type=int, help='run a specific test')
     args = p.parse_args()
 
+    # generate and load .json object
     v = generate_json(args.output)
-    print(v)
 
-    # iterate over all tests and execute
+    # set additional configuration options
     opts = '--tool=memcheck --leak-check=full --track-origins=yes'
-    for i,test in enumerate(v['tests']):
-        filename = '%s/memcheck.%.4u.%s.log' % (args.output, test['id'], test['name'])
-        cmd = 'valgrind %s --log-file=%s ./xautotest -t %u' % (opts, filename, test['id'],)
-        print(cmd)
-        os.system(cmd)
 
-        # TODO parse result
+    if args.test is None:
+        # iterate over all tests and execute
+        for i,test in enumerate(v['tests']):
+            run_test(test, opts, seed=v['rseed'])
+    else:
+        # run a specific test
+        run_test(v['tests'][args.test], opts, seed=v['rseed'])
 
 if __name__ == '__main__':
     sys.exit(main())
