a7071287472908840c74080a2422bbe69ce7b5c5
Joseph D. Gaeddert <joseph@liquidsdr.org>
2024-12-21 15:19:55 -0500

    build: fixing typo with linker makefile

--- scripts/liquid_linker_test.mk
+++ scripts/liquid_linker_test.mk
@@ -6,7 +6,7 @@ check: liquid_linker_test
 	./$<
 
 liquid_linker_test: % : scripts/%.c
-	$(CC) -Wall -O2 -o $@ %< -lm -lc -lliquid
+	$(CC) -Wall -O2 -o $@ $< -lm -lc -lliquid
 
 clean:
 	rm -f liquid_linker_test
