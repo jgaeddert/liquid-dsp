769f4e4564aa33e59cbcc31eba0bbf29ab0c6311
Joseph D. Gaeddert <joseph@liquidsdr.org>
2025-01-12 08:38:19 -0500

    cmake: moving function to print variables to external file

---
+++ cmake/debug.cmake
@@ -0,0 +1,8 @@
+# dump a (sorted) list of cmake variables
+function(liquid_debug_print_vars)
+    get_cmake_property(_variableNames VARIABLES)
+    list (SORT _variableNames)
+    foreach (_variableName ${_variableNames})
+        message(STATUS "${_variableName}=${${_variableName}}")
+    endforeach()
+endfunction()
