8097af8041533a8b64a7f46ca9e1ac899bcd6048
Joseph D. Gaeddert <joseph@liquidsdr.org>
2025-09-20 08:50:11 -0400

    nco/crcq16: reverting to old implementation
      * new nco macros are heavily focused on floating-point implementation
      * includes macros for float literals, uses 'M_PI', etc.
      * should probably refactor this, but the fixed-point implementation
        for the NCO probably doesn't need this level of fidelity
      * expecting nco_crcq16 to mostly run on embedded systems anyway

--- examples/nco_crcq16_example.c
+++ examples/nco_crcq16_example.c
@@ -11,27 +11,28 @@ int main()
     // options
     int          type        = LIQUID_NCO;      // nco type
     float        fc          = 0.1f*M_SQRT1_2;  // nco tone frequency
-    unsigned int num_samples = 240000;          // number of samples to run
-    unsigned int nfft        =   4000;          // spectral periodogram FFT size
+    unsigned int num_samples =   4000;          // number of samples to run
+    unsigned int nfft        =    960;          // spectral periodogram FFT size
 
     // create the NCO object
     nco_crcq16 q = nco_crcq16_create(type);
-    nco_crcq16_set_frequency(q, 2*M_PI*fc);
+    nco_crcq16_set_frequency(q, q16_angle_float_to_fixed(2*M_PI*fc));
     nco_crcq16_print(q);
 
     // create spectral periodogram
     spgramcf periodogram = spgramcf_create_default(nfft);
     spgramcf_print(periodogram);
 
+    cq16_t buf_out[num_samples];
     unsigned int i;
     for (i=0; i<num_samples; i++) {
-        cq16_t y;
-        nco_crcq16_cexpf(q, &y);
+        nco_crcq16_cexpf(q, buf_out + i);
         nco_crcq16_step(q);
-        printf("%12.8f %12.8f\n", q16_fixed_to_float(y.real), q16_fixed_to_float(y.imag));
+        //printf("%12.8f %12.8f\n", q16_fixed_to_float(buf_out[i].real), q16_fixed_to_float(buf_out[i].imag));
+        //printf("%12d %12d\n", buf_out[i].real, buf_out[i].imag);
         
         // push resulting sample through periodogram
-        spgramcf_push(periodogram, cq16_fixed_to_float(y));
+        spgramcf_push(periodogram, cq16_fixed_to_float(buf_out[i]));
     }
 
     // compute power spectral density output
@@ -47,17 +48,29 @@ int main()
     fprintf(fid,"%% %s : auto-generated file\n", OUTPUT_FILENAME);
     fprintf(fid,"clear all;\n");
     fprintf(fid,"close all;\n\n");
+    fprintf(fid,"n    = %u;\n", num_samples);
     fprintf(fid,"nfft = %u;\n", nfft);
+    fprintf(fid,"t    = 0:(n-1);\n");
     fprintf(fid,"f    = [0:(nfft-1)]/nfft - 0.5;\n");
+    fprintf(fid,"y    = zeros(1,n);\n");
     fprintf(fid,"psd  = zeros(1,nfft);\n");
+    for (i=0; i<num_samples; i++)
+        fprintf(fid,"y(%6u) = %9d + %9dj;\n", i+1, buf_out[i].real, buf_out[i].imag);
     for (i=0; i<nfft; i++)
         fprintf(fid,"psd(%6u) = %12.4e;\n", i+1, psd[i]);
     fprintf(fid,"figure;\n");
-    fprintf(fid,"plot(f, psd, '-', 'LineWidth',1.5);\n");
-    fprintf(fid,"xlabel('Normalized Frequency [f/F_s]');\n");
-    fprintf(fid,"ylabel('Power Spectral Density [dB]');\n");
-    fprintf(fid,"grid on;\n");
-    fprintf(fid,"axis([-0.5 0.5 -60 40]);\n");
+    fprintf(fid,"subplot(2,1,1)\n");
+    fprintf(fid,"  plot(t, real(y), t, imag(y));\n");
+    fprintf(fid,"  xlabel('Time [Sample Index]');\n");
+    fprintf(fid,"  ylabel('NCO Output');\n");
+    fprintf(fid,"  grid on;\n");
+    fprintf(fid,"  axis([-5 100 -600 600]);\n");
+    fprintf(fid,"subplot(2,1,2)\n");
+    fprintf(fid,"  plot(f, psd, '-', 'LineWidth',1.5);\n");
+    fprintf(fid,"  xlabel('Normalized Frequency [f/F_s]');\n");
+    fprintf(fid,"  ylabel('Power Spectral Density [dB]');\n");
+    fprintf(fid,"  grid on;\n");
+    fprintf(fid,"  axis([-0.5 0.5 -60 40]);\n");
 
     fclose(fid);
     printf("results written to %s.\n", OUTPUT_FILENAME);
--- src/nco/src/nco_crcq16.c
+++ src/nco/src/nco_crcq16.c
@@ -21,47 +21,472 @@
  */
 
 //
-// numerically-controlled oscillator (nco) API, floating point precision
+// numerically-controlled oscillator (nco) API, 16-bit fixed-point precision
 //
 
+#include <math.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <assert.h>
+
+#include "liquidfpm.internal.h"
 #include "liquid.internal.h"
 
-// declare fixed-point name-mangling macros
-#define LIQUID_FIXED
-#define Q(name)                 LIQUID_CONCAT(q16,name)
-#define CQ(name)                LIQUID_CONCAT(cq16,name)
+#define EXTENSION "crcq16"
 
-// data types
-#define T                       q16_t   // primitive general
-#define TC                      cq16_t  // coefficients
+#define NCO_PLL_BANDWIDTH_DEFAULT   (0.1)
+#define NCO_PLL_GAIN_DEFAULT        (1000)
 
-// naming extensions (useful for print statements)
-#define EXTENSION               "crcq16"
+#define LIQUID_DEBUG_NCO            (0)
 
-// macros
-#define NCO(name)               LIQUID_CONCAT(nco_crcq16,name)
-//#define SYNTH(name)             LIQUID_CONCAT(synth_crcq16,name)
-//#define IIRFILT_RRR(name)       LIQUID_CONCAT(iirfilt_rrrq16,   name)
-//#define IIRFILTSOS_RRR(name)    LIQUID_CONCAT(iirfiltsos_rrrq16,name)
+struct nco_crcq16_s {
+    liquid_ncotype type;
+    q16_t theta;            // NCO phase
+    q16_t d_theta;          // NCO frequency
+    q16_t sintab[256];      // sine table
+    unsigned int index; // table index
+    q16_t sine;
+    q16_t cosine;
+    int (*compute_sincos)(nco_crcq16 _q);
 
-// supporting objects/methods
-#define SIN                     q16_sin
-#define COS                     q16_cos
-#define SQRT                    q16_sqrt
-#define CONJ                    cq16_conj
+    // phase-locked loop
+    q16_t bandwidth;
+    q16_t zeta;
+    q16_t a[3];
+    q16_t b[3];
+    iirfiltsos_rrrq16 pll_filter;    // phase-locked loop filter
+};
 
-// constants, etc.
-//#define NCO_ONE                 (1.0f)
-//#define NCO_PI                  (M_PI)
-//#define NCO_2PI                 (2.0f*M_PI)
+// 
+// forward declaration of internal methods
+//
 
-// literal macros
-#define TIL_(l)                 l ## .0f
-#define TFL_(l)                 l ## f
-#define TIL(l)                  TIL_(l)
-#define TFL(l)                  TFL_(l)
+// constrain phase/frequency to be in [-pi,pi)
+int nco_crcq16_constrain_phase(nco_crcq16 _q);
+int nco_crcq16_constrain_frequency(nco_crcq16 _q);
 
-// prototypes
-#include "nco.proto.c"
-//#include "synth.proto.c"
+// compute trigonometric functions for nco/vco type
+int nco_crcq16_compute_sincos_nco(nco_crcq16 _q);
+int nco_crcq16_compute_sincos_vco(nco_crcq16 _q);
+
+// reset internal phase-locked loop filter
+int nco_crcq16_pll_reset(nco_crcq16 _q);
+
+// create nco/vco object
+nco_crcq16 nco_crcq16_create(liquid_ncotype _type)
+{
+    nco_crcq16 q = (nco_crcq16) malloc(sizeof(struct nco_crcq16_s));
+    q->type = _type;
+
+    // initialize sine table
+    unsigned int i;
+    for (i=0; i<256; i++)
+        q->sintab[i] = q16_float_to_fixed(sinf(2.0f*M_PI*(float)(i)/256.0f));
+
+    // set default pll bandwidth
+    q->a[0] = q16_one;  q->b[0] = 0;
+    q->a[1] = 0;        q->b[1] = 0;
+    q->a[2] = 0;        q->b[2] = 0;
+    q->pll_filter = iirfiltsos_rrrq16_create(q->b, q->a);
+    nco_crcq16_reset(q);
+    nco_crcq16_pll_set_bandwidth(q, NCO_PLL_BANDWIDTH_DEFAULT);
+
+    // set internal method
+    if (q->type == LIQUID_NCO) {
+        q->compute_sincos = &nco_crcq16_compute_sincos_nco;
+    } else if (q->type == LIQUID_VCO) {
+        q->compute_sincos = &nco_crcq16_compute_sincos_vco;
+    } else if (q->type == LIQUID_VCO_DIRECT) {
+        return liquid_error_config("nco_%s_create(), unsupported type 'LIQUID_VCO_DIRECT'", EXTENSION);
+    } else {
+        return liquid_error_config("nco_%s_create(), unknown type : %u", EXTENSION, q->type);
+    }
+
+    return q;
+}
+
+// copy object
+nco_crcq16 nco_crcq16_copy(nco_crcq16 _q)
+{
+    return liquid_error_config("nco_%s_copy(), method not yet implemented", EXTENSION);
+}
+
+// destroy nco object
+int nco_crcq16_destroy(nco_crcq16 _q)
+{
+    iirfiltsos_rrrq16_destroy(_q->pll_filter);
+    free(_q);
+    return LIQUID_OK;
+}
+
+//
+int nco_crcq16_print(nco_crcq16 _q)
+{
+    printf("<liquid.nco_%s", EXTENSION);
+    switch (_q->type) {
+    case LIQUID_NCO: printf(", type=\"nco\""); break;
+    case LIQUID_VCO_INTERP:
+    case LIQUID_VCO_DIRECT:
+        printf(", type=\"vco\""); break;
+    default:;
+    }
+    printf(", phase=0x%.8x", _q->theta);
+    printf(", freq=0x%.8x", _q->d_theta);
+    printf(">\n");
+    return LIQUID_OK;
+}
+
+// reset internal state of nco object
+int nco_crcq16_reset(nco_crcq16 _q)
+{
+    _q->theta = 0;
+    _q->d_theta = 0;
+
+    // reset sine table index
+    _q->index = 0;
+
+    // set internal sine, cosine values
+    _q->sine = 0;
+    _q->cosine = 1;
+
+    // reset pll filter state
+    return nco_crcq16_pll_reset(_q);
+}
+
+// get frequency
+q16_t nco_crcq16_get_frequency(nco_crcq16 _q)
+{
+    // return both internal NCO phase step as well
+    // as PLL phase step
+    return _q->d_theta;
+}
+
+// set frequency of nco object
+int nco_crcq16_set_frequency(nco_crcq16 _q,
+                             q16_t      _f)
+{
+    _q->d_theta = _f;
+    //_q->d_theta = q16_2pi >> 6;
+    return LIQUID_OK;
+}
+
+// adjust frequency of nco object
+int nco_crcq16_adjust_frequency(nco_crcq16 _q,
+                                q16_t _df)
+{
+    _q->d_theta += _df;
+    return LIQUID_OK;
+}
+
+// get phase
+q16_t nco_crcq16_get_phase(nco_crcq16 _q)
+{
+    return _q->theta;
+}
+
+// set phase of nco object, constraining phase
+int nco_crcq16_set_phase(nco_crcq16 _q, q16_t _phi)
+{
+    _q->theta = _phi;
+    //nco_crcq16_constrain_phase(_q);
+    return LIQUID_OK;
+}
+
+// adjust phase of nco object, constraining phase
+int nco_crcq16_adjust_phase(nco_crcq16 _q, q16_t _dphi)
+{
+    _q->theta += _dphi;
+    //nco_crcq16_constrain_phase(_q);
+    return LIQUID_OK;
+}
+
+// TODO: add stub for get_vcodirect_frequency
+// TODO: add stub for set_vcodirect_frequency
+
+// increment internal phase of nco object
+int nco_crcq16_step(nco_crcq16 _q)
+{
+    _q->theta += _q->d_theta;
+    //nco_crcq16_constrain_phase(_q);
+    return LIQUID_OK;
+}
+
+// compute and return sine
+q16_t nco_crcq16_sin(nco_crcq16 _q)
+{
+    // compute internal sin, cos
+    _q->compute_sincos(_q);
+
+    // return resulting cosine component
+    return _q->sine;
+}
+
+// compute and return cosine
+q16_t nco_crcq16_cos(nco_crcq16 _q)
+{
+    // compute internal sin, cos
+    _q->compute_sincos(_q);
+
+    // return resulting cosine component
+    return _q->cosine;
+}
+
+// compute sin, cos of internal phase
+int nco_crcq16_sincos(nco_crcq16 _q, q16_t* _s, q16_t* _c)
+{
+    // compute sine, cosine internally, calling implementation-
+    // specific function (nco, vco)
+    _q->compute_sincos(_q);
+
+    // return result
+    *_s = _q->sine;
+    *_c = _q->cosine;
+    return LIQUID_OK;
+}
+
+// compute complex exponential of internal phase
+int nco_crcq16_cexpf(nco_crcq16 _q,
+                      cq16_t *   _y)
+{
+#if 0
+    // compute sine, cosine internally, calling implementation-
+    // specific function (nco, vco)
+    _q->compute_sincos(_q);
+
+    // set _y[0] to [cos(theta) + _Complex_I*sin(theta)]
+    *_y = _q->cosine + _Complex_I*(_q->sine);
+#else
+    // FIXME: use internal values
+    *_y = cq16_cexpj( _q->theta );
+    //_y->real = _q->cosine;
+    //_y->imag = _q->sine;
+#endif
+    return LIQUID_OK;
+}
+
+// pll methods
+
+// reset pll state, retaining base frequency
+// TODO: add macro for this in global header
+int nco_crcq16_pll_reset(nco_crcq16 _q)
+{
+    // clear phase-locked loop filter
+    return iirfiltsos_rrrq16_reset(_q->pll_filter);
+}
+
+// set pll bandwidth
+int nco_crcq16_pll_set_bandwidth(nco_crcq16 _q,
+                                 float      _b)
+{
+    // validate input
+    if (_b < 0.0f) {
+        return liquid_error_config("nco_%s_set_bandwidth(), bandwidth must be positive", EXTENSION);
+    }
+
+    _q->bandwidth = _b;
+    _q->zeta = 1/sqrtf(2.0f);
+
+    float K     = NCO_PLL_GAIN_DEFAULT; // gain
+    float zeta  = 1.0f / sqrtf(2.0f);   // damping factor
+    float wn    = _b;                   // natural frequency
+    float t1    = K/(wn*wn);            // 
+    float t2    = 2.*zeta/wn - 1./K;    //
+
+    // compute scaling factor
+    float v = 1.0f / (1. + t1/2.0f);
+
+    // feed-forward coefficients
+    float b0 = v*2*K*(1.+t2/2.0f);
+    float b1 = v*2*K*(2.        );
+    float b2 = v*2*K*(1.-t2/2.0f);
+
+    // feed-back coefficients
+    float a0 = 1.0f;
+    float a1 = v*(-1. + t1/2.0f);
+    float a2 = 0.0f;
+    //printf("b = [%8.4f %8.4f %8.4f]\n", b0, b1, b2);
+    //printf("a = [%8.4f %8.4f %8.4f]\n", a0, a1, a2);
+
+    // feed-forward coefficients
+    _q->b[0] = q16_float_to_fixed( b0 );
+    _q->b[1] = q16_float_to_fixed( b1 );
+    _q->b[2] = q16_float_to_fixed( b2 );
+
+    // feed-back coefficients
+    _q->a[0] = q16_float_to_fixed( a0 );
+    _q->a[1] = q16_float_to_fixed( a1 );
+    _q->a[2] = q16_float_to_fixed( a2 );
+    
+    // set coefficients
+    return iirfiltsos_rrrq16_set_coefficients(_q->pll_filter, _q->b, _q->a);
+}
+
+// advance pll phase
+//  _q      :   nco object
+//  _dphi   :   phase error
+int nco_crcq16_pll_step(nco_crcq16 _q,
+                        q16_t      _dphi)
+{
+    // execute internal filter (direct form I)
+    q16_t error_filtered = 0.0f;
+    iirfiltsos_rrrq16_execute_df1(_q->pll_filter,
+                                  _dphi,
+                                  &error_filtered);
+
+    // increase frequency proportional to error
+    nco_crcq16_adjust_frequency(_q, error_filtered);
+
+    // constrain frequency
+    //nco_crcq16_constrain_frequency(_q);
+    return LIQUID_OK;
+}
+
+// mixing functions
+
+// Rotate input vector up by NCO angle, y = x exp{+j theta}
+//  _q      :   nco object
+//  _x      :   input sample
+//  _y      :   output sample
+int nco_crcq16_mix_up(nco_crcq16 _q,
+                      cq16_t     _x,
+                      cq16_t *   _y)
+{
+    // compute sine, cosine internally, calling implementation-
+    // specific function (nco, vco)
+    _q->compute_sincos(_q);
+
+    // multiply _x by [cos(theta) + _Complex_I*sin(theta)]
+    cq16_t v;
+    v.real = _q->cosine;
+    v.imag = _q->sine;
+    *_y = cq16_mul(_x, v);
+#if 0
+    *_y = _x * (_q->cosine + _Complex_I*(_q->sine));
+#endif
+    return LIQUID_OK;
+}
+
+// Rotate input vector down by NCO angle, y = x exp{-j theta}
+//  _q      :   nco object
+//  _x      :   input sample
+//  _y      :   output sample
+int nco_crcq16_mix_down(nco_crcq16 _q,
+                        cq16_t     _x,
+                        cq16_t *   _y)
+{
+    // compute sine, cosine internally
+    _q->compute_sincos(_q);
+
+    // multiply _x by [cos(-theta) + _Complex_I*sin(-theta)]
+    cq16_t v;
+    v.real = _q->cosine;
+    v.imag = -(_q->sine);
+    *_y = cq16_mul(_x, v);
+#if 0
+    *_y = _x * (_q->cosine - _Complex_I*(_q->sine));
+#endif
+    return LIQUID_OK;
+}
+
+
+// Rotate input vector array up by NCO angle:
+//      y(t) = x(t) exp{+j (f*t + theta)}
+// TODO : implement NCO/VCO-specific versions
+//  _q      :   nco object
+//  _x      :   input array [size: _n x 1]
+//  _y      :   output sample [size: _n x 1]
+//  _n      :   number of input, output samples
+int nco_crcq16_mix_block_up(nco_crcq16   _q,
+                            cq16_t *     _x,
+                            cq16_t *     _y,
+                            unsigned int _n)
+{
+    unsigned int i;
+    for (i=0; i<_n; i++)
+        nco_crcq16_mix_up(_q, _x[i], &_y[i]);
+#if 0
+    q16_t theta =   _q->theta;
+    q16_t d_theta = _q->d_theta;
+    for (i=0; i<_n; i++) {
+        // multiply _x[i] by [cos(theta) + _Complex_I*sin(theta)]
+        _y[i] = _x[i] * liquid_cexpjf(theta);
+        
+        theta += d_theta;
+    }
+
+    nco_crcq16_set_phase(_q, theta);
+#endif
+    return LIQUID_OK;
+}
+
+// Rotate input vector array down by NCO angle:
+//      y(t) = x(t) exp{-j (f*t + theta)}
+// TODO : implement NCO/VCO-specific versions
+//  _q      :   nco object
+//  _x      :   input array [size: _n x 1]
+//  _y      :   output sample [size: _n x 1]
+//  _n      :   number of input, output samples
+int nco_crcq16_mix_block_down(nco_crcq16   _q,
+                              cq16_t *     _x,
+                              cq16_t *     _y,
+                              unsigned int _n)
+{
+    unsigned int i;
+    for (i=0; i<_n; i++)
+        nco_crcq16_mix_down(_q, _x[i], &_y[i]);
+#if 0
+    q16_t theta =   _q->theta;
+    q16_t d_theta = _q->d_theta;
+    for (i=0; i<_n; i++) {
+        // multiply _x[i] by [cos(-theta) + _Complex_I*sin(-theta)]
+        _y[i] = _x[i] * liquid_cexpjf(-theta);
+        
+        theta += d_theta;
+    }
+
+    nco_crcq16_set_phase(_q, theta);
+#endif
+    return LIQUID_OK;
+}
+
+//
+// internal methods
+//
+
+// constrain frequency of NCO object to be in (-pi,pi)
+int nco_crcq16_constrain_frequency(nco_crcq16 _q)
+{
+    if (_q->d_theta > q16_pi)
+        _q->d_theta -= q16_2pi;
+    else if (_q->d_theta < -q16_pi)
+        _q->d_theta += q16_2pi;
+    return LIQUID_OK;
+}
+
+// constrain phase of NCO object to be in (-pi,pi)
+int nco_crcq16_constrain_phase(nco_crcq16 _q)
+{
+    if (_q->theta > q16_pi)
+        _q->theta -= q16_2pi;
+    else if (_q->theta < -q16_pi)
+        _q->theta += q16_2pi;
+    return LIQUID_OK;
+}
+
+// compute sin, cos of internal phase of nco
+int nco_crcq16_compute_sincos_nco(nco_crcq16 _q)
+{
+    // use fixed look-up table
+    q16_sincos_tab(_q->theta, &_q->sine, &_q->cosine);
+    return LIQUID_OK;
+}
+
+// compute sin, cos of internal phase of vco
+int nco_crcq16_compute_sincos_vco(nco_crcq16 _q)
+{
+    // use cordic (although might not be as accurate as q16_sincos_tab() method)
+    unsigned int num_iterations = q16_bits;
+    q16_sincos_cordic(_q->theta, &_q->sine, &_q->cosine, num_iterations);
+    return LIQUID_OK;
+}
 
