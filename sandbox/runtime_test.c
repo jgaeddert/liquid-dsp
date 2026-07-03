436255501e0101b646d23c851677fb027e42c6d7
Joseph D. Gaeddert <joseph@liquidsdr.org>
2026-07-03 09:11:24 -0400

    sandbox/runtime: converting to structured object test

--- sandbox/runtime_test.c
+++ sandbox/runtime_test.c
@@ -8,19 +8,16 @@ char __docstr__[] = "Test executing architecture-specific code at runtime";
 #include "liquid.h"
 #include "liquid.argparse.h"
 
-// launch point
-int runtime(void);
-
-// platform-specific funcitons and portable fallback
-int runtime_port(void);
-int runtime_neon(void);
-int runtime_sse (void);
-int runtime_sse2(void);
-int runtime_mmx (void);
-int runtime_avx (void);
-int runtime_avx2(void);
-int runtime_avx512f(void);
-int runtime_altivec(void);
+// custom object, e.g. dotprod
+typedef struct object_s * object;
+struct object_s
+{
+    // function pointer to execute method
+    int (*execute)(void);
+};
+object object_create(void);
+int    object_destroy(object);
+int    object_execute(object);
 
 //
 int main(int argc, char*argv[])
@@ -30,35 +27,54 @@ int main(int argc, char*argv[])
     liquid_argparse_parse(argc,argv);
 
     // invoke platform-specific method
-    return runtime();
+    //return runtime();
+    object q = object_create();
+    object_execute(q);
+    object_destroy(q);
+
+    return LIQUID_OK;
 }
 
-// launch point
-int runtime(void)
+// platform-specific funcitons and portable fallback
+int execute_neon(void)    { printf("execute_neon\n");    return LIQUID_OK; }
+int execute_sse(void)     { printf("execute_sse\n");     return LIQUID_OK; }
+int execute_sse2(void)    { printf("execute_sse2\n");    return LIQUID_OK; }
+int execute_mmx(void)     { printf("execute_mmx\n");     return LIQUID_OK; }
+int execute_avx(void)     { printf("execute_avx\n");     return LIQUID_OK; }
+int execute_avx2(void)    { printf("execute_avx2\n");    return LIQUID_OK; }
+int execute_avx512f(void) { printf("execute_avx512f\n"); return LIQUID_OK; }
+int execute_altivec(void) { printf("execute_altivec\n"); return LIQUID_OK; }
+int execute_port(void)    { printf("execute_port\n");    return LIQUID_OK; }
+
+object object_create(void)
 {
+    object _q = (object)malloc(sizeof(struct object_s));
+
+    // set execute function pointer based on run-time availability
     struct liquid_cpuinfo_s info;
     liquid_runtime_supported(&info);
 
-    if (info.neon)    return runtime_neon();
-    if (info.avx512f) return runtime_avx512f();
-    if (info.avx2)    return runtime_avx2();
-    if (info.avx)     return runtime_avx();
-    if (info.mmx)     return runtime_mmx();
-    if (info.sse2)    return runtime_sse2();
-    if (info.sse)     return runtime_sse();
-    if (info.altivec) return runtime_altivec();
+    if      (info.neon)    { _q->execute = &execute_neon;    }
+    else if (info.avx512f) { _q->execute = &execute_avx512f; }
+    else if (info.avx2)    { _q->execute = &execute_avx2;    }
+    else if (info.avx)     { _q->execute = &execute_avx;     }
+    else if (info.mmx)     { _q->execute = &execute_mmx;     }
+    else if (info.sse2)    { _q->execute = &execute_sse2;    }
+    else if (info.sse)     { _q->execute = &execute_sse;     }
+    else if (info.altivec) { _q->execute = &execute_altivec; }
+    else                   { _q->execute = &execute_port;    }
 
-    // fall back to portable version
-    return runtime_port();
+    return _q;
 }
 
-int runtime_neon(void)    { printf("runtime_neon\n");    return LIQUID_OK; }
-int runtime_sse(void)     { printf("runtime_sse\n");     return LIQUID_OK; }
-int runtime_sse2(void)    { printf("runtime_sse2\n");    return LIQUID_OK; }
-int runtime_mmx(void)     { printf("runtime_mmx\n");     return LIQUID_OK; }
-int runtime_avx(void)     { printf("runtime_avx\n");     return LIQUID_OK; }
-int runtime_avx2(void)    { printf("runtime_avx2\n");    return LIQUID_OK; }
-int runtime_avx512f(void) { printf("runtime_avx512f\n"); return LIQUID_OK; }
-int runtime_altivec(void) { printf("runtime_altivec\n"); return LIQUID_OK; }
-int runtime_port(void)    { printf("runtime_port\n");    return LIQUID_OK; }
+int object_destroy(object _q)
+{
+    free(_q);
+    return LIQUID_OK;
+}
+
+int object_execute(object _q)
+{
+    return _q->execute();
+}
 
