241b34709d3d92cd1bbdd4ec7628f467cd499e12
Joseph D. Gaeddert <joseph@liquidsdr.org>
2026-07-04 10:51:30 -0400

    build: using cmake to define preprocessor macros for SIMD

--- cmake/config.h.cmake
+++ cmake/config.h.cmake
@@ -6,3 +6,11 @@
 #cmakedefine HAVE_LIBC
 #cmakedefine SIZEOF_INT @SIZEOF_INT@
 #cmakedefine01 fftw3f_FOUND
+
+// SIMD extensions
+#cmakedefine01 BUILD_NEON
+#cmakedefine01 BUILD_AVX512
+#cmakedefine01 BUILD_AVX2
+#cmakedefine01 BUILD_AVX
+#cmakedefine01 BUILD_SSE4
+#cmakedefine01 BUILD_ALTIVEC
