d76f5020942c596047df2c0b84b15b37bd21c342
Joseph D. Gaeddert <joseph@liquidsdr.org>
2026-09-05 08:59:50 -0400

    autotest: printing audit results

--- autotest/autotest.c
+++ autotest/autotest.c
@@ -349,6 +349,8 @@ int liquid_registry_audit(liquid_registry _q)
 {
     // iterate over all tests
     unsigned int i;
+    unsigned int num_tests    = 0;
+    unsigned int num_warnings = 0;
     for (i=0; i<_q->num_tests; i++)
     {
         liquid_autotest autotest = _q->autotests[i];
@@ -357,22 +359,37 @@ int liquid_registry_audit(liquid_registry _q)
         if (autotest->status != LIQUID_AUTOTEST_PASS && autotest->status != LIQUID_AUTOTEST_FAIL)
             continue;
 
+        num_tests++;
+
         // test was run; ensure there was at least one check
         if (autotest->num_pass==0 && autotest->num_fail==0)
+        {
             liquid_log_warn("no checks run in %s", autotest->name);
+            num_warnings++;
+        }
 
         // check document string
         if (!strlen(autotest->docstr))
+        {
             liquid_log_warn("missing document string in %s", autotest->name);
+            num_warnings++;
+        }
 
         // check document string
         if (strcmp(autotest->docstr,"description")==0)
+        {
             liquid_log_warn("missing description in %s", autotest->name);
+            num_warnings++;
+        }
 
         // check keywords
         if (!strlen(autotest->keywords))
+        {
             liquid_log_warn("missing keywords in %s", autotest->name);
+            num_warnings++;
+        }
     }
+    liquid_log_info("audit yielded %u warnings across %u tests", num_warnings, num_tests);
 
     return LIQUID_OK;
 }
